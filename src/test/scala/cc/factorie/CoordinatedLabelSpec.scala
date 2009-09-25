c997631735df59efb05d9cb4ecb5af0214db7222
riedelcastro <riedelcastro@havana.local>
2009-09-25 15:29:18 -0400

    - added scala specs test for CoordinatedLabel

---
+++ src/test/scala/cc/factorie/CoordinatedLabelSpec.scala
@@ -0,0 +1,31 @@
+package cc.factorie
+
+
+import org.specs.runner.{JUnit, JUnit4}
+import org.specs.Specification
+
+/**
+ * @author Sebastian Riedel
+ */
+class CoordinatedLabelTest extends JUnit4(CoordinatedLabelSpec)
+object CoordinatedLabelSpec extends Specification with JUnit {
+
+  "A CoordinatedLabel" should {
+    "change its value when the set method is called" in {
+      implicit val diffList = new DiffList
+      val label = new CoordinatedLabel("ORG") {}
+      label.set("MISC")
+      label.value.entry must_== "MISC"
+    }
+
+    "create a diff object that undoes a set action when the set method is called" in {
+      implicit val diffList = new DiffList
+      val label = new CoordinatedLabel("ORG") {}
+      label.set("MISC")
+      diffList(0).undo
+      label.value.entry must_== "ORG"
+    }
+
+  }
+
+}
