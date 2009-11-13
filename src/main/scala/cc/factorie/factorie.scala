2542240ec701c22591b5f69d05c8f54870a5eecb
Andrew McCallum <mccallum@cs.umass.edu>
2009-11-13 11:18:41 +0100

    Force Iterator in for-loop in def repeat

--- src/main/scala/cc/factorie/factorie.scala
+++ src/main/scala/cc/factorie/factorie.scala
@@ -2,8 +2,8 @@ package cc.factorie
 
 /* package */ object factorie {
 
-  def repeat[T](n:Int)(f: =>T) : Iterable[T] = for (i <- 0 until n) yield f
-  def repeat(n:Int)(f: =>Unit) : Unit = for (i <- 0 until n) f
+  def repeat[T](n:Int)(f: =>T) : Iterable[T] = for (i <- 0 until n force) yield f
+  def repeat(n:Int)(f: =>Unit) : Unit = for (i <- 0 until n force) f
   def time(f: =>Unit) : Long = {
     val start = System.currentTimeMillis
     f
