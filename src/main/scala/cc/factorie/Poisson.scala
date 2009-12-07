94e3fb4f17c2bd73e50715b0b3102d5723b18c59
Andrew McCallum <mccallum@cs.umass.edu>
2009-12-07 14:35:16 +0100

    Remove extraneous . in license comment.

--- src/main/scala/cc/factorie/Poisson.scala
+++ src/main/scala/cc/factorie/Poisson.scala
@@ -1,7 +1,7 @@
 /* Copyright (C) 2008-2009 Univ of Massachusetts Amherst, Computer Science Dept
    This file is part of "FACTORIE" (Factor graphs, Imperative, Extensible)
    http://factorie.cs.umass.edu, http://code.google.com/p/factorie/
-   This software is provided under the terms of the Eclipse Public License 1.0.
+   This software is provided under the terms of the Eclipse Public License 1.0
    as published by http://www.opensource.org.  For further information,
    see the file `LICENSE.txt' included with this distribution. */
 
