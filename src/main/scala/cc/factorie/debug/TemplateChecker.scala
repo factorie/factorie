7da80eb647eddec59bcc17f1474b71a3f3e8f66b
mwick <mwick@horowitz.cs.umass.edu>
2010-09-30 16:54:55 -0400

    template checker

--- src/main/scala/cc/factorie/debug/TemplateChecker.scala
+++ src/main/scala/cc/factorie/debug/TemplateChecker.scala
@@ -1,9 +1,20 @@
 package cc.factorie.debug
 
+/**
+A tool for debugging models and template
+@author Michael Wick and Sameer Singh
+@since 0.9
+@see Template
+*/
 object TemplateChecker
 {
   def checkTemplate(t:Template) : Boolean =
     {
       false
+    }
+
+  def diffCheck : Boolean =
+    {
+      //check if efficient calculation of diff is same as exhuastive calcuation of diff
     }
 }
