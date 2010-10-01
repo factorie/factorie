a91d2114ca997c476beddc5fca526b0d1b26d4e1
Andrew McCallum <mccallum@cs.umass.edu>
2010-10-01 10:29:58 -0400

    Minor formatting changes to example/LDA

--- src/main/scala/cc/factorie/example/LDA.scala
+++ src/main/scala/cc/factorie/example/LDA.scala
@@ -13,9 +13,9 @@
    limitations under the License. */
 
 
-
 package cc.factorie.example
-import scala.collection.mutable.{ArrayBuffer,HashMap,HashSet,ListBuffer}
+
+import scala.collection.mutable.{ArrayBuffer, HashMap, HashSet, ListBuffer}
 import scala.util.matching.Regex
 import scala.io.Source
 import java.io.File
@@ -26,21 +26,21 @@ import cc.factorie.app.strings.alphaSegmenter
 
 object LDADemo {
   val numTopics = 10
-  class Z(p:Proportions, value:Int) extends MixtureChoice(p, value); Domain[Z].size = numTopics
-  class Word(ps:FiniteMixture[Proportions], z:MixtureChoiceVariable, value:String) extends CategoricalMixture[String](ps, z, value)
-  class Document(val file:String) extends ArrayBuffer[Word] { var theta:MutableDirichlet = null }
+  class Z(p: Proportions, value: Int) extends MixtureChoice(p, value);
+  Domain[Z].size = numTopics
+  class Word(ps: FiniteMixture[Proportions], z: MixtureChoiceVariable, value: String) extends CategoricalMixture[String](ps, z, value)
+  class Document(val file: String) extends ArrayBuffer[Word] {var theta: MutableDirichlet = null}
 
-  def main(args: Array[String]) : Unit = {
+  def main(args: Array[String]): Unit = {
     val directories = if (args.length > 0) args.toList else List("/Users/mccallum/research/data/text/nipstxt/nips11")
-    val lexer = new Regex("[a-zA-Z]+")
 
     // Read data and create generative variables
-    val phis = FiniteMixture(numTopics)(new GrowableDenseDirichlet(0.01) with TypedProportions[Word] { override def toString = "Phi("+countsSeq.toList+")" })
+    val phis = FiniteMixture(numTopics)(new GrowableDenseDirichlet(0.01) with TypedProportions[Word] {override def toString = "Phi(" + countsSeq.toList + ")"})
     val alphaMean = new DenseProportions(numTopics)
     val alphaPrecision = new RealVariableParameter(numTopics)
     val documents = new ArrayBuffer[Document]
     for (directory <- directories) {
-      println("Reading files from directory "+directory)
+      println("Reading files from directory " + directory)
       for (file <- new File(directory).listFiles; if (file.isFile)) {
         print("."); Console.flush
         val doc = new Document(file.toString)
@@ -52,8 +52,8 @@ object LDADemo {
         documents += doc
       }
     }
-    println("\nRead "+documents.size+" documents with "+documents.foldLeft(0)(_+_.size)+" tokens and "+Domain[Word].size+" types.")
-  
+    println("\nRead " + documents.size + " documents with " + documents.foldLeft(0)(_ + _.size) + " tokens and " + Domain[Word].size + " types.")
+
     // Fit model
     val zs = documents.flatMap(document => document.map(word => word.choice))
 
@@ -66,16 +66,17 @@ object LDADemo {
       zs.foreach(sampler.process(_))
       print("."); Console.flush
       if (i % 5 == 0) {
-        println ("Iteration "+i)
+        println("Iteration " + i)
+        // (phis ++ documents.map(_.theta)).foreach(_.export)
         (phis ++ documents.map(_.theta)).foreach(p => p.set(sampler.collapsed(p))(null))
         DirichletMomentMatching.estimate(alphaMean, alphaPrecision)
-        println("alpha = "+alphaMean.map(_ * alphaPrecision.doubleValue).mkString(" "))
-        phis.foreach(t => println("Topic "+phis.indexOf(t)+"  "+sampler.collapsed(t).top(10).map(dp => Domain[Word].get(dp.index)).mkString(" ")))
+        println("alpha = " + alphaMean.map(_ * alphaPrecision.doubleValue).mkString(" "))
+        phis.foreach(t => println("Topic " + phis.indexOf(t) + "  " + sampler.collapsed(t).top(10).map(dp => Domain[Word].get(dp.index)).mkString(" ")))
         println
       }
-    } 
+    }
     //phis.foreach(t => {println("\nTopic "+phis.indexOf(t)); t.top(20).foreach(x => println("%-16s %f".format(x.value,x.pr)))})
-    println("Finished in "+((System.currentTimeMillis-startTime)/1000.0)+" seconds")
+    println("Finished in " + ((System.currentTimeMillis - startTime) / 1000.0) + " seconds")
   }
 }
 
