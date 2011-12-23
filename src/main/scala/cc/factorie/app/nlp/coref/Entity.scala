387236a08e760a224156a653fae93ae766712827
Andrew McCallum <mccallum@cs.umass.edu>
2011-12-23 14:06:43 -0500

    Notes on pairwise coref.

--- src/main/scala/cc/factorie/app/nlp/coref/Entity.scala
+++ src/main/scala/cc/factorie/app/nlp/coref/Entity.scala
@@ -17,6 +17,7 @@ import cc.factorie._
 import cc.factorie.app.nlp._
 import scala.collection.mutable.{ArrayBuffer,ListBuffer}
 
+/** A trait for entities (and mentions, sub-entities and super-entities) in a coreference problem. */
 trait Entity {
   def string: String
   def superEntityRef: EntityRef
@@ -26,6 +27,7 @@ trait Entity {
   // Next two methods should only be called in EntityRef
   def _addSubEntity(e:Entity)(implicit d:DiffList): Unit
   def _removeSubEntity(e:Entity)(implicit d:DiffList): Unit
+  /** Recursively descend sub-entities to return only the Mentions */
   def mentions: Seq[Mention] = {
     var result = new ListBuffer[Mention]
     for (entity <- subEntities) {
@@ -37,20 +39,51 @@ trait Entity {
     }
     result
   }
+  def mentionsOfClass[A<:Mention](cls:Class[A]): Seq[A] = {
+    var result = new ListBuffer[A]
+    for (entity <- subEntities) {
+      if (cls.isAssignableFrom(entity.getClass))
+        result += entity.asInstanceOf[A]
+      result ++= entity.mentionsOfClass[A](cls)
+    }
+    result
+  }
+  def mentionsOfClass[A<:Mention](implicit m:Manifest[A]): Seq[A] = mentionsOfClass[A](m.erasure.asInstanceOf[Class[A]])
 }
 
-
-// In the world view with sub-entities and super-entities, a mention is a kind of entity with no sub-entities
-trait Mention extends TokenSpan with Entity {
-  def superEntityRef: EntityRef = attr[EntityRef]
+trait Mention extends Entity {
   // Just aliases for nicer names
   def entity: Entity = superEntity
   def entityRef: EntityRef = superEntityRef
   def setEntity(e:Entity)(implicit d:DiffList): Unit = setSuperEntity(e)
+}
+
+class PairwiseBoolean(val edge:PairwiseEdge, b:Boolean) extends BooleanVariable(b)
+class PairwiseEdge(m1:PairwiseMention, m2:PairwiseMention) extends ArrowVariable(m1,m2) {
+  val coref = new PairwiseBoolean(this, false)
+}
+trait PairwiseMention extends Mention {
+  val edges = new ArrayBuffer[ArrowVariable[Mention,Mention]]
+}
+abstract class PairwiseTemplate extends Template2[PairwiseBoolean,PairwiseEdge] with Statistics2[BooleanValue,CorefAffinity] {
+  def statistics(v:Values): Stat = {
+    val mention1 = v._2._1
+    val mention2 = v._2._2
+    val coref: Boolean = v._1.booleanValue
+    new Stat(null, null)
+  }
+}
+abstract class TransitivityTemplate extends Template3[PairwiseBoolean,PairwiseBoolean,PairwiseBoolean] with Statistics1[BooleanValue] {
+  //def unroll1(p:PairwiseBoolean) = for (m1 <- p.edge.src.edges; m2 <- p.edge.dst.edges; if (m1)
+}
+
+// In the world view with sub-entities and super-entities, a mention is a kind of entity with no sub-entities
+trait TokenSpanMention extends TokenSpan with Mention {
+  def superEntityRef: EntityRef = attr[EntityRef]
   //def setSuperEntity(e:Entity)(implicit d:DiffList): Unit = entityRef.setDst(e)
   private def _subEntities: SubEntities = attr[SubEntities]
   private def _ensuredSubEntities: SubEntities = attr.getOrElseUpdate[SubEntities](new SubEntities)
-  def subEntities: Iterable[Entity] = Nil
+  def subEntities: Iterable[Entity] = { val se = _subEntities; if (se eq null) Nil else se }
   def _addSubEntity(e:Entity)(implicit d:DiffList): Unit = _ensuredSubEntities.add(e)
   def _removeSubEntity(e:Entity)(implicit d:DiffList): Unit = _ensuredSubEntities.remove(e)
 }
@@ -59,7 +92,7 @@ class SubEntities extends SetVariable[Entity]
 
 
 class EntityVariable(var initialCanonicalString:String) extends SetVariable[Entity] with Entity with Attr {
-  def entityRef: EntityRef = attr[EntityRef]
+  //def entityRef: EntityRef = attr[EntityRef]
   val canonical = new StringVariable(initialCanonicalString)
   def canonicalString: String = canonical.value
   def string = canonicalString
@@ -86,9 +119,10 @@ class EntityRef(theSrc:Entity, initialDst:Entity) extends ArrowVariable(theSrc, 
   final def entity = dst
 }
 
+// Everything below here will be moved to a cc.factorie.example soon.
 
 object CorefAffinityDimensionDomain extends EnumDomain {
-  val ExactMatch, SuffixMatch, EntityContainsMention, EditDistance2, EditDistance4, NormalizedEditDistance9, NormalizedEditDistance5, Singleton = Value
+  val Bias, ExactMatch, SuffixMatch, EntityContainsMention, EditDistance2, EditDistance4, NormalizedEditDistance9, NormalizedEditDistance5, Singleton = Value
 }
 object CorefAffinityDomain extends CategoricalVectorDomain[String] {
   override lazy val dimensionDomain = CorefAffinityDimensionDomain
@@ -97,10 +131,45 @@ class CorefAffinity extends BinaryFeatureVectorVariable[String] {
   def domain = CorefAffinityDomain
 }
 
+class EntityMentionModel extends TemplateModel(
+  new Template1[EntityRef] with DotStatistics1[CorefAffinity#Value] {
+    override def statisticsDomains = List(CorefAffinityDimensionDomain)
+    //println("*** EntityMentionModel index="+CorefAffinityDomain.dimensionDomain.index("ExactMatch"))
+    weights(CorefAffinityDimensionDomain.Bias) = -1
+    weights(CorefAffinityDimensionDomain.ExactMatch) = 10
+    weights(CorefAffinityDimensionDomain.SuffixMatch) = 2
+    weights(CorefAffinityDimensionDomain.EntityContainsMention) = 3
+    weights(CorefAffinityDimensionDomain.EditDistance2) = 4
+    weights(CorefAffinityDimensionDomain.NormalizedEditDistance9) = -10
+    weights(CorefAffinityDimensionDomain.NormalizedEditDistance5) = -2
+    weights(CorefAffinityDimensionDomain.Singleton) = -1
+    def statistics(values:Values) = {
+      val mention: Entity = values._1._1
+      val entity: Entity = values._1._2
+      val affinity = new CorefAffinity
+      if (mention.string == entity.string) affinity += CorefAffinityDimensionDomain.ExactMatch
+      if (mention.string.takeRight(4) == entity.string.takeRight(4)) affinity += CorefAffinityDimensionDomain.SuffixMatch
+      if (entity.string.contains(mention.string)) affinity += CorefAffinityDimensionDomain.EntityContainsMention
+      val editDistance = entity.string.editDistance(mention.string)
+      val normalizedEditDistance = editDistance / entity.string.length
+      if (editDistance <= 2) affinity += CorefAffinityDimensionDomain.EditDistance2
+      if (editDistance <= 4) affinity += CorefAffinityDimensionDomain.EditDistance4
+      if (normalizedEditDistance > .5) affinity += CorefAffinityDimensionDomain.NormalizedEditDistance5
+      if (normalizedEditDistance > .9) affinity += CorefAffinityDimensionDomain.NormalizedEditDistance9
+      if (entity.subEntities.size == 1) affinity += CorefAffinityDimensionDomain.Singleton
+      val result = Stat(affinity.value)
+      val str = result.toString
+      //println("### EntityMentionModel Stat="+str)
+      result
+    }
+  }
+)
+
 class PairwiseModel extends TemplateModel(
   new Template1[EntityRef] with DotStatistics1[CorefAffinity#Value] {
     override def statisticsDomains = List(CorefAffinityDimensionDomain)
     //println("*** PairwiseModel index="+CorefAffinityDomain.dimensionDomain.index("ExactMatch"))
+    weights(CorefAffinityDimensionDomain.Bias) = -1
     weights(CorefAffinityDimensionDomain.ExactMatch) = 10
     weights(CorefAffinityDimensionDomain.SuffixMatch) = 2
     weights(CorefAffinityDimensionDomain.EntityContainsMention) = 3
@@ -147,12 +216,12 @@ object EntityTest {
   def brainDeadMentionExtraction(doc:Document): Unit = {
     for (i <- 0 until doc.length) {
       // Make a mention for simple pronouns
-      if (doc(i).string.matches("[Hh]e|[Ss]he|[Ii]t") && doc(i).spans.length == 0) new TokenSpan(doc, i, 1)(null) with Mention
+      if (doc(i).string.matches("[Hh]e|[Ss]he|[Ii]t") && doc(i).spans.length == 0) new TokenSpan(doc, i, 1)(null) with TokenSpanMention
       // Make a mention for sequences of capitalized words
       if (doc(i).isCapitalized && doc(i).spans.length == 0) {
         var len = 1
         while (i + len < doc.length && doc(i+len).isCapitalized) len += 1
-        new TokenSpan(doc, i, len)(null) with Mention
+        new TokenSpan(doc, i, len)(null) with TokenSpanMention
       }
     }
   }
@@ -165,7 +234,7 @@ object EntityTest {
   def corefInit(doc:Document): Unit = {
     //val entities = new ArrayBuffer[Entity]
     // Make each Mention its own Entity
-    for (mention <- doc.spansOfClass[Mention]) mention.attr += new EntityRef(mention, new EntityVariable("NULL"))
+    for (mention <- doc.spansOfClass[TokenSpanMention]) mention.attr += new EntityRef(mention, new EntityVariable("NULL"))
     // Assign each mention to its closest previous non-pronoun mention
     /*for (mention <- doc.orderedSpansOfClass[Mention]) {
       val prevMention = mention.document.spanOfClassPreceeding[Mention](mention.start)
@@ -174,14 +243,14 @@ object EntityTest {
     }*/
   }
   
-  object EntityRefSampler extends SettingsSampler[EntityRef](new PairwiseModel, null) {
+  object EntityRefSampler extends SettingsSampler[EntityRef](new EntityMentionModel, null) {
     def settings(entityRef:EntityRef) : SettingIterator = new SettingIterator {
-      val mention = entityRef.src.asInstanceOf[Mention]
+      val mention = entityRef.src.asInstanceOf[TokenSpanMention]
       val changes = new scala.collection.mutable.ArrayBuffer[(DiffList)=>Unit];
       // The "no change" proposal
       changes += {(d:DiffList) => {}}
       // Proposals to make coref with each of the previous mentions
-      for (antecedant <- mention.document.spansOfClassPreceeding[Mention](mention.start))
+      for (antecedant <- mention.document.spansOfClassPreceeding[TokenSpanMention](mention.start))
         changes += {(d:DiffList) => entityRef.set(antecedant)(d)}
       var i = 0
       def hasNext = i < changes.length
@@ -207,7 +276,7 @@ object EntityTest {
   }
   
   def coref(doc:Document): Unit = {
-    for (mention <- doc.orderedSpansOfClass[Mention]) {
+    for (mention <- doc.orderedSpansOfClass[TokenSpanMention]) {
       EntityRefSampler.process(mention.entityRef)
     }
   }
@@ -221,7 +290,7 @@ object EntityTest {
     corefInit(doc)
     coref(doc)
     // Print the results
-    for (mention <- doc.orderedSpansOfClass[Mention]) {
+    for (mention <- doc.orderedSpansOfClass[TokenSpanMention]) {
       println(mention+" => "+mention.entity+"\n")
     }
   }
