a76c7cc2685926f0086d45a0335f80e14ae8f8ae
Brian Martin <martin@cs.umass.edu>
2012-01-09 15:30:33 -0500

    added new bp trainer (and parallel version) (credit: Sameer)

---
+++ src/main/scala/cc/factorie/bp/Piece.scala
@@ -0,0 +1,61 @@
+package cc.factorie.bp
+
+import cc.factorie._
+import collection.mutable.{Map, HashMap}
+import la.{SparseVector, Vector}
+
+
+/**
+ * @author sameer
+ * @date 12/22/11
+ */
+
+trait Piece {
+
+  def valueAndGradient: (Double, Map[DotFamily, Vector])
+}
+
+class ModelPiece(val model: Model, val vars: Seq[VarWithTargetValue], val infer: (FG) => Unit = _.inferTreeUpDown(2, true)) extends Piece {
+
+  // compute the empirical counts of the model
+  lazy val empiricalCounts: Map[DotFamily, Vector] = {
+    val diff = new DiffList
+    vars.foreach(_.setToTarget(diff))
+    val result: Map[DotFamily, Vector] = new HashMap
+    for (dt <- model.familiesOfClass[DotFamily with Template]) {
+      // unroll the factors and count them up
+      result(dt) = dt.factors(vars).foldLeft(
+        new SparseVector(dt.statisticsVectorLength))((v, f) => {
+        v += f.statistics.vector
+        v
+      })
+    }
+    // undo the change
+    diff.undo
+    result
+  }
+
+  def truthScore: Double = {
+    val diff = new DiffList
+    vars.foreach(_.setToTarget(diff))
+    val score = model.score(vars)
+    diff.undo
+    score
+  }
+
+  def valueAndGradient: (Double, Map[DotFamily, Vector]) = {
+    val fg = new FG(model, vars.toSet) with SumProductFG
+    // perform BP
+    infer(fg)
+    // compute the gradient
+    val exps = fg.statExpectations
+    val gradient: Map[DotFamily, Vector] = new HashMap[DotFamily, Vector]
+    for (df <- model.familiesOfClass[DotFamily with Template]) {
+      val vector = new SparseVector(df.statisticsVectorLength)
+      vector += (exps(df) * -1.0)
+      vector += empiricalCounts(df)
+      gradient(df) = vector
+    }
+    (truthScore - fg.logZ, gradient)
+  }
+}
