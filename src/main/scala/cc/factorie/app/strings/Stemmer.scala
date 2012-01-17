71b468e8f8e87637055541b7e305d9e8586f1a22
Brian Martin <martin@cs.umass.edu>
2012-01-17 13:50:29 -0500

    stemmer: rewritten for style, concision, thread-safety

--- src/main/scala/cc/factorie/app/strings/Stemmer.scala
+++ src/main/scala/cc/factorie/app/strings/Stemmer.scala
@@ -1,366 +1,149 @@
 package cc.factorie.app.strings
 
-// http://tartarus.org/martin/PorterStemmer/scala.txt
-// "All these encodings of the algorithm can be used free of charge for any purpose."
-class Stemmer
-{
-  // word to be stemmed.
-  var b = ""
+/**
+ * Rewritten from http://tartarus.org/martin/PorterStemmer/scala.txt
+ * for thread-safety and style (but definitely not too pretty yet).
+ * @author Brian Martin
+ */
+object Stemmer {
+  val vowels = "aeiou"
+  val step1aVals = List(("sses", "ss"), ("ies","i"), ("ss","ss"), ("s", ""))
+  val step1bVals = List(("at", "ate"), ("bl","ble"), ("iz","ize"))
+  val step2Vals = List(("ational", "ate"),("tional","tion"),("enci","ence"),("anci","ance"),("izer","ize"),("bli","ble"),("alli", "al"), ("entli","ent"),("eli","e"),("ousli","ous"),("ization","ize"),("ation","ate"),("ator","ate"),("alism","al"), ("iveness","ive"),("fulness","ful"),("ousness", "ous"),("aliti", "al"),("iviti","ive"),("biliti", "ble"),("logi", "log"))
+  val step3Vals = List(("icate", "ic"),("ative",""),("alize","al"),("iciti","ic"),("ical","ic"),("ful",""),("ness",""))
+  val step4aVals = List(("al",""),("ance",""),("ence",""),("er",""),("ic",""),("able",""),("ible",""),("ant",""),("ement",""), ("ment",""),("ent",""))
+  val step4bVals = List(("ou",""),("ism",""),("ate",""),("iti",""),("ous",""),("ive",""),("ize",""))
 
-  // Just recode the existing stuff, then go through and refactor with some intelligence.
-  def cons( i: Int ): Boolean =
-  {
-    var ch = b( i )
+  def applySteps(_b: String): String = {
+    if (_b.size <= 2) return _b
 
-    // magic!
-    var vowels = "aeiou"
+    var b = _b
 
-    // multi return. yuck
-    if ( vowels.contains( ch ) )
-      return false
-
-    if ( ch == 'y'  )
-    {
-      if ( i == 0 )
-      {
-        return true
+    def isConsonant(i: Int): Boolean = {
+      if (b(i) == 'y') {
+        if (i == 0) true
+        else !isConsonant(i-1)
       }
-      else
-      {
-        // loop it!
-        return !cons( i - 1 )
-      }
+      else !vowels.contains(b(i))
     }
 
-    return true
+    /* m() measures the number of consonant sequences between 0 and j. if c is
+        a consonant sequence and v a vowel sequence, and <..> indicates arbitrary
+        presence,
 
-  }
-
-  // Add via letter or entire word
-  def add( ch: Char ) =
-  {
-    b += ch
-  }
-
-  def add( word: String ) =
-  {
-    b = word
-  }
-
-
-  /* m() measures the number of consonant sequences between 0 and j. if c is
-      a consonant sequence and v a vowel sequence, and <..> indicates arbitrary
-      presence,
-
-         <c><v>       gives 0
-         <c>vc<v>     gives 1
-         <c>vcvc<v>   gives 2
-         <c>vcvcvc<v> gives 3
-         ....
-   *
-   * I think this can be recoded far more neatly.
-   */
-
-   def calcM(s:String): Int =
-   {
-     var l = s.length
-     var count = 0
-     var currentConst = false
-
-     for ( c <- 0 to l-1 )
-     {
-       if ( cons( c ) )
-       {
-         if (!currentConst && c != 0 )
-         {
-           count += 1
-         }
-         currentConst = true
-       }
-       else
-       {
-         currentConst = false
-       }
-     }
-
-     return count
-   }
-
-
-   // removing the suffix 's', does a vowel exist?'
-   def vowelInStem( s: String): Boolean =
-   {
-
-     for (i <- 0 to b.length -1 - s.length )
-     {
-       if ( !cons( i ) )
-       {
-         return true
-       }
-     }
-
-     return false;
-
-   }
-
-   /* doublec(j) is true <=> j,(j-1) contain a double consonant. */
-   def doublec( ): Boolean =
-   {
-     var l = b.length - 1
-
-     if ( l < 1 )
-       return false
-
-     if ( b(l) != b(l-1) )
-       return false
-
-     return cons( l )
-
-   }
-
-   /* cvc(i) is true <=> i-2,i-1,i has the form consonant - vowel - consonant
-      and also if the second c is not w,x or y. this is used when trying to
-      restore an e at the end of a short word. e.g.
-
-         cav(e), lov(e), hop(e), crim(e), but
-         snow, box, tray.
-
-   */
-
-   def cvc( s:String): Boolean =
-   {
-     var i = b.length - 1 - s.length
-     if (i < 2 || !cons(i) || cons(i-1) || !cons(i-2))
-       return false;
-
-     var ch = b(i)
-
-     var vals = "wxy"
-
-     if ( vals.contains( ch ) )
-       return false
-
-     return true;
-   }
-
-
-  // returns true if it did the change.
-  def replacer( orig: String, replace:String, checker: Int => Boolean ): Boolean =
-  {
-
-
-    var l = b.length
-    var origLength = orig.length
-    var res = false
-
-    if ( b.endsWith( orig ) )
-    {
-      var n = b.substring( 0, l - origLength  )
-
-      var m = calcM( n )
-      if ( checker( m ) )
-      {
-        b = n + replace
+           <c><v>       gives 0
+           <c>vc<v>     gives 1
+           <c>vcvc<v>   gives 2
+           <c>vcvcvc<v> gives 3
+           ....
+     */
+    def calcM(s:String): Int = {
+      if (b.length == 0) return 0
+      var count = 0
+      (1 until s.length).foldLeft(isConsonant(0)) {
+        case (lastIsC, c) =>
+          val isC = isConsonant(c)
+          if (isC && !lastIsC) count += 1
+          isC
       }
-
-      res = true
-
+      count
     }
 
-    return res
-  }
+    /* removing the suffix string, s, does a vowel exist?' */
+    def vowelInStem(s: String): Boolean = (0 until b.length - s.length).find(!isConsonant(_)).isDefined
 
-  // process the list of tuples to find which prefix matches the case.
-  // checker is the conditional checker for m.
-  def processSubList( l:List[(String, String)], checker: Int=>Boolean ): Boolean =
-  {
-    var iter = l.elements
-    var done = false
-
-    while (!done && iter.hasNext )
-    {
-      var v = iter.next
-      done = replacer( v._1, v._2, checker )
-
+    /* doublec(j) is true <=> j,(j-1) contain a double consonant. */
+    def doublec(): Boolean = {
+      var l = b.length - 1
+      l >= 1 && b(l) == b(l-1) && isConsonant(l)
     }
 
-    return done
-  }
+    /* cvc(i) is true <=> i-2,i-1,i has the form consonant - vowel - consonant
+       and also if the second c is not w,x or y. this is used when trying to
+       restore an e at the end of a short word. e.g.
+          cav(e), lov(e), hop(e), crim(e), but
+          snow, box, tray.
+    */
+    def cvc(s:String): Boolean = {
+      val i = b.length - 1 - s.length
+      if (i < 2 || !isConsonant(i) || isConsonant(i-1) || !isConsonant(i-2)) false
+      else if ("wxy".contains(b(i))) false
+      else true
+    }
 
-  def step1()
-  {
+    def replacer(orig: String, replace:String, checker: Int => Boolean): Boolean = {
+      if (b.endsWith(orig)) {
+        var n = b.dropRight(orig.length)
+        if (checker(calcM(n)))
+          b = n + replace
+        true
+      }
+      else false
+    }
 
-    var l = b.length
-
-    var m = calcM( b )
+    def processSubList( l:List[(String, String)], checker: Int=>Boolean ): Boolean =
+      l.find(v => replacer(v._1, v._2, checker)).isDefined
 
     // step 1a
-    var vals = List( ("sses", "ss"), ("ies","i"), ("ss","ss"), ("s", "") )
-    processSubList( vals, _ >= 0)
+    processSubList(step1aVals, _ >= 0)
 
     // step 1b
-    if ( !(replacer( "eed", "ee", _>0) ) )
-    {
-
-      if ( ( vowelInStem("ed") && replacer("ed", "", _>=0) ) || ( vowelInStem("ing") && replacer( "ing", "", _>=0)  ) )
-      {
-
-        vals = List( ("at", "ate"), ("bl","ble"), ("iz","ize"))
-
-        if (! processSubList( vals, _>=0 ) )
-        {
-          // if this isn't done, then it gets more confusing.
-
-          m = calcM( b )
-          var last = b( b.length -1 )
-          if ( doublec() && !"lsz".contains( last ) )
-          {
-            b = b.substring( 0, b.length - 1 )
-          }
-          else
-          if ( m == 1 && cvc("") )
-          {
-            b = b + "e"
-          }
-        }
-      }
+    if (!(replacer("eed", "ee", _>0)) &&
+        ((vowelInStem("ed") && replacer("ed", "", _>=0) ) || ( vowelInStem("ing") && replacer( "ing", "", _>=0)  ) ) &&
+        (! processSubList(step1bVals, _>=0))) {
+      if ( doublec() && !"lsz".contains( b.last ) )
+        b = b.substring( 0, b.length - 1 )
+      else if (calcM(b) == 1 && cvc(""))
+        b = b + "e"
     }
-
 
     // step 1c
+    (vowelInStem("y") && replacer("y", "i", _>=0))
 
-    ( vowelInStem("y") && replacer("y", "i", _>=0))
+    // step 2
+    processSubList(step2Vals, _>0 )
 
-   }
+    // step 3
+    processSubList(step3Vals, _>0)
 
+    // step 4
+    var res = processSubList(step4aVals, _>1)
+    if (!res && b.length > 4 && (b(b.length-4) == 's' || b(b.length-4) == 't'))
+      res = replacer("ion", "", _>1)
+    if (!res) processSubList(step4bVals, _>1)
 
-   def step2( ) =
-   {
+    // step 5a
+    replacer("e", "", _>1)
+    if ( !cvc("e") )
+      replacer("e", "", _==1)
 
-      var vals = List( ("ational", "ate"),("tional","tion"),("enci","ence"),("anci","ance"),("izer","ize"),("bli","ble"),("alli", "al"),
-                       ("entli","ent"),("eli","e"),("ousli","ous"),("ization","ize"),("ation","ate"),("ator","ate"),("alism","al"),
-                       ("iveness","ive"),("fulness","ful"),("ousness", "ous"),("aliti", "al"),("iviti","ive"),("biliti", "ble"),("logi", "log"))
+    // step 5b
+    if (calcM(b) > 1 && doublec() && b.last == 'l') b = b.dropRight(1)
 
-      processSubList( vals, _>0 )
-
-   }
-
-  def step3( ) =
-  {
-
-      var vals = List( ("icate", "ic"),("ative",""),("alize","al"),("iciti","ic"),("ical","ic"),("ful",""),("ness",""))
-
-      processSubList( vals, _>0 )
-
-  }
-
-  def step4( ) =
-  {
-
-      // first part.
-      var vals = List( ("al",""),("ance",""),("ence",""),("er",""),("ic",""),("able",""),("ible",""),("ant",""),("ement",""),
-                       ("ment",""),("ent",""))
-
-      var res = processSubList( vals, _>1 )
-
-      // special part.
-      if ( ! res )
-      {
-        if ( b.length > 4 )
-        {
-          if (  b( b.length - 4  ) == 's' || b( b.length -4 ) == 't' )
-          {
-            res = replacer("ion", "", _>1)
-
-          }
-        }
-
-      }
-
-
-      // third part.
-      if ( !res )
-      {
-        var vals = List( ("ou",""),("ism",""),("ate",""),("iti",""),("ous",""),("ive",""),("ize",""))
-        res = processSubList( vals, _>1 )
-
-      }
-
-  }
-
-  def step5a( ) =
-  {
-
-      var res = false
-
-      res = replacer("e", "", _>1)
-
-      if ( !cvc("e") )
-      {
-        res = replacer("e", "", _==1)
-      }
-
-
-  }
-
-  def step5b( ) =
-  {
-
-    var res = false
-    var m = calcM( b )
-    if ( m > 1 && doublec() && b.endsWith("l") )
-    {
-      b = b.substring(0, b.length - 1)
-    }
-
-  }
-  
-  def apply(s:String): String = {
-    add(s)
-    if (b.length > 2) {
-      step1()
-      step2()
-      step3()
-      step4()
-      step5a()
-      step5b()
-    }
     b
   }
 
-}
+  def apply(s:String): String = applySteps(s)
 
+  def main(args: Array[String]): Unit = {
+    def getOWPL(f: String) = io.Source.fromFile(f).getLines.toSeq.map(_.trim)
 
-object Stemmer
-{
-  def apply(s:String): String = {
-    new Stemmer().apply(s)
-  }
-  
-  def main(args: Array[String]) = {
+    if (args.length != 2)
+      println("Expected arguments are a OWPL file of unstemmed and a OWPL file of properly stemmed words to check against.\n" +
+              "These are available at http://tartarus.org/martin/PorterStemmer/")
 
-    val filename = args(0)
-    val source = scala.io.Source.fromFile(filename)
-    var stemmer = new Stemmer()
+    val unstemmed = getOWPL(args(0))
+    val trueStemmed = getOWPL(args(1))
 
-    for (line <- source.getLines) {
+    println("unstemmed.size: " + unstemmed.size)
+    println("trueStemmed.size: " + trueStemmed.size)
 
-      var l = line.trim()
+    val stemmed = unstemmed.map(apply)
 
-      stemmer.add( l )
+    println("stemmed.size: " + stemmed.size)
 
-      if ( stemmer.b.length > 2 )
-      {
-        stemmer.step1()
-        stemmer.step2()
-        stemmer.step3()
-        stemmer.step4()
-        stemmer.step5a()
-        stemmer.step5b()
-      }
-
-      println(stemmer.b )
-    }
+    stemmed.zip(trueStemmed).filter(s => s._1 != s._2).foreach(println(_))
+    stemmed.zip(trueStemmed).foreach(s => assert(s._1 == s._2, s._1 + " " + s._2))
+    stemmed.zip(trueStemmed).take(20).foreach(s => println("sample: " + s._1 + " " + s._2))
   }
 }
-
