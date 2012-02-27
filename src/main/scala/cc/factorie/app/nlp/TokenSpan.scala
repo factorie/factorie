6dd0122ea112df6479c2548f9e01d31e15fef741
adamchandra <adamchandra@gmail.com>
2012-02-27 17:40:20 -0500

    uncommented formerly broken/now fixed code

--- src/main/scala/cc/factorie/app/nlp/TokenSpan.scala
+++ src/main/scala/cc/factorie/app/nlp/TokenSpan.scala
@@ -145,7 +145,6 @@ trait TokenSpanWithDocRefCubbie[DC<:DocumentCubbie[_,_,_]] extends TokenSpanCubb
   }
 }
 
-/* ACS: Commented out non-compiling code when integrating mongo-cubbie w/factorie 
 trait TokenSpanNerLabelCubbieSlot extends TokenSpanCubbie {
   def newTokenSpanNerLabel(ts:TokenSpan, s:String): cc.factorie.app.nlp.ner.NerLabel
   val ner = StringSlot("ner")
@@ -163,4 +162,3 @@ trait TokenSpanNerLabelCubbieSlot extends TokenSpanCubbie {
     ts
   }*/
 }
-*/
