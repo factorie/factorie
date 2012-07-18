7f878a37493e1999732222bc44d5ac3160068775
Andrew McCallum <mccallum@cs.umass.edu>
2012-07-18 19:08:59 -0400

    Working DecisionTreeTemplateWithStatistics2 from Luke.

--- src/main/scala/cc/factorie/example/DocumentClassifier4.scala
+++ src/main/scala/cc/factorie/example/DocumentClassifier4.scala
@@ -13,7 +13,6 @@
    limitations under the License. */
 
 
-
 package cc.factorie.example
 
 import scala.collection.mutable.{ArrayBuffer}
@@ -21,52 +20,55 @@ import scala.util.matching.Regex
 import scala.io.Source
 import java.io.File
 import cc.factorie._
+import la.Tensor
 
-/** A document classifier that will use Decision Trees (once they are actually implemented).
+/**A document classifier that will use Decision Trees (once they are actually implemented).
     Note that it also does not use any of the facilities of cc.factorie.app.classify.document */
 object DocumentClassifier4 {
-  
+
   object DocumentDomain extends CategoricalTensorDomain[String]
-  class Document(file:File) extends BinaryFeatureVectorVariable[String] {
+  class Document(file: File) extends BinaryFeatureVectorVariable[String] {
     def domain = DocumentDomain
     var label = new Label(file.getParentFile.getName, this)
     // Read file, tokenize with word regular expression, and add all matches to this BinaryFeatureVectorVariable
-    "\\w+".r.findAllIn(Source.fromFile(file).mkString).foreach(regexMatch => this += regexMatch.toString)
+    "\\w+".r.findAllIn(Source.fromFile(file, "ISO-8859-1").mkString).foreach(regexMatch => this += regexMatch.toString)
   }
   object LabelDomain extends CategoricalDomain[String]
-  class Label(name:String, val document:Document) extends LabelVariable(name) {
+  class Label(name: String, val document: Document) extends LabelVariable(name) {
     def domain = LabelDomain
   }
 
-  /** Factor between label and observed document */
-  val dtree = new DecisionTreeTemplateWithStatistics2[Label,Document] {
+  /**Factor between label and observed document */
+  val dtree = new DecisionTreeTemplateWithStatistics2[Label, Document]
+    with ID3DecisionTreeStatistics2[cc.factorie.DiscreteValue, Document#ValueType] {
     def statisticsDomains = Tuple(LabelDomain, DocumentDomain)
-    def unroll1 (label:Label) = Factor(label, label.document)
-    def unroll2 (token:Document) = throw new Error("Document values shouldn't change")
+    def unroll1(label: Label) = Factor(label, label.document)
+    def unroll2(token: Document) = throw new Error("Document values shouldn't change")
   }
 
   val model = new TemplateModel(dtree)
 
-  def main(args: Array[String]) : Unit = {
-    if (args.length < 2) 
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2)
       throw new Error("Usage: directory_class1 directory_class2 ...\nYou must specify at least two directories containing text files for classification.")
 
     // Read data and create Variables
     var documents = new ArrayBuffer[Document]
     for (directory <- args) {
       val directoryFile = new File(directory)
-      if (! directoryFile.exists) throw new IllegalArgumentException("Directory "+directory+" does not exist.")
+      if (!directoryFile.exists) throw new IllegalArgumentException("Directory " + directory + " does not exist.")
       for (file <- new File(directory).listFiles; if (file.isFile)) {
         //println ("Directory "+directory+" File "+file+" documents.size "+documents.size)
         documents += new Document(file)
       }
     }
-    
+
     // Make a test/train split
     val (testSet, trainSet) = documents.shuffle.split(0.5)
     var trainVariables = trainSet.map(_ label)
     var testVariables = testSet.map(_ label)
 
+    val start = System.currentTimeMillis
     // Train decision tree
     dtree.train(trainVariables)
 
@@ -74,9 +76,10 @@ object DocumentClassifier4 {
     val predictor = new VariableSettingsGreedyMaximizer[Label](model)
     predictor.processAll(trainVariables)
     predictor.processAll(testVariables)
-    println ("Train accuracy = "+ cc.factorie.defaultObjective.aveScore(trainVariables))
-    println ("Test  accuracy = "+ cc.factorie.defaultObjective.aveScore(testVariables))
-
+    println("Train accuracy = " + cc.factorie.defaultObjective.aveScore(trainVariables))
+    println("Test  accuracy = " + cc.factorie.defaultObjective.aveScore(testVariables))
+    println("Number of ms to train/test: " + (System.currentTimeMillis - start))
+    println(dtree.splittingFeatures.map(DocumentDomain.dimensionDomain.categories(_)))
   }
 }
 
