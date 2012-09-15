c78231a26b5cbe7e6d4a2f9b9765a0eaac602fbd
Alexandre Passos <mailto:alexandre.tp@gmail.com>
2012-09-15 15:14:14 -0400

    First implementation of linear regression

--- src/main/scala/cc/factorie/app/regress/Regression.scala
+++ src/main/scala/cc/factorie/app/regress/Regression.scala
@@ -1,7 +1,10 @@
 package cc.factorie.app.regress
 
 import cc.factorie._
+import app.classify.LabelList
 import cc.factorie.la._
+import cc.factorie.optimize.LimitedMemoryBFGS
+import java.io.File
 
 // Infrastructure for regression.  The architecture is somewhat parallel to app.classify. 
 
@@ -13,68 +16,68 @@ trait Regression[A<:TensorVar] {
   def regressor: Regressor[A]
 }
 
-class UnivariateRegression[D<:RealVar](theDependant:D, val regressor:Regressor[D], theValue:Double) extends RealSpikeMarginal1[D](theDependant, theValue) with Regression[D] {
-  val dependant = theDependant
-  val dependantValue = new RealValue(theValue)
-}
-
-
-class SimpleRegression[E<:RealVar,D<:RealVar](val explanatory:E, theDependant:D, val regressor:SimpleRegressor[E,D], theValue:Double) extends RealSpikeMarginal1[D](theDependant, theValue) with Regression[D] {
-  val dependant = theDependant
-  val dependantValue = new RealValue(theValue)
-}
-
-
 /** Perform regression, with output of type A.  Input type is not specified here.
-    @author Andrew McCallum */
+ 0   @author Andrew McCallum */
 trait Regressor[A<:TensorVar] {
   //def regressionValue(x:A#Value): Tensor // May not be possible if a Model.factors(x) is needed. 
+
   def regression(x:A): Regression[A]
   def regressions(xs:Iterable[A]): Seq[Regression[A]] = xs.toSeq.map(label => regression(label))
   //def regressionValues(xs:Iterable[A#Value]): Seq[Tensor] = xs.toSeq.map(x => regressionValue(x))
   /** Set the RealVariable to regressor-predicted value and return a Regression object summarizing the outcome. */
+
   def regress(x:A): Regression[A] = { val r = regression(x); /*r.globalize(null);*/ r }
-  def regress(xs:Iterable[A]): Seq[Regression[A]] = { val r = regressions(xs); /*r.foreach(_.globalize(null));*/ r }
+
+ def regress(xs:Iterable[A]): Seq[Regression[A]] = { val r = regressions(xs); /*r.foreach(_.globalize(null));*/ r }
 }
 
-/** A regression from a single real-valued input to a single real-valued output.
-    @author Andrew McCallum */
-trait SimpleRegressor[E<:RealVar,A<:RealVar] extends Regressor[A] {
-  def regressionValue(x:Double): Double
-  //def regressionValue(x:A#Value): RealValue = new RealValue(regressionValue(x.doubleValue)) 
-  override def regression(x:A): SimpleRegression[E,A] // = new SimpleRegression(x, this, regressionValue(x.doubleValue))
-  override def regressions(xs:Iterable[A]): Seq[SimpleRegression[E,A]] = xs.toSeq.map(label => regression(label))
-  /** Set the RealVariable to regressor-predicted value and return a Regression object summarizing the outcome. */
-  override def regress(x:A): SimpleRegression[E,A] = { val r = regression(x); r.globalize(null); r }
-  override def regress(xs:Iterable[A]): Seq[SimpleRegression[E,A]] = { val r = regressions(xs); r.foreach(_.globalize(null)); r }
-}
+class LinearRegressor[E<:TensorVar,A<:TensorVar](val dependant2Explanatory: A=>E, val weights: Tensor2) extends Regressor[A]{
+  linearRegressor =>
 
-
-/** Basic linear regressor with one real-valued input and one real-valued output.
-    @author Andrew McCallum */
-class SimpleLinearRegressor[E<:RealVar,D<:RealVar](val dependant2Explanatory:D=>E) extends SimpleRegressor[E,D] {
-  var offset = 0.0
-  var slope = 0.0
-  def regressionValue(e:Double): Double = e * slope + offset
-  def regression(x:D): SimpleRegression[E,D] = {
-    val explanatory: E = dependant2Explanatory(x)
-    new SimpleRegression[E,D](explanatory, x, this, regressionValue(explanatory.doubleValue))
-  }
-}
-class SimpleLinearRegressorTrainer[E<:RealVar,D<:RealVar](val dependant2Explanatory:D=>E) {
-  def train[E<:RealVar,D<:RealVar](ds:Iterable[D], dependant2Explanatory:D=>E): SimpleLinearRegressor[E,D] = {
-    val xs = ds.toSeq
-    val ys = xs.map(dependant2Explanatory(_))
-    //val xsum = xs.sum
-    throw new Error("Not yet implemented")
+  def regression(x: A) = {
+    val result = weights.matrixVector(dependant2Explanatory(x).value.linearize).reshape(x.value.dimensions)
+    new Regression[A] {
+      def dependant = x
+      def dependantValue = result.asInstanceOf[A#Value]
+      def regressor = linearRegressor
+    }
   }
 }
 
-class ModelBasedUnivariateRegressor[D<:RealVar](val model:Model) extends Regressor[D] {
-  def regression(d:D): Regression[D] = new UnivariateRegression(d, this, model.score(d))
+object LinearRegressionTrainer {
+  // Assumes variables are set to their target values
+  def train[E<:TensorVar, A<:TensorVar](examples: Iterable[A], dependant2Explanatory: A=>E, l2: Double): LinearRegressor[E,A] = {
+    val exampleDependent = examples.head
+    val exampleExplanatory = dependant2Explanatory(exampleDependent)
+    val weights = new DenseTensor2(exampleDependent.value.linearize.dim1, exampleExplanatory.value.linearize.dim1)
+    //val optimizer = new cc.factorie.optimize.ConjugateGradient()
+    val optimizer = new cc.factorie.optimize.LimitedMemoryBFGS()
+    val gradient = new DenseTensor2(weights.dim1, weights.dim2)
+    while(!optimizer.isConverged) {
+      gradient.zero()
+      var value = 0.0
+      examples.foreach(e => {
+        val features = dependant2Explanatory(e).value.linearize
+        val prediction = weights.matrixVector(features)
+        prediction.activeDomain.foreach(i => prediction(i) -= e.value(i))
+        value -= prediction.dot(prediction)
+        // add the gradients
+        prediction.activeDomain.foreach(i => {
+          features.activeDomain.foreach(j => {
+            gradient(i,j) -= 2*prediction(i)*features(j)
+          })
+        })
+      })
+
+      (0 to weights.dim1-1).foreach(i => {
+        (0 to weights.dim2-1).foreach(j => {
+          gradient(i,j) += l2 * weights(i,j)
+        })
+      })
+
+      optimizer.step(weights, gradient, value, 0.0)
+    }
+    new LinearRegressor(dependant2Explanatory, weights)
+  }
 }
 
-//class MultipleLinearRegressor[E<:TensorVar,D<:RealVar](val e2d:E=>D) extends Regressor[RealVar] {
-//  val weights: Tensor = null
-//  def regressionValue(t:Tensor): Double = weights dot t 
-//}
