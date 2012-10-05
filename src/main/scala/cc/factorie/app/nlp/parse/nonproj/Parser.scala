f21f3e83a95e06204eb8f04bd08233ee167172d3
Brian Martin <brianmartin@gmail.com>
2012-10-05 11:20:22 -0400

    Add NonProjective Dependency Parser

---
+++ src/main/scala/cc/factorie/app/nlp/parse/nonproj/Parser.scala
@@ -0,0 +1,432 @@
+package cc.factorie.app.nlp.parse.nonproj
+
+import cc.factorie._
+import cc.factorie.app.classify._
+import cc.factorie.app.nlp
+import nlp._
+import collection.mutable.HashSet
+import collection.mutable.ArrayBuffer
+import ParserSupport._
+import cc.factorie.la.WeightsTensor
+import scala.collection.mutable.ListBuffer
+import java.io.File
+
+/**
+ * A non-projective shift-reduce dependency parser based on Jinho Choi's thesis work and ClearNLP.
+ *
+ * @author Brian Martin
+ */
+
+object TrainWithSVM {
+  
+  def lTof(l: ParseDecisionVariable) = l.features
+  
+  def generateDecisions(ss: Seq[Sentence], p: Parser): Seq[ParseDecisionVariable] = {
+    val vs = new ArrayBuffer[ParseDecisionVariable]
+    var i = 0
+    for (s <- ss) {
+      p.parse(s)
+	  vs ++= p.instances
+	  p.clear()
+	  i += 1
+	  if (i % 1000 == 0)
+	    println("Parsed: " + i)
+    }
+    vs
+  }
+  
+  def trainSVM(ll: LabelList[ParseDecisionVariable, NonProjDependencyParserFeatures]): Classifier[ParseDecisionVariable] = {
+    (new SVMTrainer).train(ll)
+  }
+  
+  def train(ss: Seq[Sentence]): (Classifier[ParseDecisionVariable], Seq[ParseDecisionVariable]) = {
+    var p = new Parser()
+    p.mode = 0
+    val vs = generateDecisions(ss, p)
+    
+    (trainSVM(new LabelList[ParseDecisionVariable, NonProjDependencyParserFeatures](vs, lTof)), vs)
+  }
+  
+  def predict(c: Classifier[ParseDecisionVariable], ss: Seq[Sentence]): (Seq[Seq[(Int, String)]], Seq[Seq[(Int, String)]]) = {
+    val p = new Parser
+    
+    p.mode = 1 // predicting
+    p.predict = (v: ParseDecisionVariable) => { DecisionDomain.category(c.classify(v).bestLabelIndex) }
+    
+    val pred = new ListBuffer[Seq[(Int, String)]]
+    val gold = new ListBuffer[Seq[(Int, String)]]
+    
+    var i = 0
+    for (s <- ss) {
+	  gold append p.getSimpleDepArcs(s)
+	  
+      val dts = p.parse(s)
+	  p.clear()
+	  pred append dts.drop(1).map(dt => dt.head).map(arc => (arc.parentIdx, arc.label))
+	  
+	  i += 1
+	  if (i % 1000 == 0)
+	    println("Parsed: " + i)
+    }
+    
+    (gold.toSeq, pred.toSeq)
+  }
+  
+//  def boosting(m: liblinear.Model, ss: Seq[Sentence]): (Seq[Seq[(Int, String)]], Seq[Seq[(Int, String)]]) = {
+//    val p = new Parser
+//    
+//    p.mode = 2 // boosting 
+//    p.predict = (v: ParseDecisionVariable) => DecisionDomain.category(predictLibLinear(m ,lTof)(v))
+//    p.predict = (v: ParseDecisionVariable) => {c.classify(v); v.categoryValue }
+//    
+//    val pred = new ListBuffer[Seq[(Int, String)]]
+//    val gold = new ListBuffer[Seq[(Int, String)]]
+//    
+//    var i = 0
+//    for (s <- ss) {
+//	  gold append p.getSimpleDepArcs(s)
+//	  
+//      val dts = p.parse(s)
+//	  p.clear()
+//	  pred append dts.drop(1).map(dt => dt.head).map(arc => (arc.parentIdx, arc.label))
+//	  
+//	  i += 1
+//	  if (i % 1000 == 0)
+//	    println("Parsed: " + i)
+//    }
+//    
+//    (gold.toSeq, pred.toSeq)
+//  }
+    
+  def testAcc(c: Classifier[ParseDecisionVariable], ss: Seq[Sentence]): Unit = {
+    val (gold, pred) = predict(c, ss)
+    println("LAS: " + ParserEval.calcLas(gold, pred))
+    println("UAS: " + ParserEval.calcUas(gold, pred))
+  }
+  
+  def main(args: Array[String]) = {
+    
+    object opts extends cc.factorie.util.DefaultCmdOptions {
+      val trainFiles =  new CmdOption("train", List(""), "FILE...", "")
+      val testFiles =  new CmdOption("test", List(""), "FILE...", "")
+      val devFiles =   new CmdOption("dev", List(""), "FILE", "") { override def required = true }
+      val ontonotes = new CmdOption("onto", "", "", "")
+      val cutoff    = new CmdOption("cutoff", "0", "", "")
+      val modelDir =  new CmdOption("model", "model", "DIR", "Directory in which to save the trained model.")
+    }
+    opts.parse(args)
+    import opts._
+    
+    var loader = LoadConll2008.fromFilename(_)
+    if (ontonotes.wasInvoked)
+      loader = LoadOntonotes5.fromFilename(_)
+    
+    val sentences =   trainFiles.value.flatMap(f => loader(f).head.sentences)
+    val testSentences = devFiles.value.flatMap(f => loader(f).head.sentences)
+    
+    println("Total train sentences: " + sentences.size)
+    println("Total test sentences: " + testSentences.size)
+    
+    //var modelUrl: String = "/Users/brian/wrk-umass/factorie-parser/models/model1349188735567"
+    var modelUrl: String = null
+    var modelFile: File = null
+    
+    // generate training (even if predicting -- to fill domain)
+    NonProjParserFeaturesDomain.dimensionDomain.gatherCounts = true
+    
+    val trainingVs = generateDecisions(sentences, new Parser(0)) 
+    
+	println("# features < cutoff [" + cutoff.value.toInt + "]: " + (NonProjParserFeaturesDomain.dimensionDomain.size - NonProjParserFeaturesDomain.dimensionDomain.sizeAtOrAboveCount(cutoff.value.toInt)))
+	
+    NonProjParserFeaturesDomain.dimensionDomain.trimBelowCount(cutoff.value.toInt)
+    
+    NonProjParserFeaturesDomain._skipNonCategories = true
+    
+    if (modelUrl eq null) {
+	  modelFile = new File(modelDir.value + System.currentTimeMillis() + ".liblinear")
+	  modelFile.createNewFile()
+	  
+      val m = trainSVM(new LabelList[ParseDecisionVariable, NonProjDependencyParserFeatures](trainingVs, lTof))
+	  
+      testAcc(m, sentences)
+      println("Test")
+      println("------------")
+      testAcc(m, testSentences)
+	  
+//      m.save(new PrintStream(modelFile))
+      
+    }
+    else {
+      
+      throw new Error()
+      
+//      DecisionModel.skipNonCategories = true
+//      
+//	  modelFile = new File(modelUrl)
+//	  val m = SparseModel.load(modelFile)
+//	  
+//      testAcc(m, sentences)
+//      println("Test")
+//      println("------------")
+//      testAcc(m, testSentences)
+      
+    }
+    
+  }
+}
+
+class Parser(var mode: Int = 0) {
+  
+  import ParserConstants._
+  
+  var predict: ParseDecisionVariable => ParseDecision = null
+  
+  var goldHeads: Seq[DepArc] = null
+  var state: ParseState = null
+  var instances = new ArrayBuffer[ParseDecisionVariable] { override val initialSize = 100 }
+  
+  var debug = false
+  
+  val TRAINING   = 0
+  val PREDICTING = 1
+  val BOOSTING   = 2
+
+  def training   = mode == TRAINING
+  def predicting = mode == PREDICTING
+  def boosting   = mode == BOOSTING
+
+  def clear(): Unit = {
+    goldHeads = null
+    state = null
+    instances.clear()
+  }
+
+  def parse(s: Sentence): Array[DepToken] = {
+    
+    val depTokens = getDepTokens(s)
+    
+    state = new ParseState(0, 1, new HashSet[Int], depTokens)
+    
+    if (training || boosting)
+      goldHeads = getDepArcs(depTokens, s)
+      
+    var lastAction = null
+
+    while(state.input < depTokens.length) {
+      if (debug)
+	      println()
+	      
+      if (state.stack < 0)
+        noShift()
+      else {
+        val label = getDecision()
+        
+        if(debug)
+	        println(instances.last.features.activeCategories.mkString("\n"))
+        //println(label, label.shift_?, label.reduce_?, label.pass_?)
+
+        if (label.left_?) {
+          if (state.stack == ROOT_ID)
+            noShift()
+          else if (state.inputToken(0).isDescendentOf(state.stackToken(0)))
+            noPass()
+          else if (label.reduce_?)
+            leftReduce(label.label)
+          else
+            leftPass(label.label)
+        }
+        else if (label.right_?) {
+            if (state.stackToken(0).isDescendentOf(state.inputToken(0)))
+                noPass()
+            else if (label.shift_?)
+                rightShift(label.label)
+            else
+                rightPass(label.label)
+        }
+        else {
+            if (label.shift_?)
+                noShift()
+            else if (label.reduce_? && state.stackToken(0).hasHead())
+                noReduce()
+            else
+                noPass()
+        }
+
+      }
+    }
+    
+    return depTokens
+  }
+
+  def getDepTokens(s: Sentence): Array[DepToken] = {
+    val a = Array.ofDim[DepToken](s.length + 1)
+    a(0) = DepToken.root
+    for ((t, i) <- s.links.map(DepToken(_)).zipWithIndex) {
+      a(i+1) = t
+      a(i+1).state = state
+    }
+    a
+  }
+
+  def setTokenHeads(ts: Array[DepToken], as: Seq[DepArc]): Unit =
+    ts.zip(as).foreach { case (t: DepToken, a: DepArc) => t.head = a }
+  
+  def getSimpleDepArcs(s: Sentence): Seq[(Int, String)] = {
+    s.parse.parents.map(_ + 1).zip(s.parse.labels.map(_.value.category))
+  }
+  
+  def getDepArcs(ts: Array[DepToken], s: Sentence): Seq[DepArc] = {
+    Seq(new DepArc(ts(0), "<ROOT-ROOT>", 0)) ++
+	    getSimpleDepArcs(s).map { 
+	      case (i: Int, l: String) => new DepArc(ts(i), l, i)
+	    }
+  }
+
+  private def noShift() = shift()
+  private def noReduce() = reduce()
+  private def noPass() = pass()
+
+  private def leftArc(label: String) = {
+    val lambda = state.stackToken(0)
+    val beta = state.inputToken(0)
+    lambda.setHead(beta, label, state.input)
+    beta.lmDep = lambda
+  }
+
+  private def rightArc(label: String) {
+    val lambda = state.stackToken(0)
+    val beta = state.inputToken(0)
+    beta.setHead(lambda, label, state.stack)
+    lambda.rmDep = beta
+  }
+
+  private def shift() = {
+    debugPrint("Shift")
+    state.stack = state.input
+    state.input += 1
+  }
+
+  private def reduce() = {
+    debugPrint("Reduce")
+    state.reducedIds.add(state.stack)
+    passAux()
+  }
+
+  private def pass() = passAux()
+
+  private def passAux(): Unit = {
+
+    debugPrint("Pass")
+
+    var i = state.stack - 1
+    while (i >= 0) {
+      if (!state.reducedIds.contains(i)) {
+          state.stack = i
+          return
+      }
+      i -= 1
+    }
+
+    state.stack = i
+  }
+
+  private def leftReduce(label: String) { debugPrint("LeftReduce");   leftArc(label);  reduce() }
+  private def leftPass(label: String)   { debugPrint("LeftPass");  leftArc(label);  pass()   }
+  private def rightShift(label: String) { debugPrint("RightShift"); rightArc(label); shift()  }
+  private def rightPass(label: String)  { debugPrint("RightPass");  rightArc(label); pass()   }
+
+  private def getDecision(): ParseDecision = {
+
+    var decision: ParseDecision = null
+    
+    mode match {
+      case TRAINING => {
+	      decision = getGoldDecision()
+	      val label = new ParseDecisionVariable(decision, state)
+	      instances += label
+	    }
+      case PREDICTING => {
+          decision = predict(new ParseDecisionVariable(state))
+        }
+      case BOOSTING => {
+	      val label = new ParseDecisionVariable(getGoldDecision(), state)
+	      instances += label
+	      val d = predict(label)
+	      decision = d
+	    }
+    }
+    
+    decision
+  }
+
+  def getGoldDecision(): ParseDecision = {
+
+    val label: ParseDecision = getGoldLabelArc()
+
+    label.shiftOrReduceOrPass =
+      label.leftOrRightOrNo match {
+        case LEFT  => if (shouldGoldReduce(true)) REDUCE else PASS
+        case RIGHT => if (shouldGoldShift)        SHIFT  else PASS
+        case _ => {
+          if (shouldGoldShift) SHIFT
+          else if (shouldGoldReduce(false)) REDUCE
+          else PASS
+        }
+      }
+
+    label
+  }
+
+  def getGoldLabelArc(): ParseDecision = {
+
+    val headIdx   = goldHeads(state.stack).parentIdx
+    val headLabel = goldHeads(state.stack).label
+
+    // if beta is the head of lambda
+    if (headIdx == state.input)
+      return new ParseDecision(LEFT, -1, headLabel)
+
+    val inputHeadIdx   = goldHeads(state.input).parentIdx
+    val inputHeadLabel = goldHeads(state.input).label
+
+    // if lambda is the head of beta
+    if (inputHeadIdx == state.stack)
+      return new ParseDecision(RIGHT, -1, inputHeadLabel)
+
+    // lambda doesn't have a head
+    return new ParseDecision(NO, -1, "")
+  }
+
+  def shouldGoldShift: Boolean = {
+    // if the head of the input is to the left of the stack: don't shift
+    if (goldHeads(state.input).parentIdx < state.stack)
+      return false
+    // if the head of any token in the stack is the input: don't shift
+    else
+      for (i <- (state.stack - 1) until 0 by -1) if (!state.reducedIds.contains(i)) {
+        if (goldHeads(i).parentIdx == state.input)
+          return false
+      }
+
+    // else shift
+    true
+  }
+
+  def shouldGoldReduce(hasHead: Boolean): Boolean = {
+
+    if (!hasHead && !state.stackToken(0).hasHead)
+      return false
+
+    for (i <- (state.input + 1) until state.sentenceTokens.length)
+      if (goldHeads(i).parentIdx == state.stack)
+        return false
+
+    true
+  }
+  
+  private def debugPrint(str: String): Unit = {
+    if (debug)
+	  println(str + " " + state)
+  }
+
+}
---
+++ src/main/scala/cc/factorie/app/nlp/parse/nonproj/ParserSupport.scala
@@ -0,0 +1,306 @@
+package cc.factorie.app.nlp.parse.nonproj
+
+import cc.factorie._
+import app.nlp._
+
+import collection.mutable.HashSet
+
+object ParserSupport {
+    object ParserUtils {
+
+      val testFeatureSpec = """
+          {"features" : [
+
+              # basic features
+                ["l:f"], # lambda form
+                ["l:m"], # lambda lemma
+                ["l:p"], # lambda pos
+                ["b:f"], # beta form
+                ["b:m"], # beta lemma
+                ["b:p"], # beta pos
+                ["l:p", "l:m"],
+                ["b:p", "b:m"],
+                ["l:p", "b:p"],
+                ["l:p", "b:m"],
+                ["l:m", "b:p"],
+                ["l:m", "b:m"],
+
+              # 1-gram features
+               ["s-2:f"],
+               ["l-2:f"],
+               ["l-1:f"],
+               ["l+1:f"],
+               ["l+2:f"],
+               ["b-2:f"],
+               ["b-1:f"],
+               ["b+1:f"],
+               ["b+2:f"],
+
+              # 2-gram features
+               ["s-1:p", "l:p"],
+               ["s-1:m", "l:m"],
+               ["s-1:p", "b:p"],
+               ["s-1:m", "b:p"],
+               ["s-1:p", "b:m"],
+
+               ["b+1:p", "l:p"],
+               ["b+1:p", "l:m"],
+               ["b+1:p", "b:m"],
+               ["b+1:m", "b:m"],
+
+               ["b-1:p", "b:m"],
+
+              # 3-gram features
+               ["s-2:p", "s-1:p", "l:p"],
+               ["l-2:p", "l-1:p", "l:p"],
+               ["l-1:p", "l+1:p", "l:p"],
+               ["l+1:p", "l+2:p", "l:p"],
+               ["b-1:p", "b+1:p", "b:p"],
+               ["b+1:p", "b+2:p", "b:p"],
+
+               ["s-3:p", "l:p", "b:p"],
+               ["s-2:p", "l:p", "b:p"],
+               ["l+1:p", "l:p", "b:p"],
+               ["b-2:p", "l:p", "b:p"],
+               ["b-1:p", "l:p", "b:p"],
+               ["b+1:p", "l:p", "b:p"],
+               ["b+3:p", "l:p", "b:p"]
+
+            # dependency features
+     	#  	["l:d"], # lambda head label // TODO : check that this actually shows up
+         #      ["b:d"],
+         #      ["l:lmdr"]
+         #      ["l:rmdr"]
+
+         #     ["l_h:m", "l:p"], # l_h is the head of lambda
+
+         #     ["l_h:p", "l:p", "b:p"],
+         #      ["l_lmd:p", "l:p", "b:p"]
+         #      ["b_lmd:p", "l:p", "b:p"]
+
+         #    # binary features
+         #      ["l:b0"], # lambda is the leftmost token
+         #     ["b:b1"], # beta is the righttmost token
+         #     ["l:b2"] # lambda and beta are adjacent
+
+         #    # punctuation features
+         #      ["b:lnpb"] # left-nearest punctuation of beta
+         #      #["l:rnpl"], # right-nearest punctuation of lambda
+         #      #["l:lnpl"], # left-nearest punctuation of lambda
+         #      #["b:rnpb"], # right-nearest punctuation of beta
+
+            ]
+          }
+    
+        """
+    
+      lazy val featureGenerators: Seq[DependencyFeatureGenerator] = LoadParserFeatureSpec.fromJSON(testFeatureSpec)
+    }
+    
+    object ParserConstants {
+      
+      val SHIFT  = 0
+      val REDUCE = 1
+      val PASS   = 2
+    
+      val LEFT  = 0
+      val RIGHT = 1
+      val NO    = 2
+      
+      val ROOT_ID = 0
+      
+    }
+    
+    object DepToken {
+      def apply(token: Token) = new DepToken(token, form = token.string, lemma = token.attr[Lemma].lemma, pos = token.posLabel.categoryValue)
+      //TODO: will it be a problem to have this in a separate document?
+      //TODO: eclipse didn't like "lazy" here
+      val root = new DepToken(new Token(new Document("Root Dummy Document", ""), "<ROOT>"), form = "<ROOT>-f",  lemma = "<ROOT>-m", pos = "<ROOT>-p")
+      val nullToken = new DepToken(new Token(new Document("Null Dummy Document", ""), "<NULL>"), form = "<NULL>-f",  lemma = "<NULL>-m", pos = "<NULL>-p")
+    }
+    class DepToken(
+        var token: Token,
+        val form: String,
+        val lemma: String,
+        val pos: String,
+        var head: DepArc = new DepArc(null, null, -1),
+        var lmDep: DepToken = null,
+        var rmDep: DepToken = null,
+        var state: ParseState = null) {
+    
+      def setHead(headToken: DepToken, label: String, idx: Int) {
+        head.depToken = headToken
+        head.label = label
+        head.parentIdx = idx
+      }
+    
+      def hasHead(): Boolean = head.depToken != null
+    
+      def isDescendentOf(that: DepToken): Boolean = {
+        var ptr = this
+        while (ptr != null && ptr != DepToken.root) {
+          ptr = ptr.head.depToken
+          if (ptr == that)
+            return true
+        }
+        return false
+      }
+      
+//      def leftmostDependent(): DepToken = {
+//        
+//      }
+//      
+//      def rightmostDependent(): DepToken = {
+//        
+//      }
+//      
+      
+      override def toString = "(%s, f: %s, l: %s, p: %s)".format(token.string, form, lemma, pos)
+    
+    }
+    
+    class DepArc(var depToken: DepToken, var label: String, var parentIdx: Int) {
+      override def toString = (parentIdx, label).toString
+    }
+    
+    class ParseState(
+        var stack: Int,
+        var input: Int,
+        var reducedIds: HashSet[Int],
+        var sentenceTokens: Array[DepToken]) {
+      
+      def this(tokens: Array[DepToken]) = this(0, 1, HashSet[Int](), tokens)
+    
+      def inputToken(offset: Int): DepToken = {
+        val i = input + offset
+        if (i < 0 || sentenceTokens.size - 1 < i)
+          return DepToken.nullToken
+        else
+          sentenceTokens(i)
+      }
+      
+      def lambdaToken(offset: Int): DepToken = {
+        val i = stack + offset
+        if (i < 0 || sentenceTokens.size - 1 < i)
+          return DepToken.nullToken
+        else
+          sentenceTokens(i)
+      }
+      
+      def stackToken(offset: Int): DepToken = {
+    
+        // TODO: check for correctness (wait on Jinho)
+        if (offset == 0)
+          return sentenceTokens(stack)
+    
+        var off = math.abs(offset)
+        var dir = if (offset < 0) -1 else 1
+    
+        var i = stack + dir
+        while (0 < i && i < input) {
+    
+          if (!reducedIds.contains(i)) {
+            off -= 1
+            if (off == 0)
+              return sentenceTokens(i)
+          }
+    
+          i += dir
+        }
+    
+        return DepToken.nullToken
+      }
+      
+      def children(t: DepToken): Seq[Int] = {
+        throw new Error("TODO")
+        Seq(0)
+      }
+      
+      def lefmostAndRightmostDependent(t: DepToken): (DepToken, DepToken) = {
+        var min = Int.MaxValue
+        var max = Int.MinValue
+        for (c <- children(t)) {
+          if (c < min)
+            min = c
+          if (c > max)
+            max = c
+        }
+        return (sentenceTokens(min), sentenceTokens(max))
+      }
+      
+      override def toString = "[ParseState: %d, %d]" format (stack, input)
+    
+    }
+    
+    case class ParseDecision(var leftOrRightOrNo: Int, var shiftOrReduceOrPass: Int, var label: String) {
+    
+      import ParserConstants._
+    
+      def shift_?  = shiftOrReduceOrPass == SHIFT
+      def reduce_? = shiftOrReduceOrPass == REDUCE
+      def pass_?   = shiftOrReduceOrPass == PASS
+    
+      def left_?   = leftOrRightOrNo == LEFT
+      def right_?  = leftOrRightOrNo == RIGHT
+      
+      override def toString = {
+        val s = new StringBuffer
+        s append "["
+        s append "Decision: "
+        s append (leftOrRightOrNo match {
+          case LEFT  => "left"
+          case RIGHT => "right"
+          case NO    => "no"
+        })
+        s append "-"
+        s append (shiftOrReduceOrPass match {
+          case SHIFT  => "shift"
+          case REDUCE => "reduce"
+          case PASS   => "pass"
+        })
+        
+        s append (" label: " + label)
+        s append "]"
+        
+        s.toString()
+      }
+    
+    }
+    
+    // define variables and domains
+    object DecisionDomain extends CategoricalDomain[ParseDecision] {
+      this += new ParseDecision(-1, -1, "")
+      lazy val defaultCategory = this.head.category
+    }
+    class ParseDecisionVariable(targetDecision: ParseDecision, state: ParseState) extends LabelVariable(targetDecision) {
+    
+      def this(state: ParseState) = this(DecisionDomain.defaultCategory, state)
+    
+      def domain = DecisionDomain
+      val features = new NonProjDependencyParserFeatures(this)
+      features ++= ParserUtils.featureGenerators.map(_.apply(state))
+    
+    }
+    
+    object NonProjParserFeaturesDomain extends CategoricalTensorDomain[String] {
+      var _skipNonCategories = false
+    }
+    class NonProjDependencyParserFeatures(val decisionVariable: ParseDecisionVariable) extends BinaryFeatureVectorVariable[String] {
+      val decision = decisionVariable.value
+      def domain = NonProjParserFeaturesDomain
+      override def skipNonCategories = NonProjParserFeaturesDomain._skipNonCategories
+    }
+    
+//    object DecisionModel extends TemplateModel {
+//      var skipNonCategories = false
+//    
+//      val localTemplate = new TemplateWithDotStatistics2[ParseDecisionVariable, NonProjDependencyParserFeatures] {
+//        override def statisticsDomains = ((DecisionDomain, NonProjParserFeaturesDomain))
+//        def unroll1(decision: ParseDecisionVariable) = Factor(decision, decision.features)
+//        def unroll2(features: NonProjDependencyParserFeatures) = Factor(features.decisionVariable, features)
+//      }
+//    
+//      this += localTemplate
+//    }
+    
+}
