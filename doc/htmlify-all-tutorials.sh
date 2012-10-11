b6acf0d22167e2ff44c55f020549d8bcc9bdbaf3
Brian Martin <brianmartin@gmail.com>
2012-10-11 08:32:09 -0400

    Tut: add htmlify-all-tutorials.sh

---
+++ doc/htmlify-all-tutorials.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTPUT_DIR="./html"
+EXAMPLE_DIR="../src/main/scala/cc/factorie/example/"
+
+mkdir -p $OUTPUT_DIR
+
+find $EXAMPLE_DIR -name "Tutorial*" -type f | xargs -n 1 ./htmlify.sh
+
+mv *.html $OUTPUT_DIR/
+mv *.md $OUTPUT_DIR/
