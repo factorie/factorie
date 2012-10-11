469d57818173fa3fb1291313a95579e9a4e45eff
Brian Martin <brianmartin@gmail.com>
2012-10-10 20:07:09 -0400

    Tut: initial htmlify.sh

---
+++ doc/htmlify.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TUT_FILE=$1
+MD_FILE=./`basename $1`.md
+
+rm $MD_FILE
+rm $MD_FILE.html
+touch $MD_FILE
+
+echo -e "\`\`\`scala\n" >> $MD_FILE
+
+cat $TUT_FILE | \
+sed -e 's/[[:space:]]*\/\*\*/```\
+/'  -e 's/[[:space:]]*\/\*/```\
+/'  -e 's/^[[:space:]]*\* \(.*\)/\1/' \
+    -e 's/[[:space:]]*\*\//\
+```scala\
+/'  -e 's/\s*```/\
+```/' >> $MD_FILE
+
+echo -e "\`\`\`\n" >> $MD_FILE
+
+pandoc $MD_FILE -o $MD_FILE.html -s --highlight-style=pygments
+
