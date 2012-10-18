635b8c3bad75e2ba0ddb7fc48b8be8177b2e0221
Brian Martin <brianmartin@gmail.com>
2012-10-18 14:40:12 -0400

    leave HashingBFVV.domain abstract

--- src/main/scala/cc/factorie/HashDomain.scala
+++ src/main/scala/cc/factorie/HashDomain.scala
@@ -7,8 +7,8 @@ import scala.collection.mutable.SynchronizedMap
 import cc.factorie.la.GrowableSparseBinaryTensor1
 
 /*
- * A CategoricalDomain which does not preserve the mapping from Category to Value,
- * intead using the hash of the category to index.
+ * A Domain which does not preserve the mapping from Category to Value,
+ * intead using the hash of C to index.
  * 
  * @author Brian Martin
  * @date 10/7/2012
@@ -24,7 +24,7 @@ class StringHashDomain(size: Int) extends HashDomain[String](size) {
 }
 
 abstract class HashingBinaryFeatureVectorVariable[C] extends DiscreteTensorVariable {
-  override def domain: HashDomain[C] = null
+  override def domain: HashDomain[C]
   def ++=(cs: Iterable[C]) = cs.map(c => tensor.update(domain.index(c), 1.0))
   def this(initVals:Iterable[C]) = { this(); initVals.map(c => domain.index(c)).foreach(this.tensor.+=(_, 1.0)) }
   set(new GrowableSparseBinaryTensor1(domain.dimensionDomain))(null)
