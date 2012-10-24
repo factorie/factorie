59b69267ed39ebcf2f5c0396b6cf4fecca5c7e85
Luke Vilnis <lvilnis@gmail.com>
2012-10-24 00:31:38 -0400

    Remove duplicate "blankCopy" to fix build

--- src/main/scala/cc/factorie/la/WeightsTensor.scala
+++ src/main/scala/cc/factorie/la/WeightsTensor.scala
@@ -59,7 +59,6 @@ class WeightsTensor(val newTensor:DotFamily=>Tensor = (df:DotFamily) => Tensor.n
   //override def toArray: Array[Double] = throw new Error("Cannot be implemented.")
   override def toArray: Array[Double] = { val a = new Array[Double](dim1); var offset = 0; _map.values.foreach(t => { System.arraycopy(t.asArray, 0, a, offset, t.length); offset += t.length }); a }
   override def copy: WeightsTensor = { val t = new WeightsTensor(newTensor); _map.keys.foreach(k => t(k) = apply(k).copy); t }
-  override def blankCopy: WeightsTensor = new WeightsTensor(newTensor)
   override def different(t:DoubleSeq, threshold:Double): Boolean = t match {
     case t:WeightsTensor => _map.keys.exists(k => _map(k).different(t._map(k), threshold))
   }
