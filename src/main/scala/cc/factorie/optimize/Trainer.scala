f99203235b936804d39a0a8f526860689cfb49f2
Luke Vilnis <lvilnis@gmail.com>
2012-11-09 14:14:55 -0500

    Small cosmetic ParallelBatchTrainer change

--- src/main/scala/cc/factorie/optimize/Trainer.scala
+++ src/main/scala/cc/factorie/optimize/Trainer.scala
@@ -60,10 +60,9 @@ class ParallelBatchTrainer[M<:Model](val model: M, val optimizer: GradientOptimi
     val gradientAccumulator = new ThreadLocal[LocalWeightsTensorAccumulator] { def initialValue = new LocalWeightsTensorAccumulator(model.newBlankWeightsTensor.asInstanceOf[WeightsTensor]) }
     val valueAccumulator = new ThreadLocal[LocalDoubleAccumulator] { def initialValue = new LocalDoubleAccumulator }
     examples.par.foreach(example => example.accumulateExampleInto(model, gradientAccumulator.get, valueAccumulator.get, null))
-    
-    val accumulatedGradient = gradientAccumulator.instances.drop(1).foldLeft(gradientAccumulator.instances.head){ case (prev, curr) => { prev.combine(curr); prev }}
-    val accumulatedValues = valueAccumulator.instances.drop(1).foldLeft(valueAccumulator.instances.head){ case (prev, curr) => { prev.combine(curr); prev }}
-    optimizer.step(model.weightsTensor, accumulatedGradient.tensor, accumulatedValues.value, Double.NaN)
+    val grad = gradientAccumulator.instances.reduce((l, r) => { l.combine(r); l }).tensor
+    val value = valueAccumulator.instances.reduce((l, r) => { l.combine(r); l }).value
+    optimizer.step(model.weightsTensor, grad, value, Double.NaN)
   }
   def isConverged = optimizer.isConverged
 }
