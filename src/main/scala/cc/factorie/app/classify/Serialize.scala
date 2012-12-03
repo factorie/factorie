d8ee64f68920323df6bc25ce6a53fff6fc578c02
Luke Vilnis <lvilnis@gmail.com>
2012-12-02 22:22:54 -0500

    Left out file in last commit

--- src/main/scala/cc/factorie/app/classify/Serialize.scala
+++ src/main/scala/cc/factorie/app/classify/Serialize.scala
@@ -2,53 +2,22 @@ package cc.factorie.app.classify
 
 import cc.factorie.app.classify._
 import java.io.{PrintWriter, BufferedReader, PrintStream}
-import cc.factorie.la.WeightsTensor
 import cc.factorie._
-import javax.management.remote.rmi._RMIConnection_Stub
-import java.util.zip.GZIPOutputStream
 import cc.factorie.util.Cubbie
 
-// TODO: use the builtin load and save for CategoricalDomain
-// instead of writing featurestring:value etc. just write the domain on the top line
-// and then number:number on every line. do same with classifier weights too
-
-// TODO: this doesn't work because in order for the Tensor to know how long it's supposed to be,
-// we need to have read the categories out first! -luke
-class ClassifierCubbie(cls: Classifier[Label]) extends Cubbie {
+class ModelBasedClassifierCubbie(cls: Classifier[Label]) extends Cubbie {
 //  val labelDomain = CubbieSlot[CategoricalDomainCubbie]("labelDomain", () => throw new Error)
-  val model = CubbieSlot[ModelCubbie]("model", () => throw new Error)
 //  labelDomain := new CategoricalDomainCubbie(cls.labelDomain.asInstanceOf[CategoricalDomain[String]])
-  model := new ModelCubbie(cls.asInstanceOf[ModelBasedClassifier[Label]].model)
-//  def fetch(cls: Classifier[Label]) = {
-//    labelDomain.value.fetch(cls.labelDomain.asInstanceOf[CategoricalDomain[String]])
-//    cls
-//  }
+  val model = CubbieSlot[ModelCubbie]("model", () => throw new Error)
+  model := new ModelCubbie  (cls.asInstanceOf[ModelBasedClassifier[Label]].model)
 }
 
 object Serialize {
-
-  implicit object ClassificationSerializer extends Serializer[Classification[Label]] {
-    def serialize(toSerialize: Classification[Label], str: PrintStream) = {
-      val writer = new PrintWriter(str)
-      writer.println(toSerialize.label)
-      writer.flush()
-    }
-    def deserialize(deserializeTo: Classification[Label], reader: BufferedReader) = sys.error("Can't deserialize classifications.")
+  def writeClassification(toSerialize: Classification[Label], str: PrintStream) = {
+    val writer = new PrintWriter(str)
+    writer.println(toSerialize.label)
+    writer.flush()
   }
-
-//  implicit object ClassifierSerializer extends Serializer[Classifier[Label]] {
-//    def serialize(toSerialize: Classifier[Label], str: PrintStream) = toSerialize match {
-//      case cls: ModelBasedClassifier[Label] =>
-//        Serializer.serialize(cls.labelDomain.asInstanceOf[CategoricalDomain[String]], str)
-//        Serializer.serialize(cls.model, str)
-//    }
-//    def deserialize(deserializeTo: Classifier[Label], str: BufferedReader) = deserializeTo match {
-//      case cls: ModelBasedClassifier[Label] =>
-//        Serializer.deserialize(cls.labelDomain.asInstanceOf[CategoricalDomain[String]], str)
-//        Serializer.deserialize(cls.model, str)
-//    }
-//  }
-
   def writeInstancesSVMLight(labels: Iterable[Label], out: PrintStream): Unit = {
     for (label <- labels) {
       val labelStr = new StringBuilder
@@ -61,7 +30,6 @@ object Serialize {
       out.append(labelStr)
     }
   }
-
   def readInstancesSVMLight(instancesString: String): LabelList[Label, Features] = {
     object FeaturesDomain extends CategoricalTensorDomain[String]
     object LabelDomain extends CategoricalDomain[String]
