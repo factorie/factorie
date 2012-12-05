d8834d13c1615e876dff2e02bfbfcb8557999188
Ari Kobren <akobren@cs.umass.edu>
2012-12-05 10:35:03 -0500

    put UserReliability in HumanEditMention and changed HumanEditTemplate to a templateWithStatistics3

--- src/main/scala/cc/factorie/app/bib/HumanEdits.scala
+++ src/main/scala/cc/factorie/app/bib/HumanEdits.scala
@@ -4,42 +4,38 @@ import cc.factorie._
 import cc.factorie.app.nlp.coref._
 
 /* Keep track of one of these per user */
-class UserReliabilityVariable extends RealVariable with EntityAttr {
+class UserReliabilityVariable extends RealVariable {
   var totalImpactfulEdits = 0.0
   var totalEdits = 0.0
-  def updateValue(d:DiffLis) = this.set(totalImpactfulEdits/totalEdits)(d)
+  def updateValue(d:DiffList) = this.set(totalImpactfulEdits/totalEdits)(d)
 }
 
 trait HumanEditMention extends Attr{
   def editSet = attr[EditSetVariable]
   //var linkSet
   var editType:String = "none"
-  attr += new EditSetVariable
+//  attr += new EditSetVariable(entity)
+  attr += new UserReliabilityVariable
 }
 
 class EditSetVariable(val entity: Entity) extends SetVariable[HierEntity with HumanEditMention] with EntityAttr
 
-class HumanEditTemplate extends TupleTemplateWithStatistics4[EntityExists, IsEntity, EditSetVariable,UserReliabilityVariable] {
+class HumanEditTemplate extends TupleTemplateWithStatistics3[EntityExists, IsEntity, EditSetVariable] {
 
   def unroll1(eExists: EntityExists) = Factor(eExists,
-    eExists.entity.attr[IsEntity], eExists.entity.attr[EditSetVariable],
-    eExists.entity.attr[UserReliabilityVariable])
+    eExists.entity.attr[IsEntity], eExists.entity.attr[EditSetVariable])
 
   def unroll2(isEntity: IsEntity) = Factor(isEntity.entity.attr[EntityExists],
-    isEntity, isEntity.entity.attr[EditSetVariable],
-    isEntity.entity.attr[UserReliabilityVariable])
+    isEntity, isEntity.entity.attr[EditSetVariable])
 
   def unroll3(editSetVar: EditSetVariable) = Factor(
-    editSetVar.entity.attr[EntityExists], editSetVar.entity.attr[IsEntity]
-    editSetVar, editSetVar.entity.attr[UserReliabilityVariable])
-
-  def unroll4(reliability: UserReliabilityVariable) = Factor(
-    reliability.entity.attr[EntityExists], reliability.entity.attr[IsEntity],
-    reliability.entity.attr[EditSetVar], reliability)
+    editSetVar.entity.attr[EntityExists], editSetVar.entity.attr[IsEntity],
+    editSetVar)
 
   def score(eExists: EntityExists#Value, isEntity: IsEntity#Value, editSetVar:
-  EditSetVariable#Value, reliability: UserReliabilityVariable#Value):
-  Double = {
+  EditSetVariable#Value): Double = {
+
+    def scoreEdit(entity: HierEntity) = 1
 
     var result = 0.0
     for (edit <- editSetVar) {
