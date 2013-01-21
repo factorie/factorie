6476889965f54f70b5388ddf2db6473e2a2fc3ad
Luke Vilnis <lvilnis@gmail.com>
2013-01-21 16:02:53 -0500

    add support for k-step CD

--- src/main/scala/cc/factorie/optimize/ContrastiveDivergence.scala
+++ src/main/scala/cc/factorie/optimize/ContrastiveDivergence.scala
@@ -4,19 +4,19 @@ import cc.factorie._
 import cc.factorie.la.WeightsTensorAccumulator
 import cc.factorie.util.DoubleAccumulator
 
-class ContrastiveDivergenceExample[C](val context: C, val sampler: ProposalSampler[C]) extends Example[Model] {
+class ContrastiveDivergenceExample[C](val context: C, val sampler: Sampler[C], val k: Int = 1) extends Example[Model] {
   // NOTE: this assumes that variables are set to the ground truth when this method is called
   def accumulateExampleInto(model: Model, gradient: WeightsTensorAccumulator, value: DoubleAccumulator, margin: DoubleAccumulator): Unit = {
     require(gradient != null, "The ContrastiveDivergenceExample needs a gradient accumulator")
-    val proposal = sampler.pickProposal(sampler.proposals(context))
-    proposal.diff.redo
-    model.factorsOfFamilyClass[DotFamily](proposal.diff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
-    proposal.diff.undo
-    model.factorsOfFamilyClass[DotFamily](proposal.diff).foreach(f => gradient.accumulate(f.family, f.currentStatistics))
+    val proposalDiff = new DiffList
+    repeat(k) { proposalDiff ++= sampler.process(context) }
+    model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
+    proposalDiff.undo
+    model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics))
   }
 }
 
-class PersistentContrastiveDivergenceExample[C <: LabeledMutableVar[_]](val context: C, val sampler: ProposalSampler[Var]) extends Example[Model] {
+class PersistentContrastiveDivergenceExample[C <: LabeledMutableVar[_]](val context: C, val sampler: Sampler[Var]) extends Example[Model] {
   // NOTE: this assumes that the initial configuration is the ground truth
   def accumulateExampleInto(model: Model, gradient: WeightsTensorAccumulator, value: DoubleAccumulator, margin: DoubleAccumulator): Unit = {
     require(gradient != null, "The PersistentContrastiveDivergenceExample needs a gradient accumulator")
@@ -24,36 +24,38 @@ class PersistentContrastiveDivergenceExample[C <: LabeledMutableVar[_]](val cont
     context.setToTarget(groundTruthDiff)
     model.factorsOfFamilyClass[DotFamily](groundTruthDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics))
     groundTruthDiff.undo
-    val proposalDiff = sampler.processProposals(sampler.proposals(context))
+    val proposalDiff = sampler.process(context)
     model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
   }
 }
 
 class ContrastiveDivergenceHingeExample[C <: Var](
-  val context: C, val sampler: ProposalSampler[C], val learningMargin: Double = 1.0) extends Example[Model] {
+  val context: C, val sampler: Sampler[C], val learningMargin: Double = 1.0, val k: Int = 1) extends Example[Model] {
   // NOTE: this assumes that variables are set to the ground truth when this method is called
   def accumulateExampleInto(model: Model, gradient: WeightsTensorAccumulator, value: DoubleAccumulator, margin: DoubleAccumulator): Unit = {
     require(gradient != null, "The ContrastiveDivergenceHingeExample needs a gradient accumulator")
     require(margin != null, "The ContrastiveDivergenceHingeExample needs a margin accumulator")
-    val proposal = sampler.pickProposal(sampler.proposals(context))
-    if (-proposal.modelScore < learningMargin) {
-      proposal.diff.redo
-      model.factorsOfFamilyClass[DotFamily](proposal.diff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
-      proposal.diff.undo
-      model.factorsOfFamilyClass[DotFamily](proposal.diff).foreach(f => gradient.accumulate(f.family, f.currentStatistics))
-      margin.accumulate(-proposal.modelScore)
-      println("margin: %f" format (-proposal.modelScore))
-    }
+    val truthScore = model.currentScore(context)
+    val proposalDiff = new DiffList
+    repeat(k) { proposalDiff ++= sampler.process(context) }
+    val proposalScore = model.currentScore(context)
+    if (truthScore - proposalScore < learningMargin) {
+      model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
+      proposalDiff.undo
+      model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics))
+      margin.accumulate(truthScore - proposalScore)
+    } else
+      proposalDiff.undo
   }
 }
 
 class PersistentContrastiveDivergenceHingeExample[C <: LabeledMutableVar[_]](
-  val context: C, val sampler: ProposalSampler[Var], val learningMargin: Double = 1.0) extends Example[Model] {
+  val context: C, val sampler: Sampler[Var], val learningMargin: Double = 1.0) extends Example[Model] {
   // NOTE: this assumes that the initial configuration is the ground truth
   def accumulateExampleInto(model: Model, gradient: WeightsTensorAccumulator, value: DoubleAccumulator, margin: DoubleAccumulator): Unit = {
     require(gradient != null, "The PersistentContrastiveDivergenceHingeExample needs a gradient accumulator")
     require(margin != null, "The PersistentContrastiveDivergenceHingeExample needs a margin accumulator")
-    val proposalDiff = sampler.processProposals(sampler.proposals(context))
+    val proposalDiff = sampler.process(context)
     val currentConfigScore = model.currentScore(context)
     val groundTruthDiff = new DiffList
     context.setToTarget(groundTruthDiff)
@@ -63,7 +65,6 @@ class PersistentContrastiveDivergenceHingeExample[C <: LabeledMutableVar[_]](
       groundTruthDiff.undo
       model.factorsOfFamilyClass[DotFamily](proposalDiff).foreach(f => gradient.accumulate(f.family, f.currentStatistics, -1.0))
       margin.accumulate(truthScore - currentConfigScore)
-      println("margin: %f" format (truthScore - currentConfigScore))
     } else
       groundTruthDiff.undo
   }
