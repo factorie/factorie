baeff1b7df29190e1e404eabc84cf285ba2455a2
Andrew McCallum <mccallum@cs.umass.edu>
2013-05-12 21:22:27 -0400

    Rename file DocumentProcessor to DocumentAnnotator, reflecting the new class name.

--- src/main/scala/cc/factorie/app/nlp/DocumentAnnotator.scala
+++ src/main/scala/cc/factorie/app/nlp/DocumentAnnotator.scala
@@ -15,52 +15,6 @@
 package cc.factorie.app.nlp
 import cc.factorie._
 
-///** Performs some NLP on a Document and returns a Document (perhaps the same one) containing the new annotations. */
-//trait DocumentProcessor {
-//  // NOTE: this method may mutate and return the same document that was passed in
-//  def process1(document: Document): Document
-//  def prereqAttrs: Iterable[Class[_]]
-//  def postAttrs: Iterable[Class[_]]
-//  
-//  def process(document: Document): Document = process(document, defaultProcessorMap)
-//  def process(document: Document, processorMap: DocumentProcessorMap): Document = {
-//    preProcess(document, processorMap)
-//    val doc = process1(document)
-//    postAttrs.foreach(p => document.documentProcessors(p) = this) // record which attributes this processor added
-//    //println("DocumentProcessor.process adding attrs "+postAttrs.mkString(" "))
-//    doc
-//  }
-//  
-//  /** How the annotation of this DocumentProcessor should be printed in one-word-per-line (OWPL) format.
-//      If there is no per-token annotation, return null.  Used in Document.owplString. */
-//  def tokenAnnotationString(token:Token): String = null
-//  
-//  def defaultProcessorMap: DocumentProcessorMap = DefaultDocumentProcessorMap
-//  
-//  def preProcess(doc: Document, processorMap: DocumentProcessorMap): Unit = {
-//    // TODO This "contains" is a bug; it should instead mirror the same "subclass lookup" used by attr.apply
-//    for (prereq <- prereqAttrs) if (!doc.documentProcessors.contains(prereq.asInstanceOf[Class[AnyRef]])) { // TODO Why is this cast necessary?
-//      //println("DocumentProcessor.preProcess needing to add "+prereq)
-//      val processor = processorMap(prereq)
-//      // Make sure we won't over-write some pre-existing annotation
-//      for (a <- processor.postAttrs) 
-//        if (doc.documentProcessors.contains(a)) throw new Error(getClass.toString+": annotation collision conflict: "+doc.documentProcessors(a)+" already added attr "+a)
-//      processor.process(doc, processorMap)
-//    }
-//  }
-//}
-//
-//class DocumentProcessorMap extends scala.collection.mutable.HashMap[Class[_],DocumentProcessor]
-//
-//object DefaultDocumentProcessorMap extends DocumentProcessorMap {
-//  import cc.factorie.app.nlp._
-//  //this.update(classOf[pos.PTBPosLabel], new pos.POS3) // TODO where should this find its parameters?
-//  this.update(classOf[lemma.SimplifyDigitsTokenLemma], new lemma.SimplifyDigitsLemmatizer)
-//  this.update(classOf[Token], cc.factorie.app.nlp.segment.ClearTokenizer)
-//  this.update(classOf[Sentence], cc.factorie.app.nlp.segment.ClearSegmenter)
-//}
-
-
 trait DocumentAnnotator {
   def process1(document: Document): Document  // NOTE: this method may mutate and return the same document that was passed in
   def prereqAttrs: Iterable[Class[_]]
