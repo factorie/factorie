41890f8982ae5fdfbc07632a3e605340de0e2da1
Andrew McCallum <mccallum@cs.umass.edu>
2013-05-15 11:25:29 -0400

    Fix LoadOntonotes5 to work with real Ontonotes data, not Jinho WSJ data in similar format.

--- src/main/scala/cc/factorie/app/nlp/LoadOntonotes5.scala
+++ src/main/scala/cc/factorie/app/nlp/LoadOntonotes5.scala
@@ -3,28 +3,42 @@ package cc.factorie.app.nlp
 import scala.io.Source
 import cc.factorie.app.nlp._
 import cc.factorie.app.nlp.pos.PTBPosLabel
+import cc.factorie.app.nlp.ner.BioOntonotesNerLabel
 import cc.factorie.app.nlp.parse.ParseTree
 import cc.factorie.app.nlp.lemma.TokenLemma
 
 import java.io.PrintWriter
 
-/*
- * Loader for the OntoNotes 5 data
- * tokenIndex word lemma POS semantic parentIndex depLabel
- * @author Brian Martin
+/* Loader for the OntoNotes 5 data
+   @author Brian Martin, Andrew McCallum
+  1   token ID
+  2   word form
+  3   gold lemma
+  4   auto lemma
+  5   gold POS tag
+  6   auto POS tag
+  7   gold feats
+  8   gold head ID
+  9   auto head ID
+  10  gold dependency label
+  11  auto dependency label
+  12  gold secondary dependencies
+  13  gold semantic arguments
+  14  gold named entity tags
+  15  gold coreference
+  
  */
 
 object LoadOntonotes5 {
   private def addDepInfo(s: Sentence, depInfoSeq: Seq[(Int,Int,String)]): Unit = {
-    val tree = new ParseTree(s)
-    for ((childIdx, parentIdx, depLabel) <- depInfoSeq) {
-      tree.setParent(childIdx, parentIdx)
-      tree.label(childIdx).setCategory(depLabel)(null)
-    }
+    //assert(depInfoSeq.map(_._1) == Seq.tabulate(depInfoSeq.length)(i => i), "Token indices: "+depInfoSeq.map(_._1).mkString(" ")) // Assert that we have an entry for each token index, in order
+    val tree = new ParseTree(s, depInfoSeq.map(_._2), depInfoSeq.map(_._3))
     s.attr += tree
   }
 
   var loadLemma = true
+  var loadPos = true
+  var loadNer = true
 
   def fromFilename(filename:String): Seq[Document] = {
     var document: Document = new Document().setName("Ontonotes499" + filename)
@@ -45,15 +59,18 @@ object LoadOntonotes5 {
         val currTokenIdx = fields(0).toInt - 1
         val word = fields(1)
         val lemma = fields(2) // was 3
-        val partOfSpeech = fields(3) // was 5
-        val parentIdx = fields(5).toInt - 1 // was 8
-        val depLabel = fields(6) // was 9
-        document.appendString(" ")
-        val token = new Token(sentence, word)
-        token.attr += new PTBPosLabel(token, partOfSpeech)
-        if (loadLemma)
-          token.attr += new TokenLemma(token, lemma) // TODO Change this to some more specific TokenLemma subclass
-        depInfoSeq.append((currTokenIdx, parentIdx, depLabel))
+        val partOfSpeech = fields(4)
+        val parentIdx = fields(8).toInt - 1
+        val depLabel = fields(10)
+        var ner = fields(13); if (ner == "_") ner = "O"
+        if (partOfSpeech != "XX") { // Skip words marked with part-of-speech "XX"
+          document.appendString(" ")
+          val token = new Token(sentence, word)
+          if (loadPos) token.attr += new PTBPosLabel(token, partOfSpeech)
+          if (loadNer) token.attr += new BioOntonotesNerLabel(token, ner)
+          if (loadLemma) token.attr += new TokenLemma(token, lemma) // TODO Change this to some more specific TokenLemma subclass
+          depInfoSeq.append((currTokenIdx, parentIdx, depLabel))
+        }
       }
     }
     if (sentence ne null)
