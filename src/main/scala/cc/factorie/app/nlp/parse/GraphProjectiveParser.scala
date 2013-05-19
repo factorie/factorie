82aef96cf2e4f028b0b30b938f8dcb5ac3c1c6c8
Alexandre Passos <alexandre.tp@gmail.com>
2013-05-19 10:13:10 -0400

    nlp.parse: Adding a graph-based projective parser which actually works

---
+++ src/main/scala/cc/factorie/app/nlp/parse/GraphProjectiveParser.scala
@@ -0,0 +1,322 @@
+package cc.factorie.app.nlp.parse
+
+import cc.factorie.app.nlp._
+import cc.factorie._
+import cc.factorie.app.nlp.pos.PTBPosLabel
+import cc.factorie.la.{Tensor, TensorSetAccumulator}
+import cc.factorie.util.DoubleAccumulator
+import scala.collection.mutable.ArrayBuffer
+import java.io.File
+
+/**
+ * User: apassos
+ * Date: 5/19/13
+ * Time: 9:44 AM
+ */
+class GraphProjectiveParser extends DocumentAnnotator {
+  parser =>
+
+  object MyFeaturesDomain extends DiscreteDomain(1e7.toInt) // 10 million features
+  class FeatureVector extends HashFeatureVectorVariable {
+    def domain = MyFeaturesDomain
+  }
+
+  def getTokenFeatureVector(t: Token): TensorVar = {
+    val f = new FeatureVector
+    val tWord = t.string
+    val tPos = t.attr[PTBPosLabel].categoryValue
+    f += "TOKENPOS="+tPos
+    f += "TOKENWORD="+tWord
+    f += "TOKENID="+tPos+"&"+tWord
+    f
+  }
+
+  def getParentFeatureVector(p: Token): TensorVar = {
+    val f = new FeatureVector
+    val pWord = if (p ne null) p.string else "ROOT"
+    val pPos = if (p ne null) p.attr[PTBPosLabel].categoryValue else "ROOTPOS"
+    f += "PARENTPOS="+pPos
+    f += "PARENTWORD="+pWord
+    f += "PARENTID="+pPos+"&"+pWord
+    assert(f ne null)
+    f
+  }
+
+  def getPairwiseFeatureVector(t: Token, p: Token): TensorVar = {
+    val f = new FeatureVector
+    val tWord = t.string
+    val tPos = t.attr[PTBPosLabel].categoryValue
+    val pWord = if (p ne null) p.string else "ROOT"
+    val pPos = if (p ne null) p.attr[PTBPosLabel].categoryValue else "ROOTPOS"
+    f += "WORDPAIR="+tWord+"&"+pWord
+    f += "POSPAIR="+tPos+"&"+pPos
+    f += "PARENTPAIRCHILDPOS="+pPos+"&"+pWord+"&"+tPos
+    f += "PARENTPAIRCHILDWORD="+pPos+"&"+pWord+"&"+tWord
+    f += "CHILDPAIRPARENTPOS="+tPos+"&"+tWord+"&"+pPos
+    f += "CHILDPAIRPARENTWORD="+tPos+"&"+tWord+"&"+pWord
+    f += "JOINTALL="+tPos+"&"+tWord+"&"+pWord+"&"+pPos
+    if ((p ne null) && (t ne null)) {
+      val first = if (p.sentencePosition < t.sentencePosition) p else t
+      val second = if (p.sentencePosition < t.sentencePosition) t else p
+      var x = first
+      while (x.sentenceNext ne second) {
+        x = x.sentenceNext
+        // f += "BETWEENPOS="+pPos+"&"+x.attr[PosLabel].categoryValue+"&"+tPos
+      }
+      val prevHeadPos = if (p.sentenceHasPrev) p.sentencePrev.attr[PTBPosLabel].categoryValue else "NOPREV"
+      val prevTokPos = if (t.sentenceHasPrev) t.sentencePrev.attr[PTBPosLabel].categoryValue else "NOPREV"
+      val nextHeadPos = if (p.sentenceHasNext) p.sentenceNext.attr[PTBPosLabel].categoryValue else "NONEXT"
+      val nextTokPos = if (t.sentenceHasNext) t.sentenceNext.attr[PTBPosLabel].categoryValue else "NONEXT"
+      f += "HNhPcC="+pPos+"&"+nextHeadPos+"&"+prevTokPos+"&"+tPos
+      f += "PhHPcC="+prevHeadPos+"&"+pPos+"&"+prevTokPos+"&"+tPos
+      f += "HNhCNc="+pPos+"&"+nextHeadPos+"&"+tPos+"&"+nextTokPos
+      f += "PhHCNc="+prevHeadPos+"&"+pPos+"&"+tPos+"&"+nextTokPos
+      val distance = math.abs(t.sentencePosition - p.sentencePosition)
+      for (i <- 0 to distance) {
+        // f += "EdgeLength>="+i
+      }
+      val normDistance = distance*10/(t.sentence.length)
+      for (i <- 0 to normDistance) {
+        // f += "NormDistance>="+i
+      }
+    }
+    f
+  }
+
+  def groundTruthEdges(s: Sentence): Seq[TensorVar] = {
+    s.attr[ParseTree]._targetParents.zipWithIndex.flatMap(i => {
+      val parentToken = if (i._1 < 0) null else s.tokens(i._1)
+      val childToken = s.tokens(i._2)
+      Seq(getTokenFeatureVector(childToken), getParentFeatureVector(parentToken), getPairwiseFeatureVector(childToken, parentToken))
+    })
+  }
+  object DependencyModel extends WeightsDef {
+    val weights = Weights(new cc.factorie.la.DenseTensor1(MyFeaturesDomain.dimensionSize))
+  }
+
+  class ProjectiveParser(val weights: Tensor, val sent: Sentence, val negativeExamples: Seq[(Int,Int)]) {
+    val knownParents = ArrayBuffer[Int]()
+    val sentLength = sent.length
+    (0 until sentLength).foreach(e => knownParents.append(-3))
+    negativeExamples.foreach(e => knownParents(e._2) = e._1)
+    val tokenScores = Array.fill(sent.length+1)(0.0)
+    val parentScores = Array.fill(sent.length+1)(0.0)
+    parentScores(0) = getParentFeatureVector(null).value.dot(weights)
+    for (i <- 0 until sentLength) {
+      tokenScores(i+1) = getTokenFeatureVector(sent.tokens(i)).value.dot(weights)
+      parentScores(i+1) = getParentFeatureVector(sent.tokens(i)).value.dot(weights)
+    }
+    val edgeScores = Array.fill(sent.length+1,sent.length+1)(Double.NaN)
+    def getEdgeScore(parent: Int, child: Int) : Double = {
+      assert(parent != child, "can't add an edge from a token to itself")
+      if (edgeScores(parent)(child).isNaN) {
+        val loss = if ((child > 0) && (knownParents(child-1) == parent -1)) -1.0 else 0.0
+        edgeScores(parent)(child) = (if (child > 0)
+          loss + getPairwiseFeatureVector(sent.tokens(child-1), if (parent > 0) sent.tokens(parent-1) else null).value.dot(weights) + tokenScores(child) + parentScores(parent)
+        else 0)
+      }
+      edgeScores(parent)(child)
+    }
+
+
+    class SpanData(val split: Int, val score: Double)
+    case class LeftComplete(override val split: Int, override val score: Double) extends SpanData(split, score)
+    case class LeftIncomplete(override val split: Int, override val score: Double) extends SpanData(split, score)
+    case class RightComplete(override val split: Int, override val score: Double) extends SpanData(split, score)
+    case class RightIncomplete(override val split: Int, override val score: Double) extends SpanData(split, score)
+
+    def backwardSearch(left: Int, right: Int, d: SpanData, edges: ArrayBuffer[(Int, Int)]) {
+      if (left >= right) return
+      d match {
+        case d: LeftComplete =>
+          val node = lcTable(left)(right)
+          // println("searching lc " + left + " " + right)
+          backwardSearch(left, node.split, LeftIncomplete(0, 0), edges)
+          backwardSearch(node.split, right, LeftComplete(0, 0), edges)
+        case d: RightComplete =>
+          val node = rcTable(left)(right)
+          // println("searching rc " + left + " " + right)
+          backwardSearch(left, node.split, RightComplete(0, 0), edges)
+          backwardSearch(node.split, right, RightIncomplete(0, 0), edges)
+        case d: LeftIncomplete =>
+          val node = liTable(left)(right)
+          // println("searching li " + left + " " + right)
+          edges.append((left, right))
+          backwardSearch(left, node.split, LeftComplete(0, 0), edges)
+          backwardSearch(node.split+1, right, RightComplete(0, 0), edges)
+        case d: RightIncomplete =>
+          // println("searching ri " + left + " " + right)
+          val node = riTable(left)(right)
+          edges.append((right, left))
+          backwardSearch(left, node.split, LeftComplete(0, 0), edges)
+          backwardSearch(node.split+1, right, RightComplete(0, 0), edges)
+      }
+    }
+
+    val lcTable = Array.fill[LeftComplete](sentLength+1, sentLength+1)(null)
+    val liTable = Array.fill[LeftIncomplete](sentLength+1, sentLength+1)(null)
+    val rcTable = Array.fill[RightComplete](sentLength+1, sentLength+1)(null)
+    val riTable = Array.fill[RightIncomplete](sentLength+1, sentLength+1)(null)
+
+    def parse() = {
+      for (i <- 0 until sentLength+1) {
+        lcTable(i)(i) = LeftComplete(i, 0)
+        liTable(i)(i) = LeftIncomplete(i, 0)
+        rcTable(i)(i) = RightComplete(i, 0)
+        riTable(i)(i) = RightIncomplete(i, 0)
+      }
+
+      for (height <- 1 until sentLength+1) {
+        for (left <- 0 until sentLength+1 - height) {
+          // here we'll fill the table cell left,left+height
+          val right = left+height
+          var bestLI = null.asInstanceOf[LeftIncomplete]
+          var bestRI = null.asInstanceOf[RightIncomplete]
+          for (split <- left until left+height) {
+            val li = LeftIncomplete(split, lcTable(left)(split).score + rcTable(split+1)(right).score)
+            bestLI = if ((bestLI eq null) || (li.score > bestLI.score)) li else bestLI
+            val ri = RightIncomplete(split, lcTable(left)(split).score + rcTable(split+1)(right).score)
+            bestRI = if ((bestRI eq null) || (ri.score > bestRI.score)) ri else bestRI
+          }
+          liTable(left)(right) = LeftIncomplete(bestLI.split, bestLI.score + getEdgeScore(left, right))
+          riTable(left)(right) = RightIncomplete(bestRI.split, bestRI.score + getEdgeScore(right, left))
+
+          var bestLC = null.asInstanceOf[LeftComplete]
+          for (split <- left+1 to left+height) {
+            // println(left + " " + split + " " + (left+height))
+            val lc = LeftComplete(split, liTable(left)(split).score + lcTable(split)(right).score)
+            bestLC = if ((bestLC eq null) || (lc.score > bestLC.score)) lc else bestLC
+          }
+          lcTable(left)(right) = bestLC
+
+          var bestRC = null.asInstanceOf[RightComplete]
+          for (split <- left until left+height) {
+            val rc = RightComplete(split, rcTable(left)(split).score + riTable(split)(right).score)
+            bestRC = if ((bestRC eq null) || (rc.score > bestRC.score)) rc else bestRC
+          }
+          rcTable(left)(right) = bestRC
+        }
+      }
+      // because node 0 in the graph represents the root, we're really interested in the best
+      // left complete span that covers from the root to the end of the sentence
+      val finalParse = lcTable(0)(sentLength)
+      val score = finalParse.score
+
+      // now let's search for the right splits so we can get all edges in the tree
+      val edges = ArrayBuffer[(Int, Int)]()
+      backwardSearch(0, sentLength, finalParse, edges)
+      val parents = Array.ofDim[Int](sentLength)
+      var sum = 0.0
+      edges.foreach(e => {
+        sum += getEdgeScore(e._1, e._2)
+        assert(parents(e._2-1) == 0)
+        assert(e._2 != 0)
+        parents(e._2-1) = e._1-1
+      })
+      assert(math.abs(score - sum) < 0.01*math.abs(score) + 0.0001, "edge scores should match: " + score + " " + sum)
+      (parents, score)
+    }
+  }
+
+  class StructuredPerceptronParsingExample(val sent: Sentence) extends cc.factorie.optimize.Example {
+    def accumulateExampleInto(gradient: TensorSetAccumulator, value: DoubleAccumulator) {
+      if (gradient ne null) {
+        val gt = groundTruthEdges(sent)
+        gt.foreach(f => gradient.accumulate(DependencyModel.weights, f.value))
+
+        val projectiveParser = new ProjectiveParser(DependencyModel.weights.value,
+          sent,
+          sent.attr[ParseTree].targetParents.zipWithIndex)
+        val (parents, score) = projectiveParser.parse()
+        for (i <- 0 until sent.tokens.length) {
+          gradient.accumulate(DependencyModel.weights,
+            getPairwiseFeatureVector(sent.tokens(i), if (parents(i) == -1) null else sent.tokens(parents(i))).value,
+            -1.0)
+          gradient.accumulate(DependencyModel.weights,
+            getTokenFeatureVector(sent.tokens(i)).value,
+            -1.0)
+          gradient.accumulate(DependencyModel.weights,
+            getParentFeatureVector(if (parents(i) == -1) null else sent.tokens(parents(i))).value,
+            -1.0)
+          if (value ne null) {
+            val trueParent = sent.attr[ParseTree].parents(i)
+            val returned = parents(i)
+            value.accumulate(math.min(0, projectiveParser.getEdgeScore(trueParent+1,i+1) - projectiveParser.getEdgeScore(returned+1, i+1)))
+          }
+        }
+      }
+    }
+  }
+
+  def serialize(filename: String) = {
+    val file = new File(filename); if (file.getParentFile != null && !file.getParentFile.exists) file.getParentFile.mkdirs()
+    cc.factorie.util.BinarySerializer.serialize(DependencyModel, file)
+  }
+  def deserialize(file: String) = cc.factorie.util.BinarySerializer.deserialize(DependencyModel, new File(file))
+
+  def train(trainSentences: Seq[Sentence], testSentences: Seq[Sentence], file: String, nThreads: Int) {
+    val examples = trainSentences.map(new StructuredPerceptronParsingExample(_))
+    val rng = new scala.util.Random(0)
+    val opt = new cc.factorie.optimize.DualAveragingOptimizer(1.0, 0.0, 0.001, 0.001)
+    val trainer = new optimize.HogwildTrainer(DependencyModel.weightsSet, opt, maxIterations = 10, nThreads = nThreads)
+    for (iteration <- 0 until 10) {
+      trainer.processExamples(rng.shuffle(examples))
+      import DepParser1.uas
+      println("Predicting train set..."); trainSentences.par.foreach { s => parse(s) } // Was par
+      println("Predicting test set...");  testSentences.par.foreach { s => parse(s) } // Was par
+      println("Training UAS = "+ uas(trainSentences.toSeq))
+      println(" Testing UAS = "+ uas(testSentences.toSeq))
+      println()
+      println("Saving model...")
+      serialize(file + "-iter-"+iteration)
+    }
+    println("Finished training.")
+    opt.setToDense(DependencyModel.weightsSet)
+  }
+
+  def parse(sent: Sentence) {
+    val tree = sent.attr.getOrElseUpdate(new ParseTree(sent))
+    val parser = new ProjectiveParser(DependencyModel.weights.value, sent, Seq())
+    val (parents, _) = parser.parse()
+    for (i <- 0 until sent.length) tree.setParent(i, parents(i))
+  }
+
+  def process1(document: Document) = {
+    document.sentences.foreach(parse)
+    document
+  }
+  def prereqAttrs: Iterable[Class[_]] = List(classOf[Sentence], classOf[pos.PTBPosLabel]) // TODO Also TokenLemma?  But we don't have a lemmatizer that matches the training data
+  def postAttrs: Iterable[Class[_]] = List(classOf[ParseTree])
+}
+
+object GraphProjectiveParser {
+  import DepParser1.uas
+  def main(args: Array[String]): Unit = {
+    object opts extends cc.factorie.util.DefaultCmdOptions {
+      val trainFile = new CmdOption("train", "", "FILES", "CoNLL-2008 train file.")
+      //val devFile =   new CmdOption("dev", "", "FILES", "CoNLL-2008 dev file")
+      val testFile =  new CmdOption("test", "", "FILES", "CoNLL-2008 test file.")
+      val model      = new CmdOption("model", "parser-model", "FILE", "File in which to save the trained model.")
+      val nThreads   = new CmdOption("nThreads", 10, "INT", "Number of threads to use.")
+    }
+    opts.parse(args)
+
+    val parser = new GraphProjectiveParser
+
+    val trainDoc = LoadOntonotes5.fromFilename(opts.trainFile.value).head
+    val testDoc = LoadOntonotes5.fromFilename(opts.testFile.value).head
+
+    // Train
+    parser.train(trainDoc.sentences, testDoc.sentences, opts.model.value, math.min(opts.nThreads.value, Runtime.getRuntime.availableProcessors()))
+    // Test
+
+    // Print accuracy diagnostics
+    println("Predicting train set..."); parser.process1(trainDoc)
+    println("Predicting test set...");  parser.process1(testDoc)
+    println("Training UAS = "+ uas(trainDoc.sentences))
+    println(" Testing UAS = "+ uas(testDoc.sentences))
+    println()
+    println("Done.")
+  }
+
+}
