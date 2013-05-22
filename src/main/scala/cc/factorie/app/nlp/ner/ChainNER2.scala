d1065243533c4b173121559950d42a18e07c06ab
Alexandre Passos <alexandre.tp@gmail.com>
2013-05-22 19:17:14 -0400

    ner: not unfreezing the pos domains

--- src/main/scala/cc/factorie/app/nlp/ner/ChainNER2.scala
+++ src/main/scala/cc/factorie/app/nlp/ner/ChainNER2.scala
@@ -572,7 +572,6 @@ object ChainNer2 extends ChainNer2 {
       val twoagg = new CmdOption("twoagg", "Turn on second stage aggreggation features.")
     }
     opts.parse(args)
-    cc.factorie.app.nlp.pos.PTBPosDomain.unfreeze()
 
     val lexes = List("WikiArtWork.lst", "WikiArtWorkRedirects.lst", "WikiCompetitionsBattlesEvents.lst", "WikiCompetitionsBattlesEventsRedirects.lst", "WikiFilms.lst", "WikiFilmsRedirects.lst", "WikiLocations.lst", "WikiLocationsRedirects.lst", "WikiManMadeObjectNames.lst", "WikiManMadeObjectNamesRedirects.lst", "WikiOrganizations.lst", "WikiOrganizationsRedirects.lst", "WikiPeople.lst", "WikiPeopleRedirects.lst", "WikiSongs.lst", "WikiSongsRedirects.lst", "cardinalNumber.txt", "currencyFinal.txt", "known_corporations.lst", "known_country.lst", "known_jobs.lst", "known_name.lst", "known_names.big.lst", "known_nationalities.lst",  "known_state.lst", "known_title.lst", "measurments.txt", "ordinalNumber.txt", "temporal_words.txt")
 
