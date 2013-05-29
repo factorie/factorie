c68b2b89f52a480bcb0b001dad6bef16d969c8c3
belanger <devnull@localhost>
2013-05-29 14:56:34 -0400

    various updates to nlp.mentions

---
+++ src/main/scala/cc/factorie/app/nlp/mention/Mention.scala
@@ -0,0 +1,25 @@
+package cc.factorie.app.nlp.mention
+
+import collection.mutable.ArrayBuffer
+import cc.factorie.app.nlp.{Sentence, TokenSpan, Document}
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: belanger
+ * Date: 5/29/13
+ * Time: 2:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MentionList extends ArrayBuffer[Mention]
+
+object Mention{
+  def apply(doc: Document, start: Int, length: Int, headTokenIndex: Int) =  new Mention(new TokenSpan(doc,start,length),headTokenIndex)
+}
+
+case class Mention(span: TokenSpan, headTokenIndex: Int = -1, mentionType: String = null){
+  def document: Document = span.document
+  def start: Int = span.start
+  def length: Int = span.length
+  def sentence: Sentence = span.sentence
+}
--- src/main/scala/cc/factorie/app/nlp/mention/ParseBasedMention.scala
+++ src/main/scala/cc/factorie/app/nlp/mention/ParseBasedMention.scala
@@ -2,119 +2,131 @@ package cc.factorie.app.nlp.mention
 
 import scala.collection.mutable.HashSet
 import scala.collection.mutable.ArrayBuffer
-import cc.factorie.app.nlp.Token
+import cc.factorie.app.nlp._
 import cc.factorie.app.nlp.parse.ParseTree
-import cc.factorie.app.nlp.{Document, DocumentAnnotator}
 import cc.factorie.app.nlp.ner.NerSpan
+import cc.factorie.app.nlp.pos.PTBPosLabel
+import scala.Some
 
 object ParseBasedMentionFinding extends DocumentAnnotator {
-  
-  private case class DocMention(
-      doc: Document,
-      start: Int,
-      length: Int,
-      mentionType: String = "", // NAM = proper noun, NOM = common noun, PRO = pronoun
-      headTokenIdx: Int = -1
-  ){ def addAsSpan(): Unit = new NounMentionSpan(doc, start, length) }
-  
+
   private final val PERSONAL_PRONOUNS = Seq("PRP", "PRP$")
   private final val COMMON_NOUNS      = Seq("NN" , "NNS")
   private final val PROPER_NOUNS      = Seq("NNP", "NNPS")
-  
+  private final val ALL_NOUNS         = Seq("NN","NNS","NNP","NNPS","PRP","PRP$")
+
   private def isPersonalPronoun(t: Token) = PERSONAL_PRONOUNS exists(_ == t.posLabel.categoryValue.toUpperCase())
   private def isCommonNoun     (t: Token) = COMMON_NOUNS      exists(_ == t.posLabel.categoryValue.toUpperCase())
   private def isProperNoun     (t: Token) = PROPER_NOUNS      exists(_ == t.posLabel.categoryValue.toUpperCase())
-  
-  private def nerSpans(doc: Document): Seq[DocMention] = {
+  private def isNoun           (t: Token) = ALL_NOUNS         exists(_ == t.posLabel.categoryValue.toUpperCase())
+
+  private def nerSpans(doc: Document): Seq[Mention] = {
     (for (s <- doc.spansOfClass[NerSpan]) yield
-       DocMention(doc, s.start, s.length)
-    ).toSeq
+      Mention(doc, s.start, s.length,s.start - s.sentence.start  + s.length - 1) //this sets the head token idx to be the last token in the span  //todo: s.start is offset in document, right?
+      ).toSeq
   }
-  
+
   // [Assumes personal pronouns are single tokens.]
-  private def personalPronounSpans(doc: Document): Seq[DocMention] = (
-    for (s <- doc.sentences;
-         (t,i) <- s.tokens.zipWithIndex if isPersonalPronoun(t)) yield
-      DocMention(doc, s.start + i, 1)
-    ).toSeq
-    
-  private def getHeadTokenIdx(m: DocMention): Int = getHead(
-    m.doc.sentenceContaining(m.doc(m.start)).parse,
-    m.start until (m.start + m.length) // TODO: is the until correct here?
-  )
-  
-  private def getHead(parse: ParseTree, subtree: Seq[Int]): Int = {
+  private def personalPronounSpans(doc: Document): Seq[Mention] = {
+    (
+      for (s <- doc.sentences;
+           (t,i) <- s.tokens.zipWithIndex if isPersonalPronoun(t)) yield
+        Mention(doc, s.start + i, 1,s.start+i)
+      ).toSeq
+  }
+
+  private def getHeadTokenIdx(m: Mention): Int = {
+    getHead(
+      m.document.sentenceContaining(m.document(m.start)).parse,
+      m.start until (m.start + m.length) // TODO: is the until correct here?
+    )
+  }
+
+  def getHead(parse: ParseTree, subtree: Seq[Int]): Int = {
     var curr = subtree.head - parse.sentence.start
     while (parse.parentIndex(curr) > 0 && subtree.contains(parse.parentIndex(curr)))
       curr = parse.parentIndex(curr)
     curr + parse.sentence.start
   }
-    
-  private def nounPhraseSpans(doc: Document, nounFilter: Token => Boolean): Seq[DocMention] = (
+
+  final val copularVerbs = collection.immutable.HashSet[String]() ++ Seq("is","are","was","'m")
+
+  private def nounPhraseSpans(doc: Document, nounFilter: Token => Boolean): Seq[Mention] = (
+
     for (s <- doc.sentences;
-         included = new HashSet[Token]();
+         usedTokens = new HashSet[Token]();
          (t, si) <- s.tokens.zipWithIndex if nounFilter(t)) yield {
-      
+
       val di = s.start + si
-      
-      // this catches the strange case where t.sentencePosition is -1
-      //assert(t.sentencePosition == si, "%d %d, %d %s %s \n %s" format 
-      //  (si, t.sentencePosition, t.position, t, t.sentence, t.sentence.tokens.map(t => t.string -> t.sentencePosition -> t.position).mkString("\n")))
- 
+      //These two filtering rules are from 'Mention Detection: Heuristics for the OntoNotes annotations'
+      val prevTokenIsCopular = if(si > 0) copularVerbs.contains(s.tokens(si-1).string.toLowerCase) else false
+      val copularPhrase = if(s.parse.parentIndex(si)!= -1) prevTokenIsCopular && (s.parse.parent(si).attr[PTBPosLabel].value == "VB"  )  else false
+
+      val parentIsNoun = if(s.parse.parentIndex(si) == -1) false else isNoun(s.parse.parent(si))
+      val prevWordIsComma = if(si > 0) s.tokens(si -1).string == "," else false
+      val prevPhraseIsNP = if(si > 1) usedTokens.contains(s.tokens(si - 2)) else false
+      val apposition =  parentIsNoun && prevWordIsComma && prevPhraseIsNP
+
       // skip tokens that are already added as part of a larger subtree or aren't nouns
-      if (included.contains(t)) None
+      if (copularPhrase || apposition) None
       else {
         val subtree = (Seq(t) ++ s.parse.children(si)).sortBy(_.position)
+        usedTokens ++= subtree
         val (start, length) = subtree.size match {
           // leaf of the parse
-          case 0 => t.position -> 1
+          case 1 => t.position -> 1
           // non-leaf
           case _ => subtree.head.position -> (subtree.last.position - subtree.head.position + 1)
         }
-        
-        val res = Some(DocMention(doc, start, length))
-        
+
+        val res = Some(Mention(doc, start, length,si))
+
         res
       }
-      
+
     }).flatten.toSeq
-    
-  private def removeSmallerIfHeadWordEqual(doc: Document, mentions: Seq[DocMention]): Seq[DocMention] =
+
+
+
+  private def removeSmallerIfHeadWordEqual(doc: Document, mentions: Seq[Mention]): Seq[Mention] =
     mentions
-      .groupBy(_.headTokenIdx)
+      .groupBy(_.headTokenIndex)
       .map { case (_, mentions) => mentions.maxBy(_.length) }
       .toSeq
-        
-  private def dedup(mentions: Seq[DocMention]): Seq[DocMention] = {
+
+  private def dedup(mentions: Seq[Mention]): Seq[Mention] = {
     // Note: equality is only in the first set of arguments for case classes
-    case class MentionStartLength(start: Int, length: Int)(val mention: DocMention) { 
-      def this(mention: DocMention) = this(mention.start, mention.length)(mention)
+    case class MentionStartLength(start: Int, length: Int)(val mention: Mention) {
+      def this(mention: Mention) = this(mention.start, mention.length)(mention)
     }
-    
+
     (for (m <- mentions) yield new MentionStartLength(m))
       .toSet
       .map { m: MentionStartLength => m.mention }
       .toSeq
   }
-    
+
+
   def process1(doc: Document): Document = {
-    
-    var docMentions = new ArrayBuffer[DocMention]
+
+    var docMentions = new ArrayBuffer[Mention]
     // NAM = proper noun, NOM = common noun, PRO = pronoun
     docMentions ++= nerSpans(doc)                       map { m => m.copy(mentionType = "NAM")}
     docMentions ++= personalPronounSpans(doc)           map { m => m.copy(mentionType = "PRO")}
     docMentions ++= nounPhraseSpans(doc, isCommonNoun)  map { m => m.copy(mentionType = "NOM")}
     docMentions ++= nounPhraseSpans(doc, isProperNoun)  map { m => m.copy(mentionType = "NAM")}
-    
-    docMentions = docMentions map { m => m.copy(headTokenIdx = getHeadTokenIdx(m)) }
-    
-    removeSmallerIfHeadWordEqual(doc, dedup(docMentions)).map(_.addAsSpan())
-    
+
+    docMentions = docMentions map { m => m.copy(headTokenIndex = getHeadTokenIdx(m)) } //todo: fix this
+
+    doc.attr += (new MentionList() ++= removeSmallerIfHeadWordEqual(doc, dedup(docMentions)).toSeq)
+
     doc
   }
-  
+
   // TODO: to implement
   def prereqAttrs: Iterable[Class[_]] = Seq()
   def postAttrs: Iterable[Class[_]] = Seq() // TODO: what is this for?
-  
+
 }
+
+
