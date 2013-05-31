b783ea6dad54bf291e1bc68da93f5217f9eee315
Andrew McCallum <mccallum@cs.umass.edu>
2013-05-31 09:29:07 -0400

    Add method and TODO and comment about bug to Mention.

--- src/main/scala/cc/factorie/app/nlp/mention/Mention.scala
+++ src/main/scala/cc/factorie/app/nlp/mention/Mention.scala
@@ -1,7 +1,7 @@
 package cc.factorie.app.nlp.mention
 
 import collection.mutable.ArrayBuffer
-import cc.factorie.app.nlp.{Sentence, TokenSpan, Document, Section}
+import cc.factorie.app.nlp.{Sentence, TokenSpan, Document, Section, Token}
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,10 +17,16 @@ object Mention{
   def apply(sec: Section, start: Int, length: Int, headTokenIndex: Int) =  new Mention(new TokenSpan(sec, start, length),headTokenIndex)
 }
 
+// TODO I don't like the last two arguments of this constructor: -akm
+// The index should be in terms of the Span, not the Section
+// I'm not comfortable with mentionType as a String.  Too error prone.  I think it should be a Label.
 case class Mention(span: TokenSpan, headTokenIndex: Int = -1, mentionType: String = null) {
   def document: Document = span.document
+  def section: Section = span.section
+  /** The Token position in its Section. */
   def start: Int = span.start
+  /** The number of Tokens in the mention's span. */
   def length: Int = span.length
   def sentence: Sentence = span.sentence
-  def headToken = document.asSection.tokens(headTokenIndex)
+  def headToken: Token = document.asSection.tokens(headTokenIndex) // This is buggy.  If the Document has multiple Sections, this won't work. -akm
 }
