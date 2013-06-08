43388049302411ceea55f1efd8196a35f6cc138c
Andrew McCallum <mccallum@cs.umass.edu>
2013-06-08 15:10:33 -0400

    LoadPlainText: New facility for loading more than one Document from the same file, using documentSeparator regex.

--- src/main/scala/cc/factorie/app/nlp/LoadPlainText.scala
+++ src/main/scala/cc/factorie/app/nlp/LoadPlainText.scala
@@ -15,11 +15,31 @@
 package cc.factorie.app.nlp
 import java.io.File
 import cc.factorie.app.strings.StringSegmenter
+import scala.util.matching.Regex 
 
-case class LoadPlainText(annotator:DocumentAnnotator = NoopDocumentAnnotator, documentName: String = null) extends Load with LoadDirectory {
-  def fromSource(source:io.Source): Seq[Document] = Seq(annotator.process(new Document(source.mkString).setName(documentName)))
+
+/** Create Documents from plain text files.
+    By default create one Document per file.
+    To create multiple Documents from one file, set documentSeparator regex.  
+    If the regex specifics a group (via parenthesis) then the Document's name will be set to the match of this first group. */
+case class LoadPlainText(annotator:DocumentAnnotator = NoopDocumentAnnotator, documentName: String = null, documentSeparator:Regex = null) extends Load with LoadDirectory {
+  def fromSource(source:io.Source): Seq[Document] = {
+    val string = source.getLines.mkString
+    if (documentSeparator eq null) Seq(annotator.process(new Document(string).setName(documentName)))
+    else {
+      var docStart = 0
+      val matchIterator = documentSeparator.findAllIn(string).matchData
+      (for (sepMatch <- matchIterator if sepMatch.start != docStart) yield {
+        val doc = new Document(string.substring(docStart, sepMatch.start))
+        annotator.process(doc)
+        if (sepMatch.group(1) ne null) doc.setName(sepMatch.group(1))
+        docStart = sepMatch.end
+        doc
+      }).toIndexedSeq
+    }
+  }
   def fromDirectory(dir:File): Seq[Document] = (for (file <- files(dir)) yield fromFile(file)).flatten
-  def files(directory:File): Seq[File] = {
+  private def files(directory:File): Seq[File] = {
     if (!directory.exists) throw new Error("File "+directory+" does not exist")
     if (directory.isFile) return List(directory)
     val result = new scala.collection.mutable.ArrayBuffer[File]
@@ -31,45 +51,4 @@ case class LoadPlainText(annotator:DocumentAnnotator = NoopDocumentAnnotator, do
   }
 }
 
-object LoadPlainText extends LoadPlainText(annotator = NoopDocumentAnnotator, documentName = null)
-
-
-
-object OldLoadPlainText {
-  
-  def fromString(name: String, contents: String): Document = new Document(contents).setName(name)
-  
-  @deprecated("Use fromString(String,String), and do segmentation separately.")
-  def fromString(name: String, contents: String, segmentSentences: Boolean): Document =
-    fromString(name, contents, if (segmentSentences) cc.factorie.app.nlp.segment.ClearSegmenter else cc.factorie.app.nlp.segment.ClearTokenizer)
-
-  def fromString(name: String, contents: String, processor: DocumentAnnotator): Document =
-    processor.process(new Document(contents).setName(name))
-    
-  def fromStream(name:String, stream:java.io.InputStream, encoding:String): Document = 
-    new Document(scala.io.Source.fromInputStream(stream, encoding).mkString).setName(name)
-
-  def fromFile(file: File, segmentSentences: Boolean = false): Document = {
-    val string = scala.io.Source.fromFile(file).mkString
-    fromString(file.getCanonicalPath, string, segmentSentences)
-  }
-
-  def fromDirectory(dir:File, segmentSentences:Boolean): Seq[Document] = {
-    for (file <- files(dir)) yield fromFile(file, segmentSentences)
-  }
-  
-  // Recursively descend directory, returning a list of files.
-  // TODO This function should get moved into util/package.scala or somesuch.
-  def files(directory:File): Seq[File] = {
-    if (!directory.exists) throw new Error("File "+directory+" does not exist")
-    if (directory.isFile) return List(directory)
-    val result = new scala.collection.mutable.ArrayBuffer[File]
-    for (entry <- directory.listFiles) {
-      if (entry.isFile) result += entry
-      else if (entry.isDirectory) result ++= files(entry)
-    }
-    result
-  }
-
-  
-}
+object LoadPlainText extends LoadPlainText(annotator = NoopDocumentAnnotator, documentName = null, documentSeparator = null)
