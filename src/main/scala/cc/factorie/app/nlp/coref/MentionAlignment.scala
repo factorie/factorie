8e07ded7b1c5a1099656c4a424527820f8af258b
Alexandre Passos <alexandre.tp@gmail.com>
2013-06-27 13:04:28 -0400

    Finishing the code drop for WithinDocCoref2

---
+++ src/main/scala/cc/factorie/app/nlp/coref/MentionAlignment.scala
@@ -0,0 +1,168 @@
+package cc.factorie.app.nlp.coref
+
+import cc.factorie.app.nlp.wordnet.WordNet
+import cc.factorie.app.nlp.{Token, DocumentAnnotator, Document, DocumentAnnotatorLazyMap}
+import scala.collection.mutable
+import cc.factorie.util.coref.GenericEntityMap
+import cc.factorie.app.nlp.mention.{Entity, MentionList, Mention}
+import cc.factorie.app.nlp.pos.PTBPosLabel
+import scala.collection.mutable.HashMap
+import java.io.PrintWriter
+import cc.factorie.app.nlp.parse.ParseTree
+
+/**
+ * User: apassos
+ * Date: 6/27/13
+ * Time: 12:51 PM
+ */
+
+object MentionAlignment {
+  def makeLabeledData(f: String, outfile: String ,wn: WordNet, corefGazetteers: CorefGazetteers, portion: Double,useEntityTypes: Boolean, options: Coref2Options)(implicit map: DocumentAnnotatorLazyMap): (Seq[Document],mutable.HashMap[String,GenericEntityMap[Mention]]) = {
+    //first, get the gold data (in the form of factorie Mentions)
+    val documentsAll = ConllCorefLoader.loadWithParse(f)
+    val documents = documentsAll.take((documentsAll.length*portion).toInt)
+
+    //make copies of the documents with no annotation
+    val documentsToBeProcessedAll =  ConllCorefLoader.loadWithParse(f)
+    val documentsToBeProcessed =  documentsToBeProcessedAll.take((documentsToBeProcessedAll.length*portion).toInt)
+    //make sure that they have the same names, i.e. they were loaded in the same order and subspampled consistently
+    documents.zip(documentsToBeProcessed).foreach(dd => assert(dd._1.name == dd._2.name))
+
+    documentsToBeProcessed.foreach( d => d.tokens.foreach(t => t.attr.remove[PTBPosLabel]))  //remove the gold POS annotation
+    documentsToBeProcessed.foreach(_.attr.remove[MentionList])
+    //now do POS tagging and parsing on the extracted tokens
+    documentsToBeProcessed.par.foreach(findMentions(_))
+
+    //align gold mentions to detected mentions in order to get labels for detected mentions
+    val alignmentInfo =  documents.zip(documentsToBeProcessed).par.map(d => alignMentions(d._1,d._2,wn,corefGazetteers,useEntityTypes, options))
+    val entityMaps = new HashMap[String,GenericEntityMap[Mention]]() ++=  alignmentInfo.map(_._1).seq.toSeq
+
+    //do some analysis of the accuracy of this alignment
+    val prReports = alignmentInfo.map(_._2)
+    val numCorrect = prReports.map(_.numcorrect).sum.toDouble
+    val numGT = prReports.map(_.numGT).sum.toDouble
+    val numDetected = prReports.map(_.numDetected).sum.toDouble
+    println("precision = " + numCorrect/numDetected)
+    println("recall = " + numCorrect/numGT)
+
+    (documentsToBeProcessed  , entityMaps)
+  }
+
+  def printDocPOS(d: Document, out: PrintWriter): Unit = {
+    out.println("#begin document (" + d.name + ")")
+    var tokenCount = 0
+    d.sentences.foreach(s => {
+      s.tokens.zipWithIndex.foreach(ti => {
+        val stuffToPrint = mutable.Seq(d.name,"0",tokenCount,ti._2,ti._1.string,ti._1.attr[PTBPosLabel].categoryValue)
+        out.println(stuffToPrint.mkString("\t"))
+        tokenCount += 1
+      })
+      out.println()
+    })
+    d.tokens.zipWithIndex.foreach(ti => {
+
+      val stuffToPrint = mutable.Seq(d.name,"0",ti._2,ti._1.string,ti._1.attr[PTBPosLabel].categoryValue)
+      out.println(stuffToPrint.mkString("\t"))
+    })
+    out.println()
+  }
+
+  def printDoc(d: Document, out: PrintWriter): Unit = {
+    out.println("#begin document (" + d.name + ")")
+    d.attr[MentionList].filter(m => m.attr.contains[EntityType]).foreach(m => {
+      out.println(m.start + "\t" + m.length + "\t" + m.headTokenIndex + "\t" + m.attr[EntityType].categoryValue + "\t" + m.attr[EntityKey].name)
+    })
+    out.println()
+  }
+
+  //for each of the mentions in detectedMentions, this adds a reference to a ground truth entity
+  //the alignment is based on an **exact match** between the mention boundaries
+  def alignMentions(gtDoc: Document, detectedDoc: Document,wn: WordNet, cg: CorefGazetteers,useEntityTypes: Boolean, options: Coref2Options): ((String,GenericEntityMap[Mention]),PrecRecReport) = {
+    val groundTruthMentions: MentionList = gtDoc.attr[MentionList]
+    val detectedMentions: MentionList = detectedDoc.attr[MentionList]
+    val name = detectedDoc.name
+
+    val gtHash = mutable.HashMap[(Int,Int),Mention]()
+    gtHash ++= groundTruthMentions.map(m => ((m.start,m.length),m))
+    val gtAligned = mutable.HashMap[Mention,Boolean]()
+    gtAligned ++= groundTruthMentions.map(m => (m,false))
+    var exactMatches = 0
+    var relevantExactMatches = 0
+    var unAlignedEntityCount = 0
+
+    //here, we create a bunch of new entity objects, that differ from the entities that the ground truth mentions point to
+    //however, we index them by the same uIDs that the ground mentions use
+    val entityHash = groundTruthMentions.groupBy(m => m.attr[Entity]).toMap
+
+    detectedMentions.foreach(m => {
+      val alignment = checkContainment(gtHash,m.start,m.length, options)
+      if(alignment.isDefined){
+        val gtMention = alignment.get
+        m.attr +=  gtMention.attr[Entity]
+        if(entityHash(gtMention.attr[Entity]).length > 1) relevantExactMatches += 1
+        exactMatches += 1
+        val predictedEntityType = if(useEntityTypes) EntityTypeAnnotator1Util.classifyUsingRules(m.span.tokens.map(_.lemmaString),cg)  else "UKN"
+        m.attr += new EntityType(m,predictedEntityType)
+        gtAligned(gtMention) = true
+      }else{
+        val entityUID = m.document.name + unAlignedEntityCount
+        val newEntity = new Entity(entityUID)
+        m.attr += newEntity
+        val predictedEntityType = if(useEntityTypes) EntityTypeAnnotator1Util.classifyUsingRules(m.span.tokens.map(_.lemmaString),cg)  else "UKN"
+        m.attr += new EntityType(m,predictedEntityType)
+        unAlignedEntityCount += 1
+      }
+    })
+
+    //first, we add everything as a corefmention to the detectedDoc
+    val cml = new MentionList
+    cml ++= detectedDoc.attr[MentionList]
+    detectedDoc.attr += cml
+
+    //now, we make a generic entity map
+    val entityMap = new GenericEntityMap[Mention]
+
+    val unAlignedGTMentions = gtAligned.filter(kv => !kv._2).map(_._1)
+    val allCorefMentions =  detectedDoc.attr[MentionList] ++ unAlignedGTMentions
+    allCorefMentions.foreach(m => entityMap.addMention(m, entityMap.numMentions.toLong))
+
+    val corefEntities = allCorefMentions.groupBy(_.attr[Entity])
+    corefEntities.flatMap(_._2.sliding(2)).foreach(p => {
+      if (p.size == 2) entityMap.addCoreferentPair(p(0), p(1))
+    })
+
+
+    val relevantGTMentions = groundTruthMentions.count(m => entityHash(m.attr[Entity]).length > 1)
+    ((name,entityMap),new PrecRecReport(relevantExactMatches,relevantGTMentions,detectedMentions.length))
+  }
+  def checkContainment(h: mutable.HashMap[(Int,Int),Mention], start: Int, length: Int, options: Coref2Options): Option[Mention] = {
+    val startIdx = start
+    val endIdx = start + length
+    for (startShift <- Seq(0); endShift <- Seq(0); if startIdx + startShift <= endIdx + endShift) {
+      val newStart = startIdx + startShift
+      val newEnd = endIdx + endShift
+      val key = (newStart, newEnd - newStart)
+      if(h.contains(key))
+        return Some(h(key))
+    }
+    None
+  }
+  case class PrecRecReport(numcorrect: Int,numGT: Int, numDetected: Int)
+
+  def findMentions(d: Document)(implicit annotatorMap: DocumentAnnotatorLazyMap) {
+    val a = new DocumentAnnotator {
+      def process1(document: Document) = document
+      def prereqAttrs = Seq(classOf[MentionList])
+      def postAttrs = Nil
+    }
+    a.process(d)
+  }
+
+  def assertParse(tokens: Seq[Token],parse: ParseTree): Unit = {
+    val len = tokens.length
+    parse.parents.foreach(j => assert(j < len))
+  }
+
+}
+
+
---
+++ src/main/scala/cc/factorie/app/nlp/coref/WithinDocCoref2Trainer.scala
@@ -0,0 +1,177 @@
+package cc.factorie.app.nlp.coref
+
+import cc.factorie.app.nlp.Document
+import cc.factorie.util.coref.GenericEntityMap
+import cc.factorie.app.nlp.mention.{MentionList, Mention}
+import cc.factorie.app.nlp.wordnet.WordNet
+import java.io.File
+
+/**
+ * User: apassos
+ * Date: 6/27/13
+ * Time: 1:01 PM
+ */
+
+
+trait WithinDocCoref2TrainerOpts extends cc.factorie.util.DefaultCmdOptions {
+  val numPositivePairsTrain = new CmdOption("prune-train", 2, "INT", "number of positive pairs before pruning instances in training")
+  val numPositivePairsTest = new CmdOption("prune-test", 100, "INT", "number of positive pairs before pruning instances in testing")
+  val portion = new CmdOption("portion", 1.0, "DOUBLE", "Portion of corpus to load.")
+  val serialize = new CmdOption("serialize", "N/A", "FILE", "Filename in which to serialize classifier.")
+  val deserialize = new CmdOption("deserialize", "N/A", "FILE", "Filename from which to deserialize classifier.")
+  val numThreads = new CmdOption("num-threads", 4, "INT", "Number of threads to use")
+  val featureComputationsPerThread = new CmdOption("feature-computations-per-thread", 2, "INT", "Number of feature computations per thread to run in parallel during training")
+  val numTrainingIterations = new CmdOption("num-training-iterations", 3, "INT", "Number of passes through the training data")
+  val randomSeed = new CmdOption("random-seed", 0, "INT", "Seed for the random number generator")
+  val writeConllFormat = new CmdOption("write-conll-format", false, "BOOLEAN", "Write CoNLL format data.")
+  val useAverageIterate = new CmdOption("use-average-iterate", true, "BOOLEAN", "Use the average iterate instead of the last iterate?")
+  val useMIRA = new CmdOption("use-mira", false, "BOOLEAN", "Whether to use MIRA as an optimizer")
+  val saveFrequency = new CmdOption("save-frequency", 4, "INT", "how often to save the model between epochs")
+  val useExactEntTypeMatch = new CmdOption("use-exact-entity-type-match", true, "BOOLEAN", "whether to require exact alignment between NER annotation and NP annotation")
+  val trainPortionForTest = new CmdOption("train-portion-for-test", 0.1, "DOUBLE", "When testing on train, what portion to use.")
+  val wnDir = new CmdOption("wordnet", "wordnet", "STRING", "Path to the wordnet database.")   //now we load from a jar
+  val mergeFeaturesAtAll = new CmdOption("merge-features-at-all", true, "BOOLEAN", "Whether to merge features")
+  val conjunctionStyle = new CmdOption("conjunction-style", "NONE", "NONE|HASH|SLOW", "What types of conjunction features to use - options are NONE, HASH, and SLOW (use slow string-based conjunctions).")
+  val entityLR = new CmdOption("entity-left-right",false,"BOOLEAN","whether to do entity-based pruning in lr search")
+  val slackRescale = new CmdOption("slack-rescale",2.0,"FLOAT","recall bias for hinge loss")
+  val useNonGoldBoundaries = new CmdOption("use-nongold-boundaries",false,"BOOLEAN","whether to use non-gold mention boundaries")
+  val mentionAlignmentShiftWidth = new CmdOption("alignment-width",0,"INT","tolerance on boundaries when aligning detected mentions to gt mentions")
+  val useEntityType = new CmdOption("use-entity-type",true,"BOOLEAN","whether to use entity type info")
+}
+
+object WithinDocCoref2Trainer {
+
+  def printConll2011Format(doc: Document, map: GenericEntityMap[Mention], out: java.io.PrintStream) {
+    val mappedMentions = doc.attr[MentionList]
+    val (singleTokMents, multiTokMents) = mappedMentions.partition(_.span.length == 1)
+    val beginningTokMap = multiTokMents.groupBy(_.span.head)
+    val endingTokMap = multiTokMents.groupBy(_.span.last)
+    val singleTokMap = singleTokMents.groupBy(_.span.head)
+    val fId = doc.name
+    val docName = fId.substring(0, fId.length() - 4)
+    val partNum = fId.takeRight(3)
+
+    out.println("#begin document (" + docName + "); part " + partNum)
+    for (s <- doc.sentences) {
+      for (ti <- 0 until s.tokens.size) {
+        val beginningMents = beginningTokMap.get(s(ti))
+        val endingMents = endingTokMap.get(s(ti))
+        val singleTokMents = singleTokMap.get(s(ti))
+        assert(singleTokMents.size <= 1)
+        out.print(docName + " " + partNum.toInt + " " + (ti + 1) + " " + s(ti).string + " " + s(ti).posLabel.categoryValue + " - - - - - - - ")
+        var ments = List[String]()
+        if (beginningMents.isDefined) ments = beginningMents.get.reverse.map(m => "(" + map.reverseMap(m)).mkString("|") :: ments
+        if (singleTokMents.isDefined) ments = singleTokMents.get.map(m => "(" + map.reverseMap(m) + ")").mkString("|") :: ments
+        if (endingMents.isDefined) ments = endingMents.get.reverse.map(m => map.reverseMap(m) + ")").mkString("|") :: ments
+        if (ments.size > 0) out.println(ments.mkString("|"))
+        else out.println("-")
+      }
+      out.println()
+    }
+    out.println("#end document")
+  }
+
+
+  object opts extends WithinDocCoref2TrainerOpts
+
+
+  def main(args: Array[String]) {
+    opts.parse(args)
+    val options = new Coref2Options
+    //options that get serialized with the model
+    options.setConfig("useEntityType",opts.useEntityType.value)
+
+    // options which affect only learning
+    options.useAverageIterate = opts.useAverageIterate.value
+    options.numTrainingIterations = opts.numTrainingIterations.value
+    options.trainPortionForTest = opts.trainPortionForTest.value
+    options.useEntityLR = opts.entityLR.value
+    options.saveFrequency =  opts.saveFrequency.value
+    options.numThreads = opts.numThreads.value
+    options.featureComputationsPerThread = opts.featureComputationsPerThread.value
+    options.pruneNegTrain = opts.numPositivePairsTrain.value > 0
+    options.pruneNegTest = opts.numPositivePairsTest.value > 0
+    options.numPositivePairsTrain = opts.numPositivePairsTrain.value
+    options.numPositivePairsTest = opts.numPositivePairsTest.value
+    options.useExactEntTypeMatch = opts.useExactEntTypeMatch.value
+    options.slackRescale = opts.slackRescale.value
+    options.mentionAlignmentShiftWidth = opts.mentionAlignmentShiftWidth.value
+    options.useNonGoldBoundaries = opts.useNonGoldBoundaries.value
+    // options still in flux
+    options.mergeFeaturesAtAll = opts.mergeFeaturesAtAll.value
+    options.conjunctionStyle = opts.conjunctionStyle.value match {
+      case "NONE" => options.NO_CONJUNCTIONS
+      case "HASH" => options.HASH_CONJUNCTIONS
+      case "SLOW" => options.SLOW_CONJUNCTIONS
+      case s => sys.error("Unknown conjunction style: " + s)
+    }
+
+    println("** Arguments")
+    val ignoreOpts = Set("config", "help", "version")
+
+    for (o <- opts.values.toSeq.sortBy(_.name); if !ignoreOpts(o.name)) println(o.name + " = " + o.value)
+    println()
+
+    val wn =   new WordNet(new File(opts.wnDir.value))   //cc.factorie.app.nlp.wordnet.WordNet
+    val gz = new cc.factorie.app.nlp.coref.CorefGazetteers("factorie-nlp-resources/src/main/resources/cc/factorie/app/nlp/lexicon/")
+    val rng = new scala.util.Random(opts.randomSeed.value)
+
+    val (trainDocs,trainPredMaps,testDocs,testTrueMaps) =  if(opts.useNonGoldBoundaries.value )  makeTrainTestDataNonGold("conll-train-clean.txt","conll-test-clean.txt",wn,gz, options) else makeTrainTestData("conll-train-clean.txt","conll-test-clean.txt",wn,gz)
+
+    val mentPairClsf =
+      if (opts.deserialize.wasInvoked){
+        val model = new BaseCorefModel
+        val lr = new WithinDocCoref2(model, options, wn, gz)
+        lr.deserialize(opts.deserialize.value)
+        lr.doTest(testDocs, wn, testTrueMaps.toMap, "Test")
+        lr
+      }
+      else{
+        val model = if (options.conjunctionStyle == options.HASH_CONJUNCTIONS) new ImplicitCrossProductCorefModel else new BaseCorefModel
+        val lr = new WithinDocCoref2(model, options, wn, gz)
+        lr.train(trainDocs, testDocs, wn, rng, trainPredMaps.toMap, testTrueMaps.toMap,opts.saveFrequency.wasInvoked,opts.saveFrequency.value,opts.serialize.value)
+        lr
+      }
+
+
+    if (opts.serialize.wasInvoked && !opts.deserialize.wasInvoked)
+      mentPairClsf.serialize(opts.serialize.value + "-final")
+
+    if (opts.writeConllFormat.value) {
+      val conllFormatPrinter = new CorefScorer[CorefMention]
+      val conllFormatGold = new java.io.PrintStream(new java.io.File("conll-test.filteredgold"))
+      testDocs.foreach(d => conllFormatPrinter.printConll2011Format(d, testTrueMaps(d.name), conllFormatGold))
+      conllFormatGold.flush()
+      conllFormatGold.close()
+
+      val conllFormatGold2 = new java.io.PrintStream(new java.io.File("conll-test.nonfilteredgold"))
+      testDocs.foreach(d => printConll2011Format(d, testTrueMaps(d.name), conllFormatGold2))
+      conllFormatGold2.flush()
+      conllFormatGold2.close()
+    }
+  }
+
+  def makeTrainTestData(trainFile: String, testFile: String, wn: WordNet, gz: CorefGazetteers): (Seq[Document],collection.mutable.Map[String,GenericEntityMap[Mention]],Seq[Document],collection.mutable.Map[String,GenericEntityMap[Mention]]) = {
+    val allTrainDocs = ConllCorefLoader.loadWithParse("conll-train-clean.txt")
+    val allTestDocs  =  ConllCorefLoader.loadWithParse("conll-test-clean.txt")
+
+    val trainDocs = allTrainDocs.take((allTrainDocs.length*opts.portion.value).toInt)
+    val testDocs = allTestDocs.take((allTestDocs.length*opts.portion.value).toInt)
+    println("Train: "+trainDocs.length+" documents, " + trainDocs.map(d => d.attr[MentionList].length).sum.toFloat / trainDocs.length + " mentions/doc")
+    println("Test : "+ testDocs.length+" documents, " + testDocs.map(d => d.attr[MentionList].length).sum.toFloat / testDocs.length + " mention/doc")
+
+    val testEntityMaps =  collection.mutable.Map(testDocs.map(d  => d.name -> (new BaseCorefModel).generateTrueMap(d.attr[MentionList])).toSeq: _*)
+    val trainEntityMaps = collection.mutable.Map(trainDocs.map(d => d.name -> (new BaseCorefModel).generateTrueMap(d.attr[MentionList])).toSeq: _*)
+
+
+    (trainDocs,trainEntityMaps,testDocs,testEntityMaps)
+  }
+
+
+  def makeTrainTestDataNonGold(trainFile: String, testFile: String, wn: WordNet, gz: CorefGazetteers, options: Coref2Options): (Seq[Document],collection.mutable.Map[String,GenericEntityMap[Mention]],Seq[Document],collection.mutable.Map[String,GenericEntityMap[Mention]]) = {
+    import cc.factorie.app.nlp.Implicits._
+    val (trainDocs,trainMap) = MentionAlignment.makeLabeledData("conll-train-clean.txt",null,wn,gz,opts.portion.value,options.useEntityType, options)
+    val (testDocs,testMap) = MentionAlignment.makeLabeledData("conll-test-clean.txt",null,wn,gz,opts.portion.value,options.useEntityType, options)
+    (trainDocs,trainMap,testDocs,testMap)
+  }
+}
