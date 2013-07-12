06145669f56a8636f8b786b4dd89a5b059d66c46
Andrew McCallum <mccallum@cs.umass.edu>
2013-07-11 21:58:51 -0400

    MorphologicalAnalyzer1: Remove TODO that is no longer relevant.

--- src/main/scala/cc/factorie/app/nlp/morph/MorphologicalAnalyzer1.scala
+++ src/main/scala/cc/factorie/app/nlp/morph/MorphologicalAnalyzer1.scala
@@ -31,7 +31,6 @@ class MorphologicalAnalyzer1(fmap: String => io.Source) {
   }
 }
 
-// TODO Robustify this lookup using cc.factorie.util.InputStreamFromClasspath -akm
 object MorphologicalAnalyzer1 extends MorphologicalAnalyzer1((s:String) => {
   io.Source.fromURL(ClasspathURL.fromDirectory[Lexicon](s))
 })
