d4dc4e004023b0f5d8de96d227752fcadd3d7ae5
Andrew McCallum <mccallum@cs.umass.edu>
2013-08-07 16:18:50 -0400

    DocumentAnnotatorPipeline: Add apply methods using Manifests for easier calling.

--- src/main/scala/cc/factorie/app/nlp/DocumentAnnotatorPipeline.scala
+++ src/main/scala/cc/factorie/app/nlp/DocumentAnnotatorPipeline.scala
@@ -6,6 +6,8 @@ package cc.factorie.app.nlp
  * Time: 2:48 PM
  */
 
+/** A sequence of DocumentAnnotators packaged as a single DocumentAnnotator.
+    This class also properly populates the Document.annotators with a record of which DocumentAnnotator classes provided which annotation classes. */
 class DocumentAnnotationPipeline(val annotators: Seq[DocumentAnnotator], val prereqAttrs: Seq[Class[_]] = Seq()) extends DocumentAnnotator {
   def postAttrs = annotators.flatMap(_.postAttrs).distinct
   def process(document: Document) = {
@@ -19,17 +21,21 @@ class DocumentAnnotationPipeline(val annotators: Seq[DocumentAnnotator], val pre
   def tokenAnnotationString(token: Token) = annotators.map(_.tokenAnnotationString(token)).mkString("\t")
 }
 
+/** A Map from annotation class to DocumentAnnotator that provides that annotation. 
+    Used to store default ways of getting certain prerequisite annotations. */
 class MutableDocumentAnnotatorMap extends collection.mutable.HashMap[Class[_], () => DocumentAnnotator] {
   def +=(annotator: DocumentAnnotator) = annotator.postAttrs.foreach(a => this(a) = () => annotator)
 }
 
+/** A factory for creating DocumentAnnotatorPipelines given requirements about which annotations or which DocumentAnnotators are desired. */
 object DocumentAnnotatorPipeline {
   type DocumentAnnotatorMap = collection.Map[Class[_], () => DocumentAnnotator]
-  val defaultDocumentAnnotationMap: DocumentAnnotatorMap = Seq(classOf[pos.PTBPosLabel] -> (() => pos.POS1),
+  val defaultDocumentAnnotationMap: DocumentAnnotatorMap = Seq(
+    classOf[pos.PTBPosLabel] -> (() => pos.POS1),
     classOf[parse.ParseTree] -> (() => parse.DepParser1),
-    classOf[segment.SimplifyPTBTokenString] -> (() => segment.SimplifyPTBTokenNormalizer),
-    classOf[Token] -> (() => cc.factorie.app.nlp.segment.ClearTokenizer), // If you ask for this first, and then ask for Sentence, you will get a conflict. -akm),
-    classOf[Sentence] -> (() => cc.factorie.app.nlp.segment.ClearSegmenter),
+    classOf[segment.PlainNormalizedTokenString] -> (() => segment.PlainTokenNormalizer),
+    classOf[Token] -> (() => cc.factorie.app.nlp.segment.Tokenizer1),
+    classOf[Sentence] -> (() => cc.factorie.app.nlp.segment.SentenceSegmenter1),
     classOf[lemma.WordNetTokenLemma] -> (() => cc.factorie.app.nlp.lemma.WordNetLemmatizer),
     classOf[lemma.SimplifyDigitsTokenLemma] -> (() => lemma.SimplifyDigitsLemmatizer),
     classOf[lemma.CollapseDigitsTokenLemma] -> (() => lemma.CollapseDigitsLemmatizer),
@@ -39,10 +45,19 @@ object DocumentAnnotatorPipeline {
     classOf[ner.BilouOntonotesNerLabel] -> (() => ner.NER2),
     classOf[mention.ParseBasedMentionList] -> (() => mention.ParseBasedMentionFinding),
     classOf[mention.NerMentionList] -> (() => mention.NerAndPronounMentionFinder),
-    classOf[cc.factorie.util.coref.GenericEntityMap[mention.Mention]] -> (() => coref.WithinDocCoref1)).toMap
+    classOf[cc.factorie.util.coref.GenericEntityMap[mention.Mention]] -> (() => coref.WithinDocCoref1)
+  ).toMap
 
   def apply(goal: Class[_]): DocumentAnnotationPipeline = apply(Seq(goal), defaultDocumentAnnotationMap)
+  def apply[A](implicit m:Manifest[A]): DocumentAnnotationPipeline = apply(m.erasure)
+  def apply[A,B](implicit m1:Manifest[A], m2:Manifest[B]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure))
+  def apply[A,B,C](implicit m1:Manifest[A], m2:Manifest[B], m3:Manifest[C]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure, m3.erasure))
+  def apply[A,B,C,D](implicit m1:Manifest[A], m2:Manifest[B], m3:Manifest[C], m4:Manifest[D]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure, m3.erasure, m4.erasure))
   def apply(goal: Class[_], map: DocumentAnnotatorMap): DocumentAnnotationPipeline = apply(Seq(goal), map)
+  def apply[A](map: DocumentAnnotatorMap)(implicit m:Manifest[A]): DocumentAnnotationPipeline = apply(m.erasure, map)
+  def apply[A,B](map: DocumentAnnotatorMap)(implicit m1:Manifest[A], m2:Manifest[B]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure), map)
+  def apply[A,B,C](map: DocumentAnnotatorMap)(implicit m1:Manifest[A], m2:Manifest[B], m3:Manifest[C]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure, m3.erasure), map)
+  def apply[A,B,C,D](map: DocumentAnnotatorMap)(implicit m1:Manifest[A], m2:Manifest[B], m3:Manifest[C], m4:Manifest[D]): DocumentAnnotationPipeline = apply(Seq(m1.erasure, m2.erasure, m3.erasure, m4.erasure), map)
 
   def apply(goals: Iterable[Class[_]]): DocumentAnnotationPipeline = apply(goals, Seq(), defaultDocumentAnnotationMap)
   def apply(goals: Iterable[Class[_]], prereqs: Seq[Class[_]]): DocumentAnnotationPipeline = apply(goals, prereqs, defaultDocumentAnnotationMap)
