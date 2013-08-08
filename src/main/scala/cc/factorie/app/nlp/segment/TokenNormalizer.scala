67b134ae66375e9e3017f6325cb8de152428cb48
Andrew McCallum <mccallum@cs.umass.edu>
2013-08-08 09:19:42 -0400

    TokenNormalizer1: Remove print statement. Normalize space-separated dash to --.

--- src/main/scala/cc/factorie/app/nlp/segment/TokenNormalizer.scala
+++ src/main/scala/cc/factorie/app/nlp/segment/TokenNormalizer.scala
@@ -48,10 +48,10 @@ class TokenNormalizer1[A<:TokenString](
     else if (unescapeAsterisk && string == "\\*") token.attr += newTokenString(token, "*")
     else if (unescapeAsterisk && string == "\\*\\*") token.attr += newTokenString(token, "**")
     else if (normalizeMDash && mdashRegex.findFirstMatchIn(string) != None) token.attr += newTokenString(token, "--") // replace all em-dashes with two dashes
-    else if (normalizeDash && dashRegex.findPrefixMatchOf(string) != None) token.attr += newTokenString(token, "-") // replace all dashes with dash
+    else if (normalizeDash && dashRegex.findPrefixMatchOf(string) != None) token.attr += newTokenString(token, if (token.hasPrecedingWhitespace && token.hasFollowingWhitespace && !token.precedesNewline) "--" else "-") // replace all dash with dash
     else if (normalizeQuote && quoteRegex.findFirstMatchIn(string) != None) token.attr += newTokenString(token, "\"") // replace all quotes with "
     else if (normalizeCurrency && quoteRegex.findPrefixMatchOf(string) != None) token.attr += newTokenString(token, "\"") // replace all quotes with "
-    else if (normalizeApostrophe && apostropheRegex.findFirstMatchIn(string) != None) { println("TokenNormalizer1 before "+token.string); token.attr += newTokenString(token, apostropheRegex.replaceAllIn(string, "'")); println("TokenNormalizer1 after "+token.string) } // replace all apostrophes with simple '
+    else if (normalizeApostrophe && apostropheRegex.findFirstMatchIn(string) != None) token.attr += newTokenString(token, apostropheRegex.replaceAllIn(string, "'")) // replace all apostrophes with simple '
     else if (americanize && BritishToAmerican.contains(string)) token.attr += newTokenString(token, BritishToAmerican(string))
   }
   def process(document:Document): Document = {
