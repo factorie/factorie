d81b7e4b68cd782e07fd311cfa3391c3a4d86c1c
Andrew McCallum <mccallum@cs.umass.edu>
2013-08-30 22:15:47 -0400

    Additional UsersGuide documentation, especially in Overview.

---
+++ src/main/scala/cc/factorie/tutorial/UsersGuide02Installation.scala
@@ -0,0 +1,41 @@
+
+/*&
+
+# Installation
+
+FACTORIE is known to run on MacOS and Linux, and is expected to run on any platform on which the Java Virtual Machine is available.
+
+## Dependencies
+
+## Downloading Pre-compiled JAR
+
+## Checking out Source from GitHub
+
+## Compiling Development Version Source
+
+## Running Test Suite
+
+## Adding to a Maven Project
+
+## NLP Resources JAR
+
+## Questions, Bug Reports and Feature Requests
+
+The FACTORIE mailing list at factorie-discuss@googlegroups.com is a good place to ask general questions and discuss broad points.
+
+Please report specific problems with the installation, bugs in the code, or feature requests in the FACTORIE GitHub issues tracker at 
+https://github.com/factorie/factorie/issues  
+
+
+## Eclipse and IntelliJ IDE Setup
+
+## Coding Standards
+
+We generally follow the Scala style guide suggestions in 
+http://davetron5000.github.com/scala-style/index.html
+
+For indentation use two spaces rather than tabs.
+
+
+
+ */
