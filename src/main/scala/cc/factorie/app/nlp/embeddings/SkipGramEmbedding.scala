acd8d065637bec6bbb0ca841ab44adc674baab30
Vineet Kumar <vineet@cs.umass.edu>
2013-09-12 12:41:05 -0400

    create a NER object with no embeddings

--- src/main/scala/cc/factorie/app/nlp/embeddings/SkipGramEmbedding.scala
+++ src/main/scala/cc/factorie/app/nlp/embeddings/SkipGramEmbedding.scala
@@ -17,17 +17,21 @@ class SkipGramEmbedding(val inputStreamFactory: String=> java.io.InputStream, di
   def sourceFactory(string:String): io.Source = io.Source.fromInputStream(new GZIPInputStream(inputStreamFactory(string)))
 
   println("Embedding reading size: %d".format(dimensionSize))
-  val source = sourceFactory("skip-gram-d100.W.gz")
-  var count = 0
-  for (line <- source.getLines()) {
-    val fields = line.split("\\s+")
-    val tensor = new la.DenseTensor1(fields.drop(1).map(_.toDouble))
-    assert(tensor.dim1 == dimensionSize)
-    this(fields(0)) = tensor
-    count += 1
-    if (count % 100000 == 0) println("word vector count: %d".format(count))
+
+  initialize()
+  def initialize() {
+    val source = sourceFactory("skip-gram-d100.W.gz")
+    var count = 0
+    for (line <- source.getLines()) {
+      val fields = line.split("\\s+")
+      val tensor = new la.DenseTensor1(fields.drop(1).map(_.toDouble))
+      assert(tensor.dim1 == dimensionSize)
+      this(fields(0)) = tensor
+      count += 1
+      if (count % 100000 == 0) println("word vector count: %d".format(count))
+    }
+    source.close()
   }
-  source.close()
 
   def close(string:String): Seq[String] = {
     val t = this(string)
