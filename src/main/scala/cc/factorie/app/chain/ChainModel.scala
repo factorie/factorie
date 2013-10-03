b563f74425ac77e9dba7d5476595b3b34bebc3dc
Luke Vilnis <lvilnis@gmail.com>
2013-10-03 18:46:16 -0400

    add fast chain viterbi and speed up chain FB slightly

--- src/main/scala/cc/factorie/app/chain/ChainModel.scala
+++ src/main/scala/cc/factorie/app/chain/ChainModel.scala
@@ -26,7 +26,7 @@ import cc.factorie.Factorie.DotFamilyWithStatistics3
 import cc.factorie.Factorie.Parameters
 import cc.factorie.Factorie.DotFamilyWithStatistics2
 import cc.factorie.Factorie.Factor
-import cc.factorie.la.WeightsMapAccumulator
+import cc.factorie.la.{SparseIndexedTensor1, WeightsMapAccumulator}
 import cc.factorie.DenseTensor1
 import cc.factorie.Factorie.CategoricalVectorDomain
 import cc.factorie.Factorie.Var
@@ -125,28 +125,84 @@ class ChainModel[Label<:LabeledMutableDiscreteVarWithTarget, Features<:Categoric
       result
     }
   }
-  // only works for dot familys, no structured svm, obs template looks like [Label, Features]
-  def inferChainSumFast(varying: Seq[Label]): (Double, Array[DenseTensor1], Array[DenseTensor1], Array[DenseTensor1]) = {
-    assert(!useObsMarkov, "obsMarkov factors not supported with efficient sum-product")
-    val markovScores = markov.weights.value
-    val biasScores = bias.weights.value
-    val obsWeights = obs.weights.value
 
-    val localScores = Array.fill[DenseTensor1](varying.size)(null)
-    var i = 0
-    while (i < varying.length) {
-      val scores = (obsWeights.leftMultiply(labelToFeatures(varying(i)).value.asInstanceOf[Tensor1])).asInstanceOf[DenseTensor1]
-      scores += biasScores
-      localScores(i) = scores
+  case class ViterbiResults(mapScore: Double, mapValues: Array[Int], localScores: Array[DenseTensor1])
+  case class InferenceResults(logZ: Double, alphas: Array[DenseTensor1], betas: Array[DenseTensor1], localScores: Array[DenseTensor1])
+
+  def viterbiFast(varying: Seq[Label], localScoresOpt: Option[Array[DenseTensor1]] = None): ViterbiResults = {
+    assert(!useObsMarkov, "obsMarkov factors not supported with efficient max-product")
+    val markovScores = markov.weights.value
+    val localScores = localScoresOpt.getOrElse(getLocalScores(varying))
+
+    val maxMarginals = Array.fill(varying.size)(new DenseTensor1(markovScores.dim1, Double.NegativeInfinity))
+    val backPointers = Array.fill(varying.size)(Array.fill[Int](markovScores.dim1)(-1))
+
+    maxMarginals(0) := localScores(0)
+
+    val domainSize = markovScores.dim1
+
+    var i = 1
+    while (i < varying.size) {
+      val curLocalScores = localScores(i)
+      val curMaxMarginal = maxMarginals(i)
+      val curBackPointers = backPointers(i)
+      val prevMaxMarginal = maxMarginals(i - 1)
+      var vi = 0
+      while (vi < domainSize) {
+        var maxScore = Double.NegativeInfinity
+        var maxIndex = -1
+        var vj = 0
+        while (vj < domainSize) {
+          val curScore = markovScores(vj, vi) + prevMaxMarginal(vj) + curLocalScores(vi)
+          if (curScore > maxScore) {
+            maxScore = curScore
+            maxIndex = vj
+          }
+          vj += 1
+        }
+        curMaxMarginal(vi) = maxScore
+        curBackPointers(vi) = maxIndex
+        vi += 1
+      }
       i += 1
     }
+
+    val mapValues = Array.fill[Int](varying.size)(0)
+    mapValues(mapValues.size - 1) = maxMarginals.last.maxIndex
+    var j = mapValues.size - 2
+    while (j >= 0) {
+      mapValues(j) = backPointers(j + 1)(mapValues(j + 1))
+      j -= 1
+    }
+
+    ViterbiResults(maxMarginals.last.max, mapValues, localScores)
+  }
+
+  def getLocalScores(varying: Seq[Label]): Array[DenseTensor1] = {
+    val biasScores = bias.weights.value
+    val obsWeights = obs.weights.value
+    val a = Array.fill[DenseTensor1](varying.size)(null)
+    var i = 0
+    while (i < varying.length) {
+      val scores = obsWeights.leftMultiply(labelToFeatures(varying(i)).value.asInstanceOf[Tensor1]).asInstanceOf[DenseTensor1]
+      scores += biasScores
+      a(i) = scores
+      i += 1
+    }
+    a
+  }
+
+  def inferFast(varying: Seq[Label], localScoresOpt: Option[Array[DenseTensor1]] = None): InferenceResults = {
+    assert(!useObsMarkov, "obsMarkov factors not supported with efficient sum-product")
+    val markovScores = markov.weights.value
+    val localScores = localScoresOpt.getOrElse(getLocalScores(varying))
 
     val alphas = Array.fill(varying.size)(new DenseTensor1(markovScores.dim1, Double.NegativeInfinity))
     val betas = Array.fill(varying.size)(new DenseTensor1(markovScores.dim1, Double.NegativeInfinity))
 
     alphas(0) := localScores(0)
 
-    i = 1
+    var i = 1
     val d1 = markovScores.dim1
     val tmpArray = Array.fill(markovScores.dim1)(0.0)
     while (i < varying.size) {
@@ -159,7 +215,7 @@ class ChainModel[Label<:LabeledMutableDiscreteVarWithTarget, Features<:Categoric
           tmpArray(vj) = markovScores(vj, vi) + aim1(vj)
           vj += 1
         }
-        ai(vi) = maths.sumLogProbs(new ArrayDoubleSeq(tmpArray))
+        ai(vi) = maths.sumLogProbs(tmpArray)
         vi += 1
       }
       alphas(i) += localScores(i)
@@ -180,20 +236,61 @@ class ChainModel[Label<:LabeledMutableDiscreteVarWithTarget, Features<:Categoric
           tmpArray(vj) = markovScores(vi, vj) + bip1(vj) + lsp1(vj)
           vj += 1
         }
-        bi(vi) = maths.sumLogProbs(new ArrayDoubleSeq(tmpArray))
+        bi(vi) = maths.sumLogProbs(tmpArray)
         vi += 1
       }
       i -= 1
     }
 
     val logZ = maths.sumLogProbs(alphas.last)
-    (logZ, alphas, betas, localScores)
+    InferenceResults(logZ, alphas, betas, localScores)
+  }
+
+  class ChainViterbiExample(varying: Seq[Label], localScoresOpt: Option[Array[DenseTensor1]] = None) extends Example {
+    def accumulateValueAndGradient(value: DoubleAccumulator, gradient: WeightsMapAccumulator): Unit = {
+      if (varying.length == 0) return
+      val ViterbiResults(mapScore, mapValues, localScores) = viterbiFast(varying, localScoresOpt)
+      val transScores = markov.weights.value
+      if (value ne null)
+        value.accumulate(-mapScore)
+      val domainSize = transScores.dim1
+      val transGradient = new DenseTensor2(domainSize, domainSize)
+      val len = varying.length
+      var i = 0
+      while (i < len) {
+        val curLabel = varying(i)
+        val prevLabel = if (i >= 1) varying(i - 1) else null.asInstanceOf[Label]
+        val curLocalScores = localScores(i)
+        val curTargetIntValue = curLabel.targetIntValue
+        val prevTargetIntValue = if (i >= 1) prevLabel.targetIntValue else -1
+        val curPredIntValue = mapValues(i)
+        val prevPredIntValue = if (i >= 1) mapValues(i - 1) else -1
+        if (value ne null) {
+          value.accumulate(curLocalScores(curTargetIntValue))
+          if (i >= 1) value.accumulate(transScores(prevTargetIntValue, curTargetIntValue))
+        }
+        if (gradient ne null) {
+          val localMarginal = new SparseIndexedTensor1(domainSize)
+          localMarginal(curPredIntValue) += -1
+          localMarginal(curTargetIntValue) += 1
+          gradient.accumulate(bias.weights, localMarginal)
+          gradient.accumulate(obs.weights, labelToFeatures(curLabel).value outer localMarginal)
+          if (i >= 1) {
+            transGradient(prevTargetIntValue, curTargetIntValue) += 1
+            transGradient(prevPredIntValue, curPredIntValue) += -1
+          }
+        }
+        i += 1
+      }
+      if (gradient ne null)
+        gradient.accumulate(markov.weights, transGradient)
+    }
   }
 
   class ChainLikelihoodExample(varying: Seq[Label]) extends Example {
     def accumulateValueAndGradient(value: DoubleAccumulator, gradient: WeightsMapAccumulator): Unit = {
       if (varying.length == 0) return
-      val (logZ, alphas, betas, localScores) = inferChainSumFast(varying)
+      val InferenceResults(logZ, alphas, betas, localScores) = inferFast(varying)
       val transScores = markov.weights.value
       if (value ne null)
         value.accumulate(-logZ)
