e84d93b3ecadec69fa329f57f17b6ec4417022fb
David Belanger <david.b.belanger@gmail.com>
2013-10-04 14:23:42 -0400

    small improvement to cl names

--- src/main/scala/cc/factorie/app/nlp/NLPCmdOptions.scala
+++ src/main/scala/cc/factorie/app/nlp/NLPCmdOptions.scala
@@ -11,7 +11,7 @@ import cc.factorie.util.CmdOptions
  */
 trait SharedNLPCmdOptions extends CmdOptions  {
   val targetAccuracy = new CmdOption("target-accuracy", "", "FLOAT", "target accuracy for this NLP model. It will throw an exception if you don't hit this")
-  val trainPortion = new CmdOption("train-portion", "", "INT", "number of documents for both train and test to load")
-  val testPortion = new CmdOption("test-portion", "", "INT", "number of documents for both train and test to load")
+  val trainPortion = new CmdOption("train-portion", "", "INT", "portion of train to load")
+  val testPortion = new CmdOption("test-portion", "", "INT", "portion of test to load")
 
 }
