72d63cdac8ae2699bcb6519b20a9d2cc94992b56
Caitlin Cellier <Caitlin.Cellier@gmail.com>
2013-11-22 12:55:06 -0500

    Changed Tagger to tag BILOU2LayerChunkTags Span Retriever still retrieves standard BILOUChunkTags

--- src/main/scala/cc/factorie/app/nlp/mention/NPChunkMentionFinder.scala
+++ src/main/scala/cc/factorie/app/nlp/mention/NPChunkMentionFinder.scala
@@ -2,9 +2,9 @@ package cc.factorie.app.nlp.mention
 
 import java.io.{File, FileOutputStream, FileInputStream}
 import cc.factorie.app.nlp._
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ListBuffer, ArrayBuffer}
 import cc.factorie.app.nlp.phrase.{ChunkerOpts, CRFChunker}
-import cc.factorie.app.nlp.load.{BILOUChunkTag}
+import cc.factorie.app.nlp.load.{BILOU2LayerChunkTag, BILOUChunkTag}
 import cc.factorie.app.nlp.coref._
 import cc.factorie.app.nlp.pos.PennPosTag
 import cc.factorie.app.nlp.ner.{ConllChainNer, NerTag}
@@ -21,24 +21,16 @@ import cc.factorie.app.nlp.coref.ForwardCorefTrainer.opts
 
 class ChunkBasedMentionList extends MentionList
 
-object ChunkBasedMentionFinder extends DocumentAnnotator {
-  var chunk = new CRFChunker
-  def initChunker(chunkerFileName: String):CRFChunker = {
-    chunk.deserialize(new FileInputStream(new java.io.File(chunkerFileName)))
+class NPChunkBasedMentionFinder(val chunk:CRFChunker) extends DocumentAnnotator {
+  def this(filename:String) = this( {val chunk = new CRFChunker()
+    chunk.deserialize(new FileInputStream(new java.io.File("CRFChunker.factorie")))
     chunk
-  }
-  def prereqAttrs = Seq(classOf[Token])
+  })
+  def prereqAttrs = Seq(classOf[Token],classOf[BILOUChunkTag])
   def postAttrs = Seq(classOf[ChunkBasedMentionList], classOf[MentionEntityType])
   override def tokenAnnotationString(token:Token): String = token.document.attr[MentionList].filter(mention => mention.contains(token)) match { case ms:Seq[Mention] if ms.length > 0 => ms.map(m => m.attr[MentionType].categoryValue+":"+ m.attr[MentionEntityType].categoryValue +":" +m.indexOf(token)).mkString(","); case _ => "_" }
 
   val upperCase = "[A-Z]+".r
-
-  val PersonLexicon = new lexicon.UnionLexicon("MentionEntityTypePerson", lexicon.PersonPronoun, lexicon.PosessiveDeterminer)
-
-  /* TODO: Find a way to include these better */
-  private final val PERSONAL_PRONOUNS = Seq("PRP", "PRP$")
-
-  private def isPersonalPronoun(t: Token) = PERSONAL_PRONOUNS.contains(t.posTag.categoryValue.toUpperCase)
 
   def getNPChunkSpans(document: Document): Seq[(String,TokenSpan)] ={
     val spans = ArrayBuffer[(String,TokenSpan)]()
@@ -70,23 +62,7 @@ object ChunkBasedMentionFinder extends DocumentAnnotator {
       val label = labelSpan._1
       val s = labelSpan._2
       val m = new Mention(s, s.length-1)
-      if(label.startsWith("U") && s.tokens.length == 1 && isPersonalPronoun(s.tokens(0))){
-        m.attr += new MentionType(m, "PRO")
-        m.attr += new MentionEntityType(m,"PERSON")
-      } /*
-      else if(s.tokens.count(t => isCommonNoun(t)) > 0){
-        m.attr += new MentionType(m, "NOM")
-        m.attr += new MentionEntityType(m,"PERSON")
-      }
-      else if(s.tokens.count(t => isProperNoun(t)) > 0){
-        m.attr += new MentionType(m, "NAM")
-        if(s.tokens.count(t => PersonLexicon.contains(s))>0) m.attr += new MentionEntityType(m,"PERSON")
-        else m.attr += new MentionEntityType(m,"ORG")
-      }
-      //else if mention includes a    */
-      else {m.attr += new MentionType(m, "NAM")
-        m.attr += new MentionEntityType(m,"MISC")
-      }
+      m.attr += new MentionType(m, "NAM")
       m
     }
   }
@@ -98,32 +74,39 @@ object ChunkBasedMentionFinder extends DocumentAnnotator {
     document
   }
 
-  def testMentions(trainFile: String, testFile: String, options: Coref1Options) = {
-    val map = new MutableDocumentAnnotatorMap ++= DocumentAnnotatorPipeline.defaultDocumentAnnotationMap
-    map(classOf[MentionList]) = () => ChunkBasedMentionFinder
-
-    val (trainDocs,trainMap) = MentionAlignment.makeLabeledData(trainFile,null,opts.portion.value,options.useEntityType, options, map.toMap)
-    val (testDocs,testMap) = MentionAlignment.makeLabeledData(testFile,null,opts.portion.value,options.useEntityType, options, map.toMap)
-
-  }
-
-  def tagBILOUtoMentions(sentences: Iterable[Sentence], mentions: ArrayBuffer[Mention]): Iterable[Sentence]={
-    sentences.map(s=>s.tokens.map(t => t.attr += new BILOUChunkTag(t,"O")))
+  def tagBILOUtoMentions(sentences: Iterable[Sentence], mentions: ArrayBuffer[Mention]): (ListBuffer[Mention])={
+    sentences.map(s=>s.tokens.map(t => t.attr += new BILOU2LayerChunkTag(t,"O:O")))
+    val skippedMentions = ListBuffer[Mention]()
     //Negative length for descending sort
     mentions.sortBy(m => m.tokens.length).foreach{mention =>
-      if(mention.forall(t => t.attr[BILOUChunkTag].categoryValue == "O")){
+      if(mention.forall(t => t.attr[BILOU2LayerChunkTag].categoryValue.split(':')(0) == "O")){
         mention.tokens.map{t =>
-          if(mention.tokens.length == 1)  t.attr += new BILOUChunkTag(t, "U-NP")
-          else if(t == mention.head) t.attr += new BILOUChunkTag(t, "B-NP")
-          else if(t == mention.last) t.attr += new BILOUChunkTag(t,"L-NP")
-          else t.attr += new BILOUChunkTag(t,"I-NP")
+          if(mention.tokens.length == 1)  t.attr += new BILOU2LayerChunkTag(t, "U-NP:O")
+          else if(t == mention.head) t.attr += new BILOU2LayerChunkTag(t, "B-NP:O")
+          else if(t == mention.last) t.attr += new BILOU2LayerChunkTag(t,"L-NP:O")
+          else t.attr += new BILOU2LayerChunkTag(t,"I-NP:O")
         }
       }
+      else if(mention.forall(t => t.attr[BILOU2LayerChunkTag].categoryValue.split(':')(1) == "O")){
+        mention.tokens.map{t =>
+          val innerMention = t.attr[BILOU2LayerChunkTag].categoryValue.split(':')(0)
+          if(mention.tokens.length == 1)  t.attr += new BILOU2LayerChunkTag(t, innerMention+":U-NP")
+          else if(t == mention.head) t.attr += new BILOU2LayerChunkTag(t, innerMention+":B-NP")
+          else if(t == mention.last) t.attr += new BILOU2LayerChunkTag(t,innerMention+":L-NP")
+          else t.attr += new BILOU2LayerChunkTag(t,innerMention+":I-NP")
+        }
+      } else
+        skippedMentions += mention
+
     }
-    sentences
+
+    skippedMentions
   }
 }
 
+object NPChunkBasedMentionFinder extends NPChunkBasedMentionFinder(CRFChunker){
+
+}
 
 
 object CreateTrainingData{
@@ -134,33 +117,39 @@ object CreateTrainingData{
     assert(opts.trainFile.wasInvoked)
     val trainDocs = ConllCorefLoader.loadWithParse(opts.trainFile.value)
     val testDocs = ConllCorefLoader.loadWithParse(opts.testFile.value)
+    val chunker = new CRFChunker()
+    chunker.deserialize(new FileInputStream(new java.io.File("2011CM.factorie")))
+    val mentionfinder = NPChunkBasedMentionFinder
     println("Train: " + trainDocs.length+" documents, " + trainDocs.map(d => d.attr[MentionList].length).sum.toFloat / trainDocs.length + " mentions/doc")
-    println("Test: " + testDocs.length+" documents, " + testDocs.map(d => d.attr[MentionList].length).sum.toFloat / testDocs.length + " mentions/doc")
-
-    val trainSentences = trainDocs.flatMap(d=>ChunkBasedMentionFinder.tagBILOUtoMentions(d.sentences,d.attr[MentionList]).map(_.sentence))
-
+    val trainSentences = trainDocs.flatMap(_.sentences)
+    val missedMentions = trainDocs.flatMap(d=>mentionfinder.tagBILOUtoMentions(d.sentences,d.attr[MentionList]))
     println("Mentions Tagged")
-    val p = new java.io.PrintWriter(new File("src/main/resources/taggedMentionsTrain.txt"))
+    val totalMentions= trainDocs.map(d => d.attr[MentionList].length).sum.toFloat
+    println("Total Mentions: " + totalMentions)
+    println("Missed " + missedMentions.length + " Mentions")
+    println("Tagged Percent: " + (totalMentions-missedMentions.length)/totalMentions)
+
+    val p1 = new java.io.PrintWriter(new File("src/main/resources/2LayerTaggedMentionsTrain.txt"))
     try {
-      trainSentences.foreach{s => s.tokens.map{t =>  p.println(t.string + " " + t.attr[PennPosTag].categoryValue + " " + t.attr[BILOUChunkTag].categoryValue)};p.println()}
-    } finally { p.close() }
+      trainSentences.foreach{s => s.tokens.map{t =>  p1.println(t.string + " " + t.attr[PennPosTag].categoryValue + " " + t.attr[BILOU2LayerChunkTag].categoryValue)};p1.println()}
+    } finally { p1.close() }
   }
 }
 
 object ChunkingExperiments{
-
   def main(args:Array[String]){
-    /*opts.parse(args)
+    opts.parse(args)
     val map = new MutableDocumentAnnotatorMap ++= DocumentAnnotatorPipeline.defaultDocumentAnnotationMap
-    map(classOf[MentionList]) = () => ChunkBasedMentionFinder
+    map(classOf[MentionList]) = () => NPChunkBasedMentionFinder
     val options = new ForwardCoref().options
 
-    val (trainDocs,trainMap) = MentionAlignment.makeLabeledData(opts.trainFile.value,null,opts.portion.value,options.useEntityType, options, map.toMap)
-    val (testDocs,testMap) = MentionAlignment.makeLabeledData(opts.trainFile.value,null,opts.portion.value,options.useEntityType, options, map.toMap)
-    */
-    ChunkBasedMentionFinder.initChunker("2011CM.factorie")
-    println("2011 Exact Boundaries with no Headword")
-    ForwardCorefTrainer.evaluateParameters(args:+"--alignment-width=0")
+    val (testDocs,testMap) = MentionAlignment.makeLabeledData(opts.testFile.value,null,opts.portion.value,options.useEntityType, options, map.toMap)
+
+    //val (testDocs,testMap) = MentionAlignment.makeLabeledData(opts.testFile.value,null,opts.portion.value,options.useEntityType, options, map.toMap)
+
+    //ChunkBasedMentionFinder.initChunker("2011CM.factorie")
+    //println("2011 Exact Boundaries with no Headword")
+    //ForwardCorefTrainer.evaluateParameters(args:+"--alignment-width=0")
 
   }
 }
