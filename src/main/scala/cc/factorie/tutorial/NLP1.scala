2fb981360adb951ca99df68cc7373d4261521f96
Andrew McCallum <mccallum@cs.umass.edu>
2013-12-03 14:11:40 -0500

    Remove and clean up some tutorial source files.

--- src/main/scala/cc/factorie/tutorial/NLP1.scala
+++ src/main/scala/cc/factorie/tutorial/NLP1.scala
@@ -2,6 +2,7 @@ package cc.factorie.tutorial
 import cc.factorie._
 import cc.factorie.app.nlp._
 
+/** A simple example of running an NLP document processor. */
 object NLP1 extends App {
   val doc = new Document("Mr. Jones took a job at Google in New York.  He and his Australian wife moved from New South Wales on 4/1/12.")
   println(doc.string.length)
