fd56a5e67b12442300bc18a7f8da2a9157e332be
Alexandre Passos <alexandre.tp@gmail.com>
2013-12-03 16:57:26 -0500

    improving doc ordering

--- doc/get_header.py
+++ doc/get_header.py
@@ -8,6 +8,7 @@ if len(argv) != 2:
     exit(1)
 
 def makeHeader(title):
+    digits = int(''.join(c for c in title if c.isdigit()))
     if title[0] == "T":
         group = "tutorial"
     else:
@@ -16,13 +17,13 @@ def makeHeader(title):
 title: "%s"
 layout: default
 group: %s
-weight: 3
+weight: %s
 ---
 
 <a href="{{ site.baseurl }}/tutorial.html">Tutorials</a> &gt;
 
 
-""" % (title,group)
+""" % (title,group,digits)
 
 title_map = {
     'Tutorial01Introduction.scala.md' : 'Tutorial 0: Introduction',
