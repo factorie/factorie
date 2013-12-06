0f1a2f5b21578e5807e4d1bf021ec49adbd28e98
Alexandre Passos <alexandre.tp@gmail.com>
2013-12-06 13:53:24 -0800

    tutorial: better user guide formatting

--- src/main/scala/cc/factorie/tutorial/UsersGuide02Installation.scala
+++ src/main/scala/cc/factorie/tutorial/UsersGuide02Installation.scala
@@ -102,26 +102,45 @@ https://github.com/factorie/factorie/issues
 To import FACTORIE as a project in Eclipse or IDEA, start by cloning the git repository. 
 
 Eclipse (Kepler):
+
 1. Install the Scala plugin for Eclipse through the Eclipse Marketplace. Once installed the plugin should prompt you to make sure Eclipse is configured properly to run Scala, e.g. you may want to add a line to your eclipse.ini file that increased the default amount of Java heap space that Eclipse runs with
+
 2. If you want to manage git source control through Eclipse, make sure you have the latest version of the EGit plugin installed.
+
 3. Before you can import a Maven Scala project, you’ll also need to install the m2eclipse-scala connector (the Maven plugin that comes with Eclipse doesn’t support Scala by default) by installing the connector from the following Eclipse plugin site: http://alchim31.free.fr/m2e-scala/update-site (via Help->Install New Software)
+
 4. Now import Factorie as a Maven project via File->Import->Maven->Existing Maven Projects. Select the top-level  directory from your cloned version of FACTORIE, the directory containing the pom.xml file, as the root directory.
+
 5. It should work! If Eclipse complains that Factorie is full of syntax errors (yet compiles fine) try cleaning (Project->Clean), rebuilding (should happen automatically after clean, otherwise Project->Build Project) and restarting Eclipse. Also make sure that you are in the Scala perspective.
+
 6. To compile, right click on pom.xml and select Run As->Maven Build. Enter "compile" as the goal. Apply and run (in the future you can just do Run As->Maven Build)
+
 7. To run different launchers defined in your pom.xml you will have to add new Maven run configurations. For e.g. a launcher called "LauncherExample", you would want to create a new run configuration for the project with the goal "scala:run" and parameter "Dlauncher" with value "LauncherExample"
 
 IntelliJ IDEA (12 CE):
+
 1. Select Import Project from the Quick Start menu.
+
 2. Select the top-level directory from your cloned FACTORIE project, the directory containing the pom.xml file.
+
 3. Select "Import project from external model" then highlight Maven as the type.
+
 4. The root directory should be set to the directory containing the pom.xml, and select "Import Maven projects automatically"
+
 5. Select the profile "nlp-jar-with-dependencies"
+
 6. The FACTORIE Maven project should be automatically selected in the next window
+
 7. Make sure the project SDK is set to the correct JDK on your machine, version >= 1.5. You may have to set your JDK home path if you have not yet configured your JDK in IntelliJ
+
 8. Select the project name and location for the IDEA project to be stored. The defaults should be fine.
+
 9. IDEA should detect that this is a git project and ask if you want to register it as such. If you'd like to manage git version control through IDEA you should say yes.
-10. When IDEA asks whether you'd like to enable type-aware highlighting, it is probably best to say yes. 
+
+10. When IDEA asks whether you'd like to enable type-aware highlighting, it is probably best to say yes.
+
 11. To compile FACTORIE, do Build -> Make Project
+
 12. To run different launchers defined in your pom.xml, go to Run -> Edit Configurations then click on the + in the top left to create a new Maven run configuration. For e.g. a launcher called "LauncherExample" you would want to add the command line "scala:run -Dlauncher=LauncherExample". The default working directory should be the project root, which is correct.
 
 If you find you are running out of Java heap space or PermGen space when compiling, add the JVM arguments "-Xms2g -Xmx2g -XX:MaxPermSize=256m" in your run configuration.
