b7390a56850f3261ec0a1e35c6e78b09106d0fe6
Caitlin Cellier <Caitlin.Cellier@gmail.com>
2014-01-09 18:08:01 -0500

    Made NPChunkMentionFinder Generic

--- src/main/scala/cc/factorie/app/nlp/mention/NPChunkMentionFinder.scala
+++ src/main/scala/cc/factorie/app/nlp/mention/NPChunkMentionFinder.scala
@@ -4,13 +4,9 @@ import java.io.{File, FileOutputStream, FileInputStream}
 import cc.factorie.app.nlp._
 import scala.collection.mutable.{ListBuffer, ArrayBuffer}
 import cc.factorie.app.nlp.phrase.{ChunkerOpts, CRFChunker}
-import cc.factorie.app.nlp.load.{BILOU2LayerChunkTag, BILOUChunkTag}
-import cc.factorie.app.nlp.coref._
-import cc.factorie.app.nlp.pos.PennPosTag
+import cc.factorie.app.nlp.load.{ChunkTag, BILOU2LayerChunkTag, BILOUChunkTag}
 import cc.factorie.app.nlp.ner.{ConllChainNer, NerTag}
-import cc.factorie.app.nlp.coref.ForwardCorefTrainer.opts
-import cc.factorie.app.nlp.mention.Mention
-import scala.collection.mutable
+
 
 
 /**
@@ -21,11 +17,13 @@ import scala.collection.mutable
  */
 
 class ChunkBasedMentionList extends MentionList
-
-
-class MultiLayerNPChunkMentionFinder extends NPChunkMentionFinder{
-  override def getMentionSpans(document: Document): Seq[(String,TokenSpan)] ={
-    val mentionSpans = ArrayBuffer[(String,TokenSpan)]()
+/*
+ * Object to retrieve Multilayered BILOU Tags
+ */
+object MultiLayerNPChunkMentionFinder extends NPChunkMentionFinder[BILOU2LayerChunkTag]{
+  //Splits tag value and calls to retrieve NPs for the inner tags and outer tags
+  override def getMentionSpans(document: Document): Seq[TokenSpan] ={
+    val mentionSpans = ArrayBuffer[TokenSpan]()
     document.sentences.foreach{s=>
       val chunkTags = s.tokens.map(t => t.attr[BILOU2LayerChunkTag].categoryValue.split(":").map(layer => t -> layer)).map(layer => (layer(0),layer(1)))
       val (innerTags,outerTags) = chunkTags.unzip
@@ -35,9 +33,10 @@ class MultiLayerNPChunkMentionFinder extends NPChunkMentionFinder{
     mentionSpans.seq
   }
 }
+object NPChunkMentionFinder extends NPChunkMentionFinder[BILOUChunkTag]
 
-class NPChunkMentionFinder extends DocumentAnnotator {
-  def prereqAttrs = Seq(classOf[Token], classOf[Sentence],classOf[BILOU2LayerChunkTag])
+class NPChunkMentionFinder[L<:ChunkTag](implicit m: Manifest[L]) extends DocumentAnnotator {
+  def prereqAttrs = Seq(classOf[Token], classOf[Sentence],m.runtimeClass)
   def postAttrs = Seq(classOf[MentionList], classOf[MentionEntityType])
   override def tokenAnnotationString(token:Token): String = token.document.attr[MentionList].filter(mention => mention.contains(token)) match { case ms:Seq[Mention] if ms.length > 0 => ms.map(m => m.attr[MentionType].categoryValue+":"+ m.attr[MentionEntityType].categoryValue +":" +m.indexOf(token)).mkString(","); case _ => "_" }
 
@@ -48,18 +47,19 @@ class NPChunkMentionFinder extends DocumentAnnotator {
     document.attr += new MentionList() ++= mentions.sortBy(m => (m.head.stringStart, m.length))
     document
   }
-  //Todo: convert to multiple encoding capable
+
   def addChunkMentions(document: Document): Seq[Mention] = {
     getMentionSpans(document).map{labelSpan =>
-      val s = labelSpan._2
+      val s = labelSpan
       val m = new Mention(s, s.length-1)
-      m.attr += new MentionType(m, "NAM")
+      //m.attr += new MentionType(m, "NAM")
+      m.attr += new MentionEntityType(m,"")
       m
     }
   }
 
-  def getMentionSpans(document: Document): Seq[(String,TokenSpan)] ={
-    val mentionSpans = ArrayBuffer[(String,TokenSpan)]()
+  def getMentionSpans(document: Document): Seq[TokenSpan] ={
+    val mentionSpans = ArrayBuffer[TokenSpan]()
     document.sentences.foreach{s=>
       val chunkTags = s.tokens.map(t => t-> t.attr[BILOUChunkTag].categoryValue)
       mentionSpans ++= getNPChunkSpans(s,chunkTags)
@@ -67,17 +67,17 @@ class NPChunkMentionFinder extends DocumentAnnotator {
     mentionSpans.seq
   }
 
-  def getNPChunkSpans(s: Sentence,chunkTags: IndexedSeq[(Token, String)]):Seq[(String,TokenSpan)]={
-    val spans = ArrayBuffer[(String,TokenSpan)]()
+  def getNPChunkSpans(s: Sentence,chunkTags: IndexedSeq[(Token, String)]):Seq[TokenSpan]={
+    val spans = ArrayBuffer[TokenSpan]()
     chunkTags.map{case (t,chunk) =>
       if (chunk != "O") {
-        if(chunk == "U-NP") spans += (chunk -> new TokenSpan(s.section, t.positionInSection, 1))
+        if(chunk == "U-NP") spans += new TokenSpan(s.section, t.positionInSection, 1)
         else if(chunk == "B-NP"){
           if(t.hasNext) {
             var lookFor = t.next
             while (lookFor.hasNext && lookFor.sentence == t.sentence && chunkTags(chunkTags.map(_._1.string).indexOf(lookFor.string))._2.matches("(I|L)-NP")) lookFor = lookFor.next
-            spans += (chunk -> new TokenSpan(s.section, t.positionInSection, lookFor.positionInSection - t.positionInSection))
-          } else  spans += (chunk -> new TokenSpan(s.section, t.positionInSection, 1))
+            spans += new TokenSpan(s.section, t.positionInSection, lookFor.positionInSection - t.positionInSection)
+          } else  spans += new TokenSpan(s.section, t.positionInSection, 1)
         }
       }
     }
