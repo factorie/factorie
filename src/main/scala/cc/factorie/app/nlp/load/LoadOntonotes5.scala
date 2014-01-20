3fdad99c8b3ed25a5b003260828bc1c8a183af02
Emma Strubell <strubell@cs.umass.edu>
2014-01-20 15:54:31 -0500

    Updated Ontonotes loader for newest version of Ontonotes, added parameters to specify whether to load auto or gold values

--- src/main/scala/cc/factorie/app/nlp/load/LoadOntonotes5.scala
+++ src/main/scala/cc/factorie/app/nlp/load/LoadOntonotes5.scala
@@ -18,16 +18,22 @@ import java.io.PrintWriter
   5   gold POS tag
   6   auto POS tag
   7   gold feats
-  8   gold head ID
-  9   auto head ID
-  10  gold dependency label
-  11  auto dependency label
-  12  gold secondary dependencies
-  13  gold semantic arguments
-  14  gold named entity tags
-  15  gold coreference
-  
+  8	  auto feats
+  9   auto head ID				// why are these switched???
+  10  gold head ID
+  11  auto dependency label		// why are these switched???
+  12  gold dependency label
+  13  gold secondary dependencies
+  14  gold semantic arguments
+  15  gold named entity tags
+  16  gold coreference
+
  */
+object AnnotationTypes{
+  val NONE = 0
+  val GOLD = 1
+  val AUTO = 2
+}
 
 object LoadOntonotes5 {
   private def addDepInfo(s: Sentence, depInfoSeq: Seq[(Int,Int,String)]): Unit = {
@@ -36,54 +42,11 @@ object LoadOntonotes5 {
     s.attr += tree
   }
 
-  def fromFilename(filename:String, loadLemma:Boolean = true, loadPos:Boolean = true, loadNer:Boolean = true, nerBilou:Boolean = false): Seq[Document] = {
+  def fromLines(lines:Iterator[String], filename:String = "?UNKNOWN?", loadLemma:Int = AnnotationTypes.GOLD, loadPos:Int = AnnotationTypes.GOLD, loadParse:Int = AnnotationTypes.GOLD, loadNer:Boolean = true, nerBilou:Boolean = true): Seq[Document] = {
     val document: Document = new Document().setName("Ontonotes499/" + filename)
     document.annotators(classOf[Token]) = UnknownDocumentAnnotator.getClass // register that we have token boundaries
     document.annotators(classOf[Sentence]) = UnknownDocumentAnnotator.getClass // register that we have sentence boundaries
-    if (loadPos) document.annotators(classOf[pos.PennPosTag]) = UnknownDocumentAnnotator.getClass // register that we have POS tags
-    if (loadNer) if (nerBilou) document.annotators(classOf[ner.BilouOntonotesNerTag]) = UnknownDocumentAnnotator.getClass else document.annotators(classOf[ner.BioOntonotesNerTag]) = UnknownDocumentAnnotator.getClass
-    val source = Source.fromFile(filename)
-    var sentence: Sentence = new Sentence(document)
-    var depInfoSeq = new collection.mutable.ArrayBuffer[(Int,Int,String)]
-    for (line <- source.getLines()) {
-      if (line.length < 2) { // Sentence boundary
-        document.appendString("\n")
-        addDepInfo(sentence, depInfoSeq)
-        depInfoSeq = new collection.mutable.ArrayBuffer[(Int,Int,String)]
-        sentence = null
-      } else {
-        if (sentence eq null)
-          sentence = new Sentence(document) // avoids empty sentence at the end of doc
-        val fields = line.split('\t')
-        assert(fields.length >= 10, "Fewer than 10 fields in file "+filename+"\nOffending line:\n"+line)
-        val currTokenIdx = fields(0).toInt - 1
-        val word = fields(1)
-        val lemma = fields(2) // was 3
-        val partOfSpeech = fields(4)
-        val parentIdx = fields(7).toInt - 1
-        val depLabel = fields(9)
-        var ner = fields(13); if (ner == "_") ner = "O"  // If we wanted to distinguish "unnamed entities" from background, we wouldn't have this.
-        document.appendString(" ")
-        val token = new Token(sentence, word)
-        if (loadPos) token.attr += new LabeledPennPosTag(token, if (partOfSpeech == "XX") "PUNC" else partOfSpeech)
-        if (loadNer) token.attr += (if (nerBilou) new LabeledBilouOntonotesNerTag(token, ner) else new LabeledBioOntonotesNerTag(token, ner))
-        if (loadLemma) token.attr += new TokenLemma(token, lemma) // TODO Change this to some more specific TokenLemma subclass
-        depInfoSeq.append((currTokenIdx, parentIdx, depLabel))
-      }
-    }
-    if (sentence ne null) addDepInfo(sentence, depInfoSeq)
-    if (nerBilou) convertBioBilou(document.asSection)
-
-    println("Loaded 1 document with "+document.sentences.size+" sentences with "+document.asSection.length+" tokens total from file "+filename)
-    Seq(document)
-  }
-  
-  // TODO Use this method in method above, to avoid redundancy.
-  def fromLines(lines:Iterator[String], filename:String = "?UNKNOWN?", loadLemma:Boolean = true, loadPos:Boolean = true, loadNer:Boolean = true, nerBilou:Boolean = false): Seq[Document] = {
-    val document: Document = new Document()
-    document.annotators(classOf[Token]) = UnknownDocumentAnnotator.getClass // register that we have token boundaries
-    document.annotators(classOf[Sentence]) = UnknownDocumentAnnotator.getClass // register that we have sentence boundaries
-    if (loadPos) document.annotators(classOf[pos.PennPosTag]) = UnknownDocumentAnnotator.getClass // register that we have POS tags
+    if (loadPos != AnnotationTypes.NONE) document.annotators(classOf[pos.PennPosTag]) = UnknownDocumentAnnotator.getClass // register that we have POS tags
     if (loadNer) if (nerBilou) document.annotators(classOf[ner.BilouOntonotesNerTag]) = UnknownDocumentAnnotator.getClass else document.annotators(classOf[ner.BioOntonotesNerTag]) = UnknownDocumentAnnotator.getClass
     var sentence: Sentence = new Sentence(document)
     var depInfoSeq = new collection.mutable.ArrayBuffer[(Int,Int,String)]
@@ -98,28 +61,58 @@ object LoadOntonotes5 {
           sentence = new Sentence(document) // avoids empty sentence at the end of doc
         val fields = line.split('\t')
         assert(fields.length >= 10, "Fewer than 10 fields in file "+filename+"\nOffending line:\n"+line)
+
         val currTokenIdx = fields(0).toInt - 1
         val word = fields(1)
-        val lemma = fields(2) // was 3
-        val partOfSpeech = fields(4)
-        val parentIdx = fields(7).toInt - 1
-        val depLabel = fields(9)
-        var ner = fields(13); if (ner == "_") ner = "O"  // If we wanted to distinguish "unnamed entities" from background, we wouldn't have this.
+        
+        val autoLemma = fields(2)
+        val goldLemma = fields(3)
+        
+        val autoPartOfSpeech = fields(4)
+        val goldPartOfSpeech = fields(5)
+        
+        // OFF BY 1!
+        val autoParentIdx = fields(8).toInt - 1 
+        val goldParentIdx = fields(9).toInt - 1
+        
+        val autoDepLabel = fields(10)
+        val goldDepLabel = fields(11)
+        
+        var ner = fields(14); if (ner == "_") ner = "O"  // If we wanted to distinguish "unnamed entities" from background, we wouldn't have this.
+        
+          
+        //var ner = "O" // NER reader needs to be revisited to make this work
         document.appendString(" ")
         val token = new Token(sentence, word)
-        if (loadPos) token.attr += new PennPosTag(token, if (partOfSpeech == "XX") "PUNC" else partOfSpeech)
-        if (loadNer) token.attr += (if (nerBilou) new BilouOntonotesNerTag(token, ner) else new BioOntonotesNerTag(token, ner))
-        if (loadLemma) token.attr += new TokenLemma(token, lemma) // TODO Change this to some more specific TokenLemma subclass
-        depInfoSeq.append((currTokenIdx, parentIdx, depLabel))
+        loadPos match{
+	      case AnnotationTypes.GOLD => {token.attr += new LabeledPennPosTag(token, if (goldPartOfSpeech == "XX") "PUNC" else goldPartOfSpeech)}
+	      case AnnotationTypes.AUTO => {token.attr += new LabeledPennPosTag(token, if (autoPartOfSpeech == "XX") "PUNC" else autoPartOfSpeech)}
+	      case AnnotationTypes.NONE => {/* do nothing */}
+        }
+        loadLemma match{
+	      case AnnotationTypes.GOLD => {token.attr += new TokenLemma(token, goldLemma)}
+	      case AnnotationTypes.AUTO => {token.attr += new TokenLemma(token, autoLemma)}
+	      case AnnotationTypes.NONE => {/* do nothing */}
+        }
+        loadParse match{
+	      case AnnotationTypes.GOLD => {depInfoSeq.append((currTokenIdx, goldParentIdx, goldDepLabel))}
+	      case AnnotationTypes.AUTO => {depInfoSeq.append((currTokenIdx, autoParentIdx, autoDepLabel))}
+	      case AnnotationTypes.NONE => {/* do nothing */}
+        }
+        if (loadNer) token.attr += (if (nerBilou) new LabeledBilouOntonotesNerTag(token, ner) else new LabeledBioOntonotesNerTag(token, ner))
       }
     }
-    if (sentence ne null) addDepInfo(sentence, depInfoSeq)
+    if ((sentence != null) && (loadParse != AnnotationTypes.NONE)) addDepInfo(sentence, depInfoSeq)
     if (nerBilou) convertBioBilou(document.asSection)
 
     println("Loaded 1 document with "+document.sentences.size+" sentences with "+document.asSection.length+" tokens total from file "+filename)
     Seq(document)
   }
-  
+
+  def fromFilename(filename:String, loadLemma:Int = AnnotationTypes.GOLD, loadPos:Int = AnnotationTypes.GOLD, loadParse:Int = AnnotationTypes.GOLD, loadNer:Boolean = true, nerBilou:Boolean = true): Seq[Document] = {
+    fromLines(Source.fromFile(filename).getLines(), filename, loadLemma, loadPos, loadParse, loadNer, nerBilou)
+  }
+
   // TODO Don't we need convertIobBilou since CoNLL 2003 data is actually in IOB format? -akm
   def convertBioBilou(section:Section): Unit = {
     /** Return the string of the NER label, including the two letter (B- or I-) prefix. */
