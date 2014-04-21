78d20ede9c0f86fe93f6556b21c6d58531870cb4
Jeevan Shankar <jeevanshankar1991@gmail.com>
2014-04-21 14:02:02 -0400

    Update WordEmbeddingModel.scala

--- src/main/scala/cc/factorie/app/nlp/embeddings/WordEmbeddingModel.scala
+++ src/main/scala/cc/factorie/app/nlp/embeddings/WordEmbeddingModel.scala
@@ -39,8 +39,8 @@ abstract class WordEmbeddingModel(val opts: EmbeddingOpts) extends Parameters {
   // Component-1
   def buildVocab(): Unit = {
     vocab = new VocabBuilder(vocabHashSize, samplingTableSize, 0.7) // 0.7 is the load factor 
-    println("Building Vocab")
     if (loadVocabFilename.size == 0) {
+      println("Building Vocab")
       val corpusLineItr = corpus.endsWith(".gz") match {
         case true => io.Source.fromInputStream(new GZIPInputStream(new FileInputStream(corpus)), encoding).getLines
         case false => io.Source.fromInputStream(new FileInputStream(corpus), encoding).getLines
@@ -49,8 +49,10 @@ abstract class WordEmbeddingModel(val opts: EmbeddingOpts) extends Parameters {
         val line = corpusLineItr.next
         line.stripLineEnd.split(' ').foreach(word => vocab.addWordToVocab(word)) // addWordToVocab() will incr by count. TODO : make this also parallel ? but it is an one time process, next time use load-vocab option
       }
-    } else vocab.loadVocab(loadVocabFilename, encoding)
-
+    } else {
+      println("Loading Vocab")
+      vocab.loadVocab(loadVocabFilename, encoding)
+    }
     vocab.sortVocab(minCount, ignoreStopWords, maxVocabSize) // removes words whose count is less than minCount and sorts by frequency
     vocab.buildSamplingTable() // for getting random word from vocab in O(1) otherwise would O(log |V|)
     vocab.buildSubSamplingTable(opts.sample.value) // precompute subsampling table
