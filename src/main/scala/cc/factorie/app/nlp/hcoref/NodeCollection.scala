0ebfcd4905c68f8e06d4af0802d971c66386db20
John Sullivan <john.t.sullivan@gmail.com>
2014-05-15 00:03:35 -0500

    Renamed xcoref to hcoref.

--- src/main/scala/cc/factorie/app/nlp/hcoref/NodeCollection.scala
+++ src/main/scala/cc/factorie/app/nlp/hcoref/NodeCollection.scala
@@ -10,7 +10,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License. */
-package cc.factorie.app.nlp.xcoref
+package cc.factorie.app.nlp.hcoref
 
 import cc.factorie.db.mongo.{LazyCubbieConverter, MongoCubbieCollection, MutableCubbieCollection}
 import cc.factorie.db.mongo.MongoCubbieImplicits._
