11eae38aa6a1ef5715f6a723cea5812dcf6fb613
Oskar Singer <oskarsinger@gmail.com>
2014-10-04 17:11:56 -0400

    Readded punctuation feature

--- src/main/scala/cc/factorie/app/nlp/pos/ChainPosTagger.scala
+++ src/main/scala/cc/factorie/app/nlp/pos/ChainPosTagger.scala
@@ -259,9 +259,10 @@ class CtbChainPosTagger extends ChainPosTagger((t:Token) => new CtbPosTag(t, 0))
 
         features ++= suffixCTBMorph
       }
-/*
+
       if (hasPunctuation(rawWord)) features += "PUNCTUATION"
-      if (hasNumeric(rawWord)) features += "NUMERIC"
+/*
+      if (hasNumeric(rawWord) || hasChineseNumeric(rawWord)) features += "NUMERIC"
       if (hasChineseNumeric(rawWord)) features += "CHINESE_NUMERIC"
       if (hasAlpha(rawWord)) features += "ALPHA"
 */
