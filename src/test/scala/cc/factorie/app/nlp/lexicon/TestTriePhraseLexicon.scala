bf7d27599d3a08eb64122d909ac483a3d2727a8b
k8si <ksilvers@cs.umass.edu>
2015-01-12 16:58:57 -0500

    back to junit

--- src/test/scala/cc/factorie/app/nlp/lexicon/TestTriePhraseLexicon.scala
+++ src/test/scala/cc/factorie/app/nlp/lexicon/TestTriePhraseLexicon.scala
@@ -1,30 +1,32 @@
 package cc.factorie.app.nlp.lexicon
 
-import org.scalatest._
-
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
 
 /**
  * @author Kate Silverstein 
  *         created on 1/12/15
  */
 
-class TestTriePhraseLexicon extends FlatSpec{
-  val phrase = "The quick brown fox jumped over the lazy dog"
+class TestTriePhraseLexicon extends JUnitSuite{
+  val phrase = "the quick brown fox jumped over the lazy dog"
 
-
-  "TriePhraseLexicon" should "contain 'fox'" in {
+  @Test
+  def testContainsLemmatizedWord(): Unit = {
     val lexicon = new TriePhraseLexicon("test")
     phrase.split(" ").foreach(lexicon += _)
     assert(lexicon.containsLemmatizedWord("fox"))
   }
 
-  it should "contain 'the quick brown fox...'" in {
+  @Test
+  def testContainsLemmatizedWords(): Unit = {
     val lexicon = new TriePhraseLexicon("test")
     lexicon += phrase
     assert(lexicon.containsLemmatizedWords(phrase.split(" ")))
   }
 
-  it should "contain both 'fox' and 'quick brown'" in {
+  @Test
+  def testMultiword(): Unit = {
     val words = List("the", "quick brown", "fox")
     val lexicon = new TriePhraseLexicon("test")
     words.foreach(lexicon += _)
