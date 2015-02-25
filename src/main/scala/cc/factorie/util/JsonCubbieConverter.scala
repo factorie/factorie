3b651c9677d498d99b5cc569d2730168d6ff0ddc
John Sullivan <john.t.sullivan@gmail.com>
2015-02-25 12:15:28 -0500

    Added some code to serialize arbitrary cubbies back and forth into JSON, along with some tests that use DocumentCubbie.

--- src/main/scala/cc/factorie/util/JsonCubbieConverter.scala
+++ src/main/scala/cc/factorie/util/JsonCubbieConverter.scala
@@ -2,18 +2,20 @@ package cc.factorie.util
 
 import org.json4s._
 import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
 import scala.collection.mutable
 
 /**
  * @author John Sullivan
  */
-class JsonCubbieConverter {
+object JsonCubbieConverter {
 
   def toJson(c:Cubbie):JObject = {
     def toJsonImpl(a:Any):JValue = a match {
-      case m:mutable.Map[String,_] =>
-        m.toMap.map{case (k,v) => k -> toJsonImpl(v)}
+      case null => JNull
+      case is:IntSeq => is._rawArray.map(toJsonImpl).toIterable  // we can do something cleverer here if we want
+      case ds:DoubleSeq => ds._rawArray.map(toJsonImpl).toIterable
+      case m:mutable.Map[_,_] =>
+        m.toMap.map{case (k,v) => println((k,v));k.toString -> toJsonImpl(v)}
       case it:Iterable[_] =>
         it.map(toJsonImpl)
       case i:Int => i
@@ -22,6 +24,9 @@ class JsonCubbieConverter {
       case f:Float => f
       case b:Boolean => b
       case s:String => s
+      case d:java.util.Date => d.toString
+      case Some(an) => toJsonImpl(an)
+      case None => JNothing
     }
     toJsonImpl(c._map).asInstanceOf[JObject]
   }
@@ -32,10 +37,16 @@ class JsonCubbieConverter {
       case JObject(fields) =>
         val m = new mutable.HashMap[String,Any]()
         fields foreach { case JField(name, value) =>
-          m += (name, toCubbieImpl(value))
+          m += name -> toCubbieImpl(value)
         }
         m
-      case JArray(vals) => vals map toCubbieImpl
+      case JArray(vals) => if(vals.forall(_.isInstanceOf[JInt])) { // we assume this was an IntSeq
+        new ArrayIntSeq(vals.map{case JInt(i) => i.toInt}.toArray)
+      } else if(vals.forall(_.isInstanceOf[JDouble])) {
+        new ArrayDoubleSeq(vals.map{case JDouble(d) => d.toDouble}.toArray)
+      } else {
+        vals map toCubbieImpl
+      }
       case JInt(i) => if(i.isValidInt) {
         i.toInt
       } else if(i.isValidLong) {
@@ -46,6 +57,13 @@ class JsonCubbieConverter {
       case JDouble(d) => d
       case JBool(b) => b
       case JString(s) => s
+      case JDecimal(d) => if(d.isDecimalDouble){
+        d.toDouble
+      } else {
+        d
+      }
+      case JNothing => None
+      case JNull => null
     }
     c._map = toCubbieImpl(jObj).asInstanceOf[mutable.Map[String, Any]]
     c
