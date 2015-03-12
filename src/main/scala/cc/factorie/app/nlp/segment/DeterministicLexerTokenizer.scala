e8288387906178f42fa9a8ad9a629c6283ad1b03
Emma Strubell <strubell@cs.umass.edu>
2015-03-12 15:34:35 -0400

    try to fix arabic issue

--- src/main/scala/cc/factorie/app/nlp/segment/DeterministicLexerTokenizer.scala
+++ src/main/scala/cc/factorie/app/nlp/segment/DeterministicLexerTokenizer.scala
@@ -52,8 +52,8 @@ class DeterministicLexerTokenizer(caseSensitive:Boolean = false, tokenizeSgml:Bo
 
 object DeterministicLexerTokenizer extends DeterministicLexerTokenizer(false, false, false, false, false) {
   def main(args: Array[String]): Unit = {
-//    val fname = "/iesl/canvas/strubell/data/tackbp/source/2013/LDC2013E45_TAC_2013_KBP_Source_Corpus_disc_2/data/English/discussion_forums/bolt-eng-DF-200"
-    val fname = "/iesl/canvas/strubell/arabic.txt"
+    val fname = "/iesl/canvas/strubell/data/tackbp/source/2013/LDC2013E45_TAC_2013_KBP_Source_Corpus_disc_2/data/English/discussion_forums/bolt-eng-DF-200"
+//    val fname = "/iesl/canvas/strubell/arabic.txt"
 //    val fname = "/Users/strubell/Documents/research/arabic.txt"
     println(s"Loading $fname")
     val string = io.Source.fromFile(fname, "utf-8").mkString
