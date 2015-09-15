8cc7346ac0031e2216c9a4655021d8a0eae4751c
Nicholas Monath <nicholas.monath@gmail.com>
2015-09-14 20:08:13 -0400

    Added random seeds (followed seed in CBOW implementation) to the skipgram negative sampling implementation and in the initialization in the embeddings package.

--- src/main/scala/cc/factorie/app/nlp/embeddings/SkipGramEmbeddingModel.scala
+++ src/main/scala/cc/factorie/app/nlp/embeddings/SkipGramEmbeddingModel.scala
@@ -18,7 +18,7 @@ import cc.factorie.util.DoubleAccumulator
 class SkipGramNegSamplingEmbeddingModel(override val opts: EmbeddingOpts) extends WordEmbeddingModel(opts) {
   val negative = opts.negative.value
   val window = opts.window.value
-  val rng = new util.Random
+  val rng = new util.Random(5) // set rng seed
   val sample = opts.sample.value.toDouble
   override def process(doc: String): Int = {
     // given a document, below line splits by space and converts each word to Int (by vocab.getId) and filters out words not in vocab
